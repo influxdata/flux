9e4f3479f117369dcefe55c97b11ed9e84492d6c
Christopher Wolff <chris.wolff@influxdata.com>
2018-09-28 11:29:39 -0700

    Logical and physical plan nodes with common interface

--- planner/physical.go
+++ planner/physical.go
@@ -1,98 +1,34 @@
 package planner
 
-import (
-	"github.com/influxdata/flux"
-	"github.com/influxdata/flux/semantic"
-	"github.com/influxdata/platform"
-)
+import "github.com/influxdata/flux/plan"
 
-// PhysicalPlan represents a query tree/subtree whose nodes
-// are the physical operations/algorithms to be performed.
-type PhysicalPlan interface {
-	physical()
-	Predecessors() []PhysicalPlan
-	Successors() []PhysicalPlan
-	Cost() Cost
+// PhysicalProcedureSpec is similar to its logical counterpart but must provide a method to determine cost
+type PhysicalProcedureSpec interface {
+	Kind() plan.ProcedureKind
+	Copy() plan.ProcedureSpec
+	Cost(inStats []Statistics) (cost Cost, outStats Statistics)
 }
 
-func (node *FromRangeScan) physical()      {}
-func (node *FromRangeIndexScan) physical() {}
-func (node *Filter) physical()             {}
-func (node *Sort) physical()               {}
+type PhysicalPlanNode struct {
+	Edges
+	procedureSpec PhysicalProcedureSpec
 
-func (node *FromRangeIndexScan) Predecessors() []PhysicalPlan {
-	return node.predecessors
-}
-func (node *FromRangeScan) Predecessors() []PhysicalPlan {
-	return node.predecessors
-}
-func (node *Filter) Predecessors() []PhysicalPlan {
-	return node.predecessors
-}
-func (node *Sort) Predecessors() []PhysicalPlan {
-	return node.predecessors
+	// The attributes required from inputs to this node
+	RequiredAttrs []PhysicalAttributes
+
+	// The attributes provided to consumers of this node's output
+	OutputAttrs PhysicalAttributes
 }
 
-func (node *FromRangeIndexScan) Successors() []PhysicalPlan {
-	return node.successors
-}
-func (node *FromRangeScan) Successors() []PhysicalPlan {
-	return node.successors
-}
-func (node *Filter) Successors() []PhysicalPlan {
-	return node.successors
-}
-func (node *Sort) Successors() []PhysicalPlan {
-	return node.successors
+func (ppn *PhysicalPlanNode) ProcedureSpec() plan.ProcedureSpec {
+	return ppn.procedureSpec
 }
 
-// FromRangeScan represents the physical operation of
-// scanning a series from a start time to an end time.
-type FromRangeScan struct {
-	Bucket   string
-	BucketID platform.ID
-	Bounds   flux.Bounds
-
-	predecessors []PhysicalPlan
-	successors   []PhysicalPlan
+func (ppn *PhysicalPlanNode) Cost(inStats []Statistics) (cost Cost, outStats Statistics) {
+	return ppn.procedureSpec.Cost(inStats)
 }
 
-func (node *FromRangeScan) cost() Cost { return Cost{} }
-
-// FromRangeIndexScan represents the physical operation of
-// scanning a series within a specified time range using an index.
-type FromRangeIndexScan struct {
-	Bucket   string
-	BucketID platform.ID
-	Bounds   flux.Bounds
-	Filter   *semantic.FunctionExpression
-
-	predecessors []PhysicalPlan
-	successors   []PhysicalPlan
+type PhysicalAttributes struct {
+	// Any physical attributes of the result produced by a physical plan node:
+	// Collation, etc.
 }
-
-func (node *FromRangeIndexScan) cost() Cost { return Cost{} }
-
-// Filter represents the physical operation of filtering a
-// stream of data based on some condition. No index is involved.
-type Filter struct {
-	Schema    []flux.ColMeta
-	Condition *semantic.FunctionExpression
-
-	predecessors []PhysicalPlan
-	successors   []PhysicalPlan
-}
-
-func (node *Filter) cost() Cost { return Cost{} }
-
-// Sort represents the physical operations of sorting a stream
-// according to the values in a specified column/columns.
-type Sort struct {
-	Schema flux.ColMeta
-	On     []flux.ColMeta
-
-	predecessors []PhysicalPlan
-	successors   []PhysicalPlan
-}
-
-func (node *Sort) cost() Cost { return Cost{} }
