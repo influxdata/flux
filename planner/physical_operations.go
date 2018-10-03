a6bc7f4ab5c24a794f2957084284cd5a65b74656
jlapacik <joshua.lapacik@pacbell.net>
2018-10-03 09:55:37 -0700

    first pass logical-to-physical planner

---
+++ planner/physical_operations.go
@@ -0,0 +1,97 @@
+package planner
+
+import (
+	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/semantic"
+	"github.com/influxdata/platform"
+)
+
+// FromRangeProcedureSpec represents a sequential scanning operation
+type FromRangeProcedureSpec struct {
+	Bucket   string
+	BucketID platform.ID
+	Bounds   flux.Bounds
+	TimeCol  string
+	StartCol string
+	StopCol  string
+}
+
+func (spec *FromRangeProcedureSpec) Kind() ProcedureKind {
+	return "FromRange"
+}
+func (spec *FromRangeProcedureSpec) Copy() ProcedureSpec {
+	return &FromRangeProcedureSpec{
+		Bucket:   spec.Bucket,
+		BucketID: spec.BucketID,
+		Bounds:   spec.Bounds,
+		TimeCol:  spec.TimeCol,
+		StartCol: spec.StartCol,
+		StopCol:  spec.StopCol,
+	}
+}
+func (spec *FromRangeProcedureSpec) Cost(inStats []Statistics) (Cost, Statistics) {
+	return Cost{}, Statistics{}
+}
+
+// FromTagFilterProcedureSpec represents an index scanning operation
+type FromTagFilterProcedureSpec struct {
+	Bucket   string
+	BucketID platform.ID
+
+	// A conjunctive predicate of tag, value equalities.
+	// For example, if the predicate is: r.tag1 = "A" AND r.tag1 == "B",
+	// then TagEqualityFilters is: map[string]string{"tag0": "A", "tag1": "B"}.
+	TagEqualityFilters map[string]string
+	PredicateFunction  *semantic.FunctionExpression
+}
+
+func (spec *FromTagFilterProcedureSpec) Kind() ProcedureKind {
+	return "FromTagFilter"
+}
+func (spec *FromTagFilterProcedureSpec) Copy() ProcedureSpec {
+	tagPreds := make(map[string]string, len(spec.TagEqualityFilters))
+	for k, v := range spec.TagEqualityFilters {
+		tagPreds[k] = v
+	}
+	return &FromTagFilterProcedureSpec{
+		Bucket:   spec.Bucket,
+		BucketID: spec.BucketID,
+
+		TagEqualityFilters: tagPreds,
+	}
+}
+func (spec *FromTagFilterProcedureSpec) Cost(inStats []Statistics) (Cost, Statistics) {
+	return Cost{}, Statistics{}
+}
+
+// FromRangeFieldFilterProcedureSpec represents a sequential scanning operation
+type FromRangeFieldFilterProcedureSpec struct {
+	Bucket   string
+	BucketID platform.ID
+	Bounds   flux.Bounds
+	TimeCol  string
+	StartCol string
+	StopCol  string
+
+	// predicate not involving any tags
+	// For example, (r) => r._value > 0
+	PredicateFn *semantic.FunctionExpression
+}
+
+func (spec *FromRangeFieldFilterProcedureSpec) Kind() ProcedureKind {
+	return "FromRange"
+}
+func (spec *FromRangeFieldFilterProcedureSpec) Copy() ProcedureSpec {
+	return &FromRangeFieldFilterProcedureSpec{
+		Bucket:      spec.Bucket,
+		BucketID:    spec.BucketID,
+		Bounds:      spec.Bounds,
+		TimeCol:     spec.TimeCol,
+		StartCol:    spec.StartCol,
+		StopCol:     spec.StopCol,
+		PredicateFn: spec.PredicateFn.Copy().(*semantic.FunctionExpression),
+	}
+}
+func (spec *FromRangeFieldFilterProcedureSpec) Cost(inStats []Statistics) (Cost, Statistics) {
+	return Cost{}, Statistics{}
+}
--- planner/rules.go
+++ planner/rules.go
@@ -6,11 +6,65 @@ type Rule interface {
 	Pattern() Pattern
 
 	// Rewrite an operation into an equivalent one
-	Rewrite(PlanNode) PlanNode
+	Rewrite(PlanNode) (PlanNode, bool)
 }
 
 // Pattern represents an operator tree pattern
 // It can match itself against a query plan
 type Pattern interface {
+	Root() ProcedureKind
 	Match(PlanNode) bool
+}
+
+// LeafPattern implements Pattern.
+// It matches any node that has no predecessors.
+type LeafPattern struct {
+	RootType ProcedureKind
+}
+
+func (lp *LeafPattern) Root() ProcedureKind {
+	return lp.RootType
+}
+
+func (lp *LeafPattern) Match(node PlanNode) bool {
+	return node.ProcedureSpec().Kind() == lp.Root() && (node.Predecessors() == nil || len(node.Predecessors()) == 0)
+}
+
+// AnyPattern implements Pattern.
+// It matches a node that can have any number of predecessors.
+type AnyPattern struct {
+	RootType ProcedureKind
+}
+
+func (ap *AnyPattern) Root() ProcedureKind {
+	return ap.RootType
+}
+
+func (ap *AnyPattern) Match(node PlanNode) bool {
+	return node.ProcedureSpec().Kind() == ap.Root()
+}
+
+// TreePattern implements Pattern.
+// It matches a single node type and a predecessor pattern.
+type TreePattern struct {
+	RootType     ProcedureKind
+	Predecessors []Pattern
+}
+
+func (tp *TreePattern) Root() ProcedureKind {
+	return tp.RootType
+}
+
+func (tp *TreePattern) Match(node PlanNode) bool {
+	if node.ProcedureSpec().Kind() != tp.Root() {
+		return false
+	}
+
+	for i, n := range node.Predecessors() {
+		if !tp.Predecessors[i].Match(n) {
+			return false
+		}
+	}
+
+	return true
 }
