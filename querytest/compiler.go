3d7c35de594e96ab6bb66f7b894efefd2ba24a04
jlapacik <joshua.lapacik@pacbell.net>
2018-11-01 08:29:31 -0700

    add compiler for fromJSON

--- querytest/compiler.go
+++ querytest/compiler.go
@@ -12,11 +12,26 @@ type FromCSVCompiler struct {
 	InputFile string
 }
 
+// FromJSONCompiler wraps a compiler and replaces all From operations with FromJSON
+type FromJSONCompiler struct {
+	flux.Compiler
+	InputFile string
+}
+
 func (c FromCSVCompiler) Compile(ctx context.Context) (*flux.Spec, error) {
 	spec, err := c.Compiler.Compile(ctx)
 	if err != nil {
 		return nil, err
 	}
 	ReplaceFromSpec(spec, c.InputFile)
+	return spec, nil
+}
+
+func (c FromJSONCompiler) Compile(ctx context.Context) (*flux.Spec, error) {
+	spec, err := c.Compiler.Compile(ctx)
+	if err != nil {
+		return nil, err
+	}
+	ReplaceFromWithFromJSONSpec(spec, c.InputFile)
 	return spec, nil
 }
