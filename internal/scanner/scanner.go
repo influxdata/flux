aa70cec8f7c745726e79222f58c1ddfe09d551b9
Jonathan A. Sternberg <jonathan@influxdata.com>
2018-11-02 11:36:31 -0500

    feat: initial work on a scanner using ragel
    The current parser is a mix of the lexing and parsing components and it
    is decently slow and doesn't follow the spec completely. This is the
    initial process of creating a well tested scanner for flux.
    
    It uses ragel for the finite state machine generation. It supports
    lexing the entirety of the language. It includes two methods for
    scanning the input because, in the grammar, regular expressions and the
    division operator will conflict with each other. The `ScanWithRegex`
    method is intended to be used in contexts where we may have a regular
    expression.
    
    This package is placed into internal so that it is not used and it can
    be gradually modified if needed while developing the parser. It may be
    moved to a non-internal package in the future.

---
+++ internal/scanner/scanner.go
@@ -0,0 +1,88 @@
+package scanner
+
+import (
+	"fmt"
+	"unicode/utf8"
+
+	"github.com/influxdata/flux/internal/token"
+)
+
+//go:generate ruby unicode2ragel.rb -e utf8 -o unicode.rl
+//go:generate ragel -I. -Z scanner.rl -o scanner.gen.go
+//go:generate sh -c "go fmt scanner.gen.go > /dev/null"
+
+// Scanner is used to tokenize a flux program.
+type Scanner struct {
+	p, pe, eof  int
+	ts, te, act int
+	curline     int
+	token       token.Token
+	data        []byte
+	checkpoint  int
+	reset       int
+}
+
+// New will construct and initialize a new Scanner.
+func New(data []byte) *Scanner {
+	s := &Scanner{}
+	s.Init(data)
+	return s
+}
+
+// Init initializes the Scanner to scan the data in the byte array.
+func (s *Scanner) Init(data []byte) {
+	s.p, s.pe, s.eof = 0, len(data), len(data)
+	s.data = data
+	s.curline = 1
+	s.init()
+}
+
+// ScanWithRegex will scan the next token for the entire grammar.
+func (s *Scanner) ScanWithRegex() (pos token.Pos, tok token.Token, lit string) {
+	return s.scan(flux_en_main_with_regex)
+}
+
+// Scan will scan the next token while excluding regex literal tokens.
+// This is useful in situations where the parser is not expecting a regular
+// expression such as when reading a binary operator in an expression.
+// This method is needed because the start token for division and the token for a
+// regular expression are the same so this ensures the expression "a / b / c"
+// will not be parsed as a regular expression.
+func (s *Scanner) Scan() (pos token.Pos, tok token.Token, lit string) {
+	return s.scan(flux_en_main)
+}
+
+// Unread will reset the Scanner to go back to the Scanner's location
+// before the last ScanWithRegex or Scan call. If either of the ScanWithRegex methods
+// returned an EOF token, a call to Unread will not unread the discarded whitespace.
+// This method is a no-op if called multiple times.
+func (s *Scanner) Unread() {
+	// If the checkpoint marker is ahead of reset, then use that instead.
+	if s.checkpoint > s.reset {
+		s.p = s.checkpoint
+		return
+	}
+	s.p = s.reset
+}
+
+func (s *Scanner) scan(cs int) (pos token.Pos, tok token.Token, lit string) {
+	s.reset, s.token, s.checkpoint = s.p, token.ILLEGAL, -1
+	if es := s.exec(cs); es == flux_error {
+		// Execution failed meaning we hit a pattern that we don't support and
+		// doesn't produce a token. Use the unicode library to decode the next character
+		// in the sequence so we don't break up any unicode tokens.
+		ch, size := utf8.DecodeRune(s.data[s.ts:])
+		if size == 0 {
+			// This should be impossible as we would have produced an EOF token
+			// instead, but going to handle this anyway as in this impossible scenario
+			// we would enter an infinite loop if we continued scanning past the token.
+			return 0, token.EOF, ""
+		}
+		// Advance the data pointer to after the character we just emitted.
+		s.p = s.ts + size
+		return 0, token.ILLEGAL, fmt.Sprintf("%c", ch)
+	} else if s.token == token.ILLEGAL && s.p == s.eof {
+		return 0, token.EOF, ""
+	}
+	return 0, s.token, string(s.data[s.ts:s.te])
+}
