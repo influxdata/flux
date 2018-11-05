eb066799feae0f90d390cbbcc0afde289ebbd41f
Christopher Wolff <chris.wolff@influxdata.com>
2018-11-05 10:39:50 -0800

    Improve formatted plan output

--- plan/format.go
+++ plan/format.go
@@ -21,12 +21,18 @@ type formatter struct {
 
 func (f formatter) Format(fs fmt.State, c rune) {
 	fmt.Fprintf(fs, "\ndigraph {\n")
+	var edges []string
 	f.p.BottomUpWalk(func(pn PlanNode) error {
 		fmt.Fprintf(fs, "  %v\n", pn.ID())
 		for _, pred := range pn.Predecessors() {
-			fmt.Fprintf(fs, "  %v -> %v\n", pred.ID(), pn.ID())
+			edges = append(edges, fmt.Sprintf("  %v -> %v", pred.ID(), pn.ID()))
 		}
 		return nil
 	})
+
+	fmt.Fprintf(fs, "\n")
+	for _, e := range edges {
+		fmt.Fprintf(fs, "%v\n", e)
+	}
 	fmt.Fprintf(fs, "}\n")
 }
