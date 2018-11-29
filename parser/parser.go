1af43d5b2f39e7c2bbdc7ce0c0bbc5f533d0646f
Jonathan A. Sternberg <jonathan@influxdata.com>
2018-11-29 17:21:50 -0600

    feat: switch over to the new parser

--- parser/parser.go
+++ parser/parser.go
@@ -6,13 +6,15 @@ package parser
 
 import (
 	"github.com/influxdata/flux/ast"
+	fastparser "github.com/influxdata/flux/internal/parser"
+	"github.com/influxdata/flux/internal/scanner"
+	"github.com/influxdata/flux/internal/token"
 )
 
 // NewAST parses Flux query and produces an ast.Program
 func NewAST(flux string, opts ...Option) (*ast.Program, error) {
-	f, err := Parse("", []byte(flux), opts...)
-	if err != nil {
-		return nil, err
-	}
-	return f.(*ast.Program), nil
+	fset := token.NewFileSet()
+	f := fset.AddFile("", -1, len(flux))
+	s := scanner.New(f, []byte(flux))
+	return fastparser.NewAST(s), nil
 }
