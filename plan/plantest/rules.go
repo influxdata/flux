9e9dfbe3ada3753928907ccd004bedd19e54acfc
Christopher Wolff <chris.wolff@influxdata.com>
2018-12-05 13:18:34 -0800

    test(plan): add RuleTestCase and helper method to package plantest
    update from_test.go to use new rule test helper

--- plan/plantest/rules.go
+++ plan/plantest/rules.go
@@ -1,6 +1,12 @@
 package plantest
 
-import "github.com/influxdata/flux/plan"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/influxdata/flux/plan"
+	"github.com/influxdata/flux/semantic/semantictest"
+)
 
 // SimpleRule is a simple rule whose pattern matches any plan node and
 // just stores the NodeIDs of nodes it has visited in SeenNodes.
@@ -19,4 +25,64 @@ func (sr *SimpleRule) Rewrite(node plan.PlanNode) (plan.PlanNode, bool, error) {
 
 func (sr *SimpleRule) Name() string {
 	return "simple"
+}
+
+// RuleTestCase allows for concise creation of test cases that exercise rules
+type RuleTestCase struct {
+	Name     string
+	Rules    []plan.Rule
+	Before   *PlanSpec
+	After    *PlanSpec
+	NoChange bool
+}
+
+// RuleTestHelper will run a rule test case.
+func RuleTestHelper(t *testing.T, tc *RuleTestCase) {
+	t.Helper()
+
+	before := CreatePlanSpec(tc.Before)
+	var after *plan.PlanSpec
+	if tc.NoChange {
+		after = CreatePlanSpec(tc.Before.Copy())
+	} else {
+		after = CreatePlanSpec(tc.After)
+	}
+
+	// Disable validation so that we can avoid having to push a range into every from
+	physicalPlanner := plan.NewPhysicalPlanner(
+		plan.OnlyPhysicalRules(tc.Rules...),
+		plan.DisableValidatation(),
+	)
+
+	pp, err := physicalPlanner.Plan(before)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type testAttrs struct {
+		ID   plan.NodeID
+		Spec plan.ProcedureSpec
+	}
+	want := make([]testAttrs, 0)
+	after.BottomUpWalk(func(node plan.PlanNode) error {
+		want = append(want, testAttrs{
+			ID:   node.ID(),
+			Spec: node.ProcedureSpec(),
+		})
+		return nil
+	})
+
+	got := make([]testAttrs, 0)
+	pp.BottomUpWalk(func(node plan.PlanNode) error {
+		got = append(got, testAttrs{
+			ID:   node.ID(),
+			Spec: node.ProcedureSpec(),
+		})
+		return nil
+	})
+
+	if !cmp.Equal(want, got, semantictest.CmpOptions...) {
+		t.Errorf("transformed plan not as expected, -want/+got:\n%v",
+			cmp.Diff(want, got, semantictest.CmpOptions...))
+	}
 }
