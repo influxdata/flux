c0f28acca7b0dfea12cb5872c3ad311e5aeb14e8
Nathaniel Cook <nvcook42@gmail.com>
2018-12-06 17:27:19 -0700

    fix(ast): add import/package nodes to ast.Walk

--- ast/walk.go
+++ ast/walk.go
@@ -39,20 +39,46 @@ func (v *visitor) Visit(node Node) Visitor {
 
 func (v *visitor) Done(node Node) {}
 
-func walk(v Visitor, n Node) {
-	if n == nil {
-		return
-	}
-
-	switch n := n.(type) {
+func walk(v Visitor, node Node) {
+	// We need to check if node is nil,
+	// because of Go's type system we need to compare against the concrete type of n,
+	// which means we need to check in each case statement.
+	switch n := node.(type) {
 	case *Program:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
+			walk(w, n.Package)
+			for _, i := range n.Imports {
+				walk(w, i)
+			}
 			for _, s := range n.Body {
 				walk(w, s)
 			}
 		}
+	case *PackageClause:
+		if n == nil {
+			return
+		}
+		w := v.Visit(n)
+		if w != nil {
+			walk(w, n.Name)
+		}
+	case *ImportDeclaration:
+		if n == nil {
+			return
+		}
+		w := v.Visit(n)
+		if w != nil {
+			walk(w, n.As)
+			walk(w, n.Path)
+		}
 	case *Block:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			for _, s := range n.Body {
@@ -60,27 +86,42 @@ func walk(v Visitor, n Node) {
 			}
 		}
 	case *OptionStatement:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil && n.Assignment != nil {
 			walk(w, n.Assignment)
 		}
 	case *ExpressionStatement:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Expression)
 		}
 	case *ReturnStatement:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Argument)
 		}
 	case *VariableAssignment:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.ID)
 			walk(w, n.Init)
 		}
 	case *CallExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Callee)
@@ -89,41 +130,62 @@ func walk(v Visitor, n Node) {
 			}
 		}
 	case *PipeExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Argument)
 			walk(w, n.Call)
 		}
 	case *MemberExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Object)
 			walk(w, n.Property)
 		}
 	case *IndexExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Array)
 			walk(w, n.Index)
 		}
 	case *BinaryExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Left)
 			walk(w, n.Right)
 		}
 	case *UnaryExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Argument)
 		}
 	case *LogicalExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Left)
 			walk(w, n.Right)
 		}
 	case *ObjectExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			for _, p := range n.Properties {
@@ -131,6 +193,9 @@ func walk(v Visitor, n Node) {
 			}
 		}
 	case *ConditionalExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Test)
@@ -138,6 +203,9 @@ func walk(v Visitor, n Node) {
 			walk(w, n.Consequent)
 		}
 	case *ArrayExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			for _, e := range n.Elements {
@@ -145,6 +213,9 @@ func walk(v Visitor, n Node) {
 			}
 		}
 	case *FunctionExpression:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			for _, e := range n.Params {
@@ -153,34 +224,68 @@ func walk(v Visitor, n Node) {
 			walk(w, n.Body)
 		}
 	case *Property:
+		if n == nil {
+			return
+		}
 		w := v.Visit(n)
 		if w != nil {
 			walk(w, n.Key)
 			walk(w, n.Value)
 		}
 	case *Identifier:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *PipeLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *StringLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *BooleanLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *FloatLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *IntegerLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *UnsignedIntegerLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *RegexpLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *DurationLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	case *DateTimeLiteral:
+		if n == nil {
+			return
+		}
 		v.Visit(n)
 	default:
 		panic(fmt.Errorf("walk not defined for node %T", n))
 	}
 
-	v.Done(n)
+	// Cannot use defer here as we need to check for n == nil which needs to happen in each case.
+	v.Done(node)
 }
