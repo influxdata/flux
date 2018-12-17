7d9ce0a249b4f3337b6b85b1d1ab6a12b4e77624
Jonathan A. Sternberg <jonathan@influxdata.com>
2018-12-17 17:16:33 -0600

    feat(arrow): add utility methods for converting a slice into an arrow array buffer (#552)
    This adds utility methods for converting a slice into an arrow array
    buffer. These implementations were previously inside of the
    `ColListTableBuilder` and have now been exposed for other
    implementation to use.
    
    This also updates the `executetest` package to implement `DoArrow`. This
    was previously missed and would cause any test that used arrow buffers
    to fail with a panic.

--- arrow/bool.go
+++ arrow/bool.go
@@ -6,6 +6,17 @@ import (
 	"github.com/influxdata/flux/memory"
 )
 
+func NewBool(vs []bool, alloc *memory.Allocator) *array.Boolean {
+	b := NewBoolBuilder(alloc)
+	b.Reserve(len(vs))
+	for _, v := range vs {
+		b.UnsafeAppend(v)
+	}
+	a := b.NewBooleanArray()
+	b.Release()
+	return a
+}
+
 func NewBoolBuilder(a *memory.Allocator) *array.BooleanBuilder {
 	return array.NewBooleanBuilder(&allocator{
 		Allocator: arrowmemory.NewGoAllocator(),
--- arrow/uint.go
+++ arrow/uint.go
@@ -6,6 +6,17 @@ import (
 	"github.com/influxdata/flux/memory"
 )
 
+func NewUint(vs []uint64, alloc *memory.Allocator) *array.Uint64 {
+	b := NewUintBuilder(alloc)
+	b.Reserve(len(vs))
+	for _, v := range vs {
+		b.UnsafeAppend(v)
+	}
+	a := b.NewUint64Array()
+	b.Release()
+	return a
+}
+
 func NewUintBuilder(a *memory.Allocator) *array.Uint64Builder {
 	return array.NewUint64Builder(&allocator{
 		Allocator: arrowmemory.NewGoAllocator(),
