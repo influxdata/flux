d8d948f6774c188ba3dcd15b7a9cf372104600a1
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-01-09 09:29:10 -0600

    refactor(functions): support null values in the sum aggregate (#689)

--- functions/transformations/sum_test.go
+++ functions/transformations/sum_test.go
@@ -3,6 +3,7 @@ package transformations_test
 import (
 	"testing"
 
+	"github.com/apache/arrow/go/arrow/array"
 	"github.com/influxdata/flux"
 	"github.com/influxdata/flux/arrow"
 	"github.com/influxdata/flux/execute/executetest"
@@ -22,11 +23,54 @@ func TestSumOperation_Marshaling(t *testing.T) {
 }
 
 func TestSum_Process(t *testing.T) {
-	executetest.AggFuncTestHelper(t,
-		new(transformations.SumAgg),
-		arrow.NewFloat([]float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, nil),
-		float64(45),
-	)
+	testCases := []struct {
+		name string
+		data func() *array.Float64
+		want float64
+	}{
+		{
+			name: "zero",
+			data: func() *array.Float64 {
+				return arrow.NewFloat([]float64{0, 0, 0}, nil)
+			},
+			want: 0.0,
+		},
+		{
+			name: "nonzero",
+			data: func() *array.Float64 {
+				return arrow.NewFloat([]float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, nil)
+			},
+			want: 45,
+		},
+		{
+			name: "with nulls",
+			data: func() *array.Float64 {
+				b := arrow.NewFloatBuilder(nil)
+				defer b.Release()
+				b.AppendValues([]float64{0, 1, 2, 3}, nil)
+				b.AppendNull()
+				b.AppendValues([]float64{5, 6}, nil)
+				b.AppendNull()
+				b.AppendValues([]float64{8, 9}, nil)
+				return b.NewFloat64Array()
+			},
+			want: 34,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			data := tc.data()
+			defer data.Release()
+
+			executetest.AggFuncTestHelper(
+				t,
+				new(transformations.SumAgg),
+				data,
+				tc.want,
+			)
+		})
+	}
 }
 
 func BenchmarkSum(b *testing.B) {
