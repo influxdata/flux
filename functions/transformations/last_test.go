e6db3aa6e0b3cbf857209453c97a7dc5208f466e
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-01-09 09:28:45 -0600

    refactor(functions): make the last transformation nil-aware (#687)
    Also merging the two tests for first into the same test function with
    subtests. It was a mistake for those to be separate to begin with.

--- functions/transformations/last_test.go
+++ functions/transformations/last_test.go
@@ -57,6 +57,33 @@ func TestLast_Process(t *testing.T) {
 				Values: []interface{}{execute.Time(90), 7.0, "a", "x"},
 			}},
 		},
+		{
+			name: "with null",
+			data: &executetest.Table{
+				KeyCols: []string{"t1"},
+				ColMeta: []flux.ColMeta{
+					{Label: "_time", Type: flux.TTime},
+					{Label: "_value", Type: flux.TFloat},
+					{Label: "t1", Type: flux.TString},
+					{Label: "t2", Type: flux.TString},
+				},
+				Data: [][]interface{}{
+					{execute.Time(0), 0.0, "a", "y"},
+					{execute.Time(10), 5.0, "a", "x"},
+					{execute.Time(20), 9.0, "a", "y"},
+					{execute.Time(30), 4.0, "a", "x"},
+					{execute.Time(40), 6.0, "a", "y"},
+					{execute.Time(50), 8.0, "a", "x"},
+					{execute.Time(60), 1.0, "a", "y"},
+					{execute.Time(70), 2.0, "a", "x"},
+					{execute.Time(80), 3.0, "a", "y"},
+					{execute.Time(90), nil, "a", "x"},
+				},
+			},
+			want: []execute.Row{{
+				Values: []interface{}{execute.Time(80), 3.0, "a", "y"},
+			}},
+		},
 	}
 	for _, tc := range testCases {
 		tc := tc
