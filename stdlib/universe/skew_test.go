93836638780df4e90280e6e3191ca9cb31208b28
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-01-14 16:43:36 -0600

    refactor(stdlib): add null support to skew

--- stdlib/universe/skew_test.go
+++ stdlib/universe/skew_test.go
@@ -4,6 +4,7 @@ import (
 	"math"
 	"testing"
 
+	"github.com/apache/arrow/go/arrow/array"
 	"github.com/influxdata/flux"
 	"github.com/influxdata/flux/arrow"
 	"github.com/influxdata/flux/execute/executetest"
@@ -25,37 +26,74 @@ func TestSkewOperation_Marshaling(t *testing.T) {
 func TestSkew_Process(t *testing.T) {
 	testCases := []struct {
 		name string
-		data []float64
+		data func() *array.Float64
 		want interface{}
 	}{
 		{
 			name: "zero",
-			data: []float64{1, 2, 3},
+			data: func() *array.Float64 {
+				return arrow.NewFloat([]float64{1, 2, 3}, nil)
+			},
 			want: 0.0,
 		},
 		{
 			name: "nonzero",
-			data: []float64{2, 2, 3},
+			data: func() *array.Float64 {
+				return arrow.NewFloat([]float64{2, 2, 3}, nil)
+			},
 			want: 0.7071067811865475,
 		},
 		{
-			name: "nonzero",
-			data: []float64{2, 2, 3, 4},
+			name: "nonzero 2",
+			data: func() *array.Float64 {
+				return arrow.NewFloat([]float64{2, 2, 3, 4}, nil)
+			},
 			want: 0.49338220021815854,
 		},
 		{
 			name: "NaN short",
-			data: []float64{1},
+			data: func() *array.Float64 {
+				return arrow.NewFloat([]float64{1}, nil)
+			},
 			want: math.NaN(),
 		},
 		{
 			name: "NaN divide by zero",
-			data: []float64{1, 1, 1},
+			data: func() *array.Float64 {
+				return arrow.NewFloat([]float64{1, 1, 1}, nil)
+			},
 			want: math.NaN(),
 		},
 		{
 			name: "empty",
-			data: []float64{},
+			data: func() *array.Float64 {
+				return arrow.NewFloat(nil, nil)
+			},
+			want: nil,
+		},
+		{
+			name: "with nulls",
+			data: func() *array.Float64 {
+				b := arrow.NewFloatBuilder(nil)
+				defer b.Release()
+				b.Append(2)
+				b.AppendNull()
+				b.Append(2)
+				b.AppendNull()
+				b.Append(3)
+				return b.NewFloat64Array()
+			},
+			want: 0.7071067811865475,
+		},
+		{
+			name: "only nulls",
+			data: func() *array.Float64 {
+				b := arrow.NewFloatBuilder(nil)
+				defer b.Release()
+				b.AppendNull()
+				b.AppendNull()
+				return b.NewFloat64Array()
+			},
 			want: nil,
 		},
 	}
@@ -65,7 +103,7 @@ func TestSkew_Process(t *testing.T) {
 			executetest.AggFuncTestHelper(
 				t,
 				new(universe.SkewAgg),
-				arrow.NewFloat(tc.data, nil),
+				tc.data(),
 				tc.want,
 			)
 		})
