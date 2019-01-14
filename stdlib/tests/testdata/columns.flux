32c1e00970b64f139492e3759356b81d199ee92e
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2019-01-14 21:43:13 +0100

    feat(functions): columns function (#640)

---
+++ stdlib/tests/testdata/columns.flux
@@ -0,0 +1,50 @@
+inData = "
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,long,string,string,string,string
+#group,false,false,false,false,false,false,false,false,true,true
+#default,_result,,,,,,,,,
+,result,table,_start,_stop,_time,_value,_field,_measurement,host,name
+,,1,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:26Z,15204688,io_time,diskio,host.local,disk0
+,,1,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:36Z,15204894,io_time,diskio,host.local,disk0
+,,1,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:46Z,15205102,io_time,diskio,host.local,disk0
+,,1,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:56Z,15205226,io_time,diskio,host.local,disk0
+,,1,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:54:06Z,15205499,io_time,diskio,host.local,disk0
+,,1,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:54:16Z,15205755,io_time,diskio,host.local,disk0
+,,10,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:26Z,648,io_time,diskio,host.local,disk2
+,,10,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:36Z,648,io_time,diskio,host.local,disk2
+,,10,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:46Z,648,io_time,diskio,host.local,disk2
+,,10,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:53:56Z,648,io_time,diskio,host.local,disk2
+,,10,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:54:06Z,648,io_time,diskio,host.local,disk2
+,,10,2018-05-22T19:53:26Z,2018-05-22T19:54:16Z,2018-05-22T19:54:16Z,648,io_time,diskio,host.local,disk2
+"
+outData = "
+#datatype,string,long,string,string,string
+#group,false,false,true,true,false
+#default,,,,,
+,result,table,host,name,_value
+,,0,host.local,disk0,_start
+,,0,host.local,disk0,_stop
+,,0,host.local,disk0,_time
+,,0,host.local,disk0,_value
+,,0,host.local,disk0,_field
+,,0,host.local,disk0,_measurement
+,,0,host.local,disk0,host
+,,0,host.local,disk0,name
+,,1,host.local,disk2,_start
+,,1,host.local,disk2,_stop
+,,1,host.local,disk2,_time
+,,1,host.local,disk2,_value
+,,1,host.local,disk2,_field
+,,1,host.local,disk2,_measurement
+,,1,host.local,disk2,host
+,,1,host.local,disk2,name
+"
+
+t_columns = (table=<-) =>
+  table
+  |> range(start: 2018-05-20T19:53:26Z)
+  |> columns()
+
+testingTest(name: "columns",
+            input: testLoadStorage(csv: inData),
+            want: testLoadMem(csv: outData),
+            test: t_columns)
---
+++ stdlib/universe/columns.go
@@ -0,0 +1,162 @@
+package universe
+
+import (
+	"fmt"
+
+	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/arrow"
+	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/plan"
+	"github.com/influxdata/flux/semantic"
+)
+
+const ColumnsKind = "columns"
+
+type ColumnsOpSpec struct {
+	Column string `json:"column"`
+}
+
+func init() {
+	columnsSignature := flux.FunctionSignature(map[string]semantic.PolyType{
+		"column": semantic.String,
+	}, nil)
+
+	flux.RegisterPackageValue("universe", ColumnsKind, flux.FunctionValue(ColumnsKind, createColumnsOpSpec, columnsSignature))
+	flux.RegisterOpSpec(ColumnsKind, newColumnsOp)
+	plan.RegisterProcedureSpec(ColumnsKind, newColumnsProcedure, ColumnsKind)
+	execute.RegisterTransformation(ColumnsKind, createColumnsTransformation)
+}
+
+func createColumnsOpSpec(args flux.Arguments, a *flux.Administration) (flux.OperationSpec, error) {
+	if err := a.AddParentFromArgs(args); err != nil {
+		return nil, err
+	}
+
+	spec := new(ColumnsOpSpec)
+
+	if col, found, err := args.GetString("column"); err != nil {
+		return nil, err
+	} else if found {
+		spec.Column = col
+	} else {
+		spec.Column = execute.DefaultValueColLabel
+	}
+
+	return spec, nil
+}
+
+func newColumnsOp() flux.OperationSpec {
+	return new(ColumnsOpSpec)
+}
+
+func (s *ColumnsOpSpec) Kind() flux.OperationKind {
+	return ColumnsKind
+}
+
+type ColumnsProcedureSpec struct {
+	plan.DefaultCost
+	Column string
+}
+
+func newColumnsProcedure(qs flux.OperationSpec, pa plan.Administration) (plan.ProcedureSpec, error) {
+	spec, ok := qs.(*ColumnsOpSpec)
+	if !ok {
+		return nil, fmt.Errorf("invalid spec type %T", qs)
+	}
+
+	return &ColumnsProcedureSpec{
+		Column: spec.Column,
+	}, nil
+}
+
+func (s *ColumnsProcedureSpec) Kind() plan.ProcedureKind {
+	return ColumnsKind
+}
+
+func (s *ColumnsProcedureSpec) Copy() plan.ProcedureSpec {
+	ns := new(ColumnsProcedureSpec)
+	*ns = *s
+	return ns
+}
+
+func createColumnsTransformation(id execute.DatasetID, mode execute.AccumulationMode, spec plan.ProcedureSpec, a execute.Administration) (execute.Transformation, execute.Dataset, error) {
+	s, ok := spec.(*ColumnsProcedureSpec)
+	if !ok {
+		return nil, nil, fmt.Errorf("invalid spec type %T", spec)
+	}
+	cache := execute.NewTableBuilderCache(a.Allocator())
+	d := execute.NewDataset(id, mode, cache)
+	t := NewColumnsTransformation(d, cache, s)
+	return t, d, nil
+}
+
+type columnsTransformation struct {
+	d     execute.Dataset
+	cache execute.TableBuilderCache
+
+	column string
+}
+
+func NewColumnsTransformation(d execute.Dataset, cache execute.TableBuilderCache, spec *ColumnsProcedureSpec) *columnsTransformation {
+	return &columnsTransformation{
+		d:      d,
+		cache:  cache,
+		column: spec.Column,
+	}
+}
+
+func (t *columnsTransformation) RetractTable(id execute.DatasetID, key flux.GroupKey) error {
+	return t.d.RetractTable(key)
+}
+
+func (t *columnsTransformation) Process(id execute.DatasetID, tbl flux.Table) error {
+	builder, created := t.cache.TableBuilder(tbl.Key())
+	if !created {
+		return fmt.Errorf("columns found duplicate table with key: %v", tbl.Key())
+	}
+
+	labels := make([]string, len(tbl.Cols()))
+	for i, c := range tbl.Cols() {
+		labels[i] = c.Label
+	}
+
+	// Add the group key columns to this table.
+	if err := execute.AddTableKeyCols(tbl.Key(), builder); err != nil {
+		return err
+	}
+
+	// Create a new column for the column names.
+	colIdx, err := builder.AddCol(flux.ColMeta{Label: t.column, Type: flux.TString})
+	if err != nil {
+		return err
+	}
+
+	// Append the key values repeatedly to the table.
+	for i := 0; i < len(labels); i++ {
+		if err := execute.AppendKeyValues(tbl.Key(), builder); err != nil {
+			return err
+		}
+	}
+
+	// Append the labels to the column index.
+	labelsArrow := arrow.NewString(labels, nil)
+	defer labelsArrow.Release()
+	if err := builder.AppendStrings(colIdx, labelsArrow); err != nil {
+		return err
+	}
+
+	// TODO: call Do at least once to ensure that the iterators work properly
+	return tbl.Do(func(flux.ColReader) error {
+		return nil
+	})
+}
+
+func (t *columnsTransformation) UpdateWatermark(id execute.DatasetID, mark execute.Time) error {
+	return t.d.UpdateWatermark(mark)
+}
+func (t *columnsTransformation) UpdateProcessingTime(id execute.DatasetID, pt execute.Time) error {
+	return t.d.UpdateProcessingTime(pt)
+}
+func (t *columnsTransformation) Finish(id execute.DatasetID, err error) {
+	t.d.Finish(err)
+}
