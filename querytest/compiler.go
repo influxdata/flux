78fcd1febcfaca7e0bca0b8edcd438bf244d41a0
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2019-01-23 09:05:40 -0800

    refactor(influxdb): remove from implementation to move it to influxdb

--- querytest/compiler.go
+++ querytest/compiler.go
@@ -4,57 +4,35 @@ import (
 	"context"
 
 	"github.com/influxdata/flux"
-	"github.com/influxdata/flux/stdlib/csv"
-	"github.com/influxdata/flux/stdlib/influxdata/influxdb"
-	"github.com/influxdata/flux/stdlib/influxdata/influxdb/v1"
 )
 
-// FromCSVCompiler wraps a compiler and replaces all From operations with FromCSV
-type FromCSVCompiler struct {
+// ReplaceOpInSpecFunction is a function that takes an Operation and returns a new OperationSpec for substitution,
+// or nil, if nothing has to be changed.
+type ReplaceOpInSpecFunction func(op *flux.Operation) flux.OperationSpec
+
+type ReplaceSpecCompiler struct {
 	flux.Compiler
-	InputFile string
+	fn ReplaceOpInSpecFunction
 }
 
-// FromInfluxJSONCompiler wraps a compiler and replaces all From operations with FromJSON
-type FromInfluxJSONCompiler struct {
-	flux.Compiler
-	InputFile string
+func NewReplaceSpecCompiler(compiler flux.Compiler, fn ReplaceOpInSpecFunction) *ReplaceSpecCompiler {
+	return &ReplaceSpecCompiler{Compiler: compiler, fn: fn}
 }
 
-func (c FromCSVCompiler) Compile(ctx context.Context) (*flux.Spec, error) {
+func (c *ReplaceSpecCompiler) Compile(ctx context.Context) (*flux.Spec, error) {
 	spec, err := c.Compiler.Compile(ctx)
 	if err != nil {
 		return nil, err
 	}
-	ReplaceFromSpec(spec, c.InputFile)
+	ReplaceOpInSpec(spec, c.fn)
 	return spec, nil
 }
 
-func (c FromInfluxJSONCompiler) Compile(ctx context.Context) (*flux.Spec, error) {
-	spec, err := c.Compiler.Compile(ctx)
-	if err != nil {
-		return nil, err
-	}
-	ReplaceFromWithFromInfluxJSONSpec(spec, c.InputFile)
-	return spec, nil
-}
-
-func ReplaceFromSpec(q *flux.Spec, csvSrc string) {
+func ReplaceOpInSpec(q *flux.Spec, replaceFn ReplaceOpInSpecFunction) {
 	for _, op := range q.Operations {
-		if op.Spec.Kind() == influxdb.FromKind {
-			op.Spec = &csv.FromCSVOpSpec{
-				File: csvSrc,
-			}
-		}
-	}
-}
-
-func ReplaceFromWithFromInfluxJSONSpec(q *flux.Spec, jsonSrc string) {
-	for _, op := range q.Operations {
-		if op.Spec.Kind() == influxdb.FromKind {
-			op.Spec = &v1.FromInfluxJSONOpSpec{
-				File: jsonSrc,
-			}
+		newOpSpec := replaceFn(op)
+		if newOpSpec != nil {
+			op.Spec = newOpSpec
 		}
 	}
 }
