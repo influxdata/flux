90ce58c56b50fc000abf8770290fbda9d1108a38
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-04-11 10:09:10 -0500

    fix: require a github token to perform a release (#1150)
    The release would fail when attempting to upload release notes if a
    github token wasn't in the environment. This will check the environment
    before running the commands so we don't have to back out a tag midway
    through.

--- release.sh
+++ release.sh
@@ -5,6 +5,12 @@ cd $DIR
 
 set -e
 
+# Ensure that the GITHUB_TOKEN is exposed in the environment.
+if ! env | grep GITHUB_TOKEN= > /dev/null; then
+  echo "GITHUB_TOKEN must be exported in the environment to perform a release." 2>&1
+  exit 1
+fi
+
 export GO111MODULE=on
 
 version=$(go run ./internal/cmd/changelog nextver)
