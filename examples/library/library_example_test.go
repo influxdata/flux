eb409af9157154b846ab06b6c974052cb57ea6df
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2019-04-11 16:54:41 +0200

    fix(examples): change example name to make go vet pass (#1151)

--- examples/library/library_example_test.go
+++ examples/library/library_example_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/influxdata/flux/values"
 )
 
-func ExampleLibrary_fromGenerator() {
+func Example_fromGenerator() {
 	t := `import g "generate"
 g.from(start: 1993-02-16T00:00:00Z, stop: 1993-02-16T00:03:00Z, count: 5, fn: (n) => 1)`
 
