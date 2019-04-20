3e951eb3f616700d9e7fa23b9ff8df49df5579c6
Julius Volz <julius.volz@gmail.com>
2019-04-20 13:25:11 +0200

    feat(stdlib/strings): Add trim{Suffix,Prefix} functions
    Also, use the same hack as in the math package to register the package
    globally, but make it look like you can access its functions through the
    package name as well. This is needed to use the functions from e.g. a `map`
    or `rename` custom function.
    
    Not sure how useful the current tests for the string functions are, as
    they mainly test the underlying Go functions, but I added similar ones for
    the new string functions.
    
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- stdlib/strings/strings.go
+++ stdlib/strings/strings.go
@@ -9,7 +9,14 @@ import (
 	"github.com/influxdata/flux/values"
 )
 
-func generateStringFunction(name string, stringFn func(string) string) values.Function {
+const (
+	stringArg = "v"
+	cutset    = "cutset"
+	prefix    = "prefix"
+	suffix    = "suffix"
+)
+
+func generateSingleArgStringFunction(name string, stringFn func(string) string) values.Function {
 	return values.NewFunction(
 		name,
 		semantic.NewFunctionPolyType(semantic.FunctionPolySignature{
@@ -22,7 +29,7 @@ func generateStringFunction(name string, stringFn func(string) string) values.Fu
 
 			v, ok := args.Get(stringArg)
 			if !ok {
-				return nil, errMissingV
+				return nil, fmt.Errorf("missing argument %q", stringArg)
 			}
 
 			if v.Type().Nature() == semantic.String {
@@ -37,10 +44,49 @@ func generateStringFunction(name string, stringFn func(string) string) values.Fu
 	)
 }
 
+func generateDualArgStringFunction(name string, argNames []string, stringFn func(string, string) string) values.Function {
+	if len(argNames) != 2 {
+		panic("unexpected number of argument names")
+	}
+
+	return values.NewFunction(
+		name,
+		semantic.NewFunctionPolyType(semantic.FunctionPolySignature{
+			Parameters: map[string]semantic.PolyType{
+				argNames[0]: semantic.String,
+				argNames[1]: semantic.String,
+			},
+			Required: semantic.LabelSet{argNames[0], argNames[1]},
+			Return:   semantic.String,
+		}),
+		func(args values.Object) (values.Value, error) {
+			var argVals = make([]values.Value, 2)
+
+			for i, name := range argNames {
+				val, ok := args.Get(name)
+				if !ok {
+					return nil, fmt.Errorf("missing argument %q", name)
+				}
+
+				if val.Type().Nature() != semantic.String {
+					return nil, fmt.Errorf("expected argument %q to be of type %v, got type %v", name, semantic.String, val.Type().Nature())
+				}
+
+				argVals[i] = val
+			}
+
+			return values.NewString(stringFn(argVals[0].Str(), argVals[1].Str())), nil
+		},
+		false,
+	)
+}
+
 func init() {
-	flux.RegisterPackageValue("strings", "trim", generateMultiArgStringFunction("trim", strings.Trim))
-	flux.RegisterPackageValue("strings", "trimSpace", generateStringFunction("trimSpace", strings.TrimSpace))
-	flux.RegisterPackageValue("strings", "title", generateStringFunction("title", strings.Title))
-	flux.RegisterPackageValue("strings", "toUpper", generateStringFunction("toUpper", strings.ToUpper))
-	flux.RegisterPackageValue("strings", "toLower", generateStringFunction("toLower", strings.ToLower))
+	flux.RegisterPackageValue("strings", "trim", generateDualArgStringFunction("trim", []string{stringArg, cutset}, strings.Trim))
+	flux.RegisterPackageValue("strings", "trimSpace", generateSingleArgStringFunction("trimSpace", strings.TrimSpace))
+	flux.RegisterPackageValue("strings", "trimPrefix", generateDualArgStringFunction("trimSuffix", []string{stringArg, prefix}, strings.TrimPrefix))
+	flux.RegisterPackageValue("strings", "trimSuffix", generateDualArgStringFunction("trimSuffix", []string{stringArg, suffix}, strings.TrimSuffix))
+	flux.RegisterPackageValue("strings", "title", generateSingleArgStringFunction("title", strings.Title))
+	flux.RegisterPackageValue("strings", "toUpper", generateSingleArgStringFunction("toUpper", strings.ToUpper))
+	flux.RegisterPackageValue("strings", "toLower", generateSingleArgStringFunction("toLower", strings.ToLower))
 }
--- stdlib/strings/strings_test.go
+++ stdlib/strings/strings_test.go
@@ -24,7 +24,7 @@ func TestTrim(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			trim := generateMultiArgStringFunction("trim", strings.Trim)
+			trim := generateDualArgStringFunction("trim", []string{stringArg, cutset}, strings.Trim)
 			testCase := values.NewObjectWithValues(map[string]values.Value{"v": values.NewString(tc.v), "cutset": values.NewString(tc.cutset)})
 			result, err := trim.Call(testCase)
 			res := result.Str()
@@ -41,6 +41,85 @@ func TestTrim(t *testing.T) {
 	}
 }
 
+func TestTrimPrefix(t *testing.T) {
+	testCases := []struct {
+		name   string
+		v      string
+		prefix string
+		want   string
+	}{
+		{
+			name:   "String with prefix",
+			v:      "prefix_test",
+			prefix: "prefix",
+			want:   "_test",
+		},
+		{
+			name:   "String without prefix",
+			v:      "prefi_test",
+			prefix: "prefix",
+			want:   "prefi_test",
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			trimPrefix := generateDualArgStringFunction("trimPrefix", []string{stringArg, prefix}, strings.TrimPrefix)
+			testCase := values.NewObjectWithValues(map[string]values.Value{"v": values.NewString(tc.v), "prefix": values.NewString(tc.prefix)})
+			result, err := trimPrefix.Call(testCase)
+			res := result.Str()
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if res != tc.want {
+				t.Errorf("string function result %s expected: %s, got: %s", tc.name, tc.want, result)
+			}
+		})
+
+	}
+}
+
+func TestTrimSuffix(t *testing.T) {
+	testCases := []struct {
+		name   string
+		v      string
+		suffix string
+		want   string
+	}{
+		{
+			name:   "String with suffix",
+			v:      "test_suffix",
+			suffix: "suffix",
+			want:   "test_",
+		},
+		{
+			name:   "String without suffix",
+			v:      "test_suffi",
+			suffix: "suffix",
+			want:   "test_suffi",
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			trimSuffix := generateDualArgStringFunction("trimSuffix", []string{stringArg, suffix}, strings.TrimSuffix)
+			testCase := values.NewObjectWithValues(map[string]values.Value{"v": values.NewString(tc.v), "suffix": values.NewString(tc.suffix)})
+			result, err := trimSuffix.Call(testCase)
+			res := result.Str()
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if res != tc.want {
+				t.Errorf("string function result %s expected: %s, got: %s", tc.name, tc.want, result)
+			}
+		})
+
+	}
+}
 func TestTrimSpace(t *testing.T) {
 	testCases := []struct {
 		name string
@@ -56,7 +135,7 @@ func TestTrimSpace(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			trimSpace := generateStringFunction("trimSpace", strings.TrimSpace)
+			trimSpace := generateSingleArgStringFunction("trimSpace", strings.TrimSpace)
 			testCase := values.NewObjectWithValues(map[string]values.Value{"v": values.NewString(tc.v)})
 			result, err := trimSpace.Call(testCase)
 			res := result.Str()
@@ -88,7 +167,7 @@ func TestTitle(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			title := generateStringFunction("title", strings.Title)
+			title := generateSingleArgStringFunction("title", strings.Title)
 			testCase := values.NewObjectWithValues(map[string]values.Value{"v": values.NewString(tc.v)})
 			result, err := title.Call(testCase)
 			res := result.Str()
@@ -120,7 +199,7 @@ func TestToUpper(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			toUpper := generateStringFunction("toUpper", strings.ToUpper)
+			toUpper := generateSingleArgStringFunction("toUpper", strings.ToUpper)
 			testCase := values.NewObjectWithValues(map[string]values.Value{"v": values.NewString(tc.v)})
 			result, err := toUpper.Call(testCase)
 			res := result.Str()
@@ -152,7 +231,7 @@ func TestToLower(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			toLower := generateStringFunction("toLower", strings.ToLower)
+			toLower := generateSingleArgStringFunction("toLower", strings.ToLower)
 			testCase := values.NewObjectWithValues(map[string]values.Value{"v": values.NewString(tc.v)})
 			result, err := toLower.Call(testCase)
 			res := result.Str()
