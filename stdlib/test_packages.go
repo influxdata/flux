4c61c18184a1c2eb5020822a0291a92844be852e
jlapacik <joshua.lapacik@pacbell.net>
2019-05-06 17:21:56 -0700

    test(influxdb/v1): chronograf meta query flux tests

--- stdlib/test_packages.go
+++ stdlib/test_packages.go
@@ -4,11 +4,13 @@ package stdlib
 
 import (
 	ast "github.com/influxdata/flux/ast"
+	fluxtests "github.com/influxdata/flux/stdlib/influxdata/influxdb/v1/fluxtests"
 	testdata "github.com/influxdata/flux/stdlib/testing/testdata"
 )
 
 var FluxTestPackages = func() []*ast.Package {
 	var pkgs []*ast.Package
+	pkgs = append(pkgs, fluxtests.FluxTestPackages...)
 	pkgs = append(pkgs, testdata.FluxTestPackages...)
 	return pkgs
 }()
