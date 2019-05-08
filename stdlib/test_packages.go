5a63e424ac428c7e0cae7392a7ad0924d9044dc7
jlapacik <joshua.lapacik@pacbell.net>
2019-05-08 13:30:53 -0700

    test(stdlib):chronograf aggregate window flux tests

--- stdlib/test_packages.go
+++ stdlib/test_packages.go
@@ -5,12 +5,14 @@ package stdlib
 import (
 	ast "github.com/influxdata/flux/ast"
 	fluxtests "github.com/influxdata/flux/stdlib/influxdata/influxdb/v1/fluxtests"
+	chronograf "github.com/influxdata/flux/stdlib/testing/chronograf"
 	testdata "github.com/influxdata/flux/stdlib/testing/testdata"
 )
 
 var FluxTestPackages = func() []*ast.Package {
 	var pkgs []*ast.Package
 	pkgs = append(pkgs, fluxtests.FluxTestPackages...)
+	pkgs = append(pkgs, chronograf.FluxTestPackages...)
 	pkgs = append(pkgs, testdata.FluxTestPackages...)
 	return pkgs
 }()
