80eeede299b1b00b1156b991d866aa5beafe56fc
Julius Volz <julius.volz@gmail.com>
2019-05-11 18:44:02 +0200

    Rename PromQL transpiler package from "main"->"promflux"
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- promflux/escape.go
+++ promflux/escape.go
@@ -1,4 +1,4 @@
-package main
+package promflux
 
 import (
 	"github.com/prometheus/prometheus/pkg/labels"
--- promflux/subquery_expr.go
+++ promflux/subquery_expr.go
@@ -1,4 +1,4 @@
-package main
+package promflux
 
 import (
 	"github.com/influxdata/flux/ast"
