f68482faadd1a0c71b24f3a9a0169b29ae9f8f38
Julius Volz <julius.volz@gmail.com>
2019-05-11 18:47:20 +0200

    Subquery naming cleanups
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- promflux/subquery_expr.go
+++ promflux/subquery_expr.go
@@ -5,17 +5,17 @@ import (
 	"github.com/prometheus/prometheus/promql"
 )
 
-func (t *transpiler) transpileSubqueryExpr(a *promql.SubqueryExpr) (ast.Expression, error) {
+func (t *transpiler) transpileSubqueryExpr(sq *promql.SubqueryExpr) (ast.Expression, error) {
 	// 1. Create new transpiler with boundaries and step of subquery.
 	sqt := &transpiler{
 		bucket:     t.bucket,
-		start:      t.start.Add(-a.Range - a.Offset),
-		end:        t.end.Add(-a.Offset),
-		resolution: a.Step,
+		start:      t.start.Add(-sq.Range - sq.Offset),
+		end:        t.end.Add(-sq.Offset),
+		resolution: sq.Step,
 	}
 
 	// 2. Transpile subexpression with that transpiler.
-	subquery, err := sqt.transpileExpr(a.Expr)
+	subquery, err := sqt.transpileExpr(sq.Expr)
 	if err != nil {
 		return nil, err
 	}
@@ -31,13 +31,13 @@ func (t *transpiler) transpileSubqueryExpr(a *promql.SubqueryExpr) (ast.Expressi
 		// At every resolution step, include the specified range of data.
 		call("window", map[string]ast.Expression{
 			"every":  &ast.DurationLiteral{Values: []ast.Duration{{Magnitude: t.resolution.Nanoseconds(), Unit: "ns"}}},
-			"period": &ast.DurationLiteral{Values: []ast.Duration{{Magnitude: a.Range.Nanoseconds(), Unit: "ns"}}},
+			"period": &ast.DurationLiteral{Values: []ast.Duration{{Magnitude: sq.Range.Nanoseconds(), Unit: "ns"}}},
 		}),
 		// Remove any windows smaller than the specified range at the edges of the graph range.
-		call("filter", map[string]ast.Expression{"fn": windowCutoffFn(t.start.Add(-a.Offset), t.end.Add(-a.Range-a.Offset))}),
+		call("filter", map[string]ast.Expression{"fn": windowCutoffFn(t.start.Add(-sq.Offset), t.end.Add(-sq.Range-sq.Offset))}),
 		// Apply offsets to make past data look like it's in the present.
 		call("timeShift", map[string]ast.Expression{
-			"duration": &ast.DurationLiteral{Values: []ast.Duration{{Magnitude: a.Offset.Nanoseconds(), Unit: "ns"}}},
+			"duration": &ast.DurationLiteral{Values: []ast.Duration{{Magnitude: sq.Offset.Nanoseconds(), Unit: "ns"}}},
 		}),
 	), nil
 }
