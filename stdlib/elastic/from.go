f724702bd9c7893ce53ba09a392a7c8c0ccf970e
i.selivanov <i.selivanov@localhost.localdomain>
2019-05-23 13:27:15 +0300

    feat(elastic): QueryDSL implement, but Decode not complete

--- stdlib/elastic/from.go
+++ stdlib/elastic/from.go
@@ -1,15 +1,7 @@
 package elastic
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"reflect"
-	"time"
-
-	"github.com/olivere/elastic"
-	"github.com/olivere/elastic/config"
-
 	"github.com/influxdata/flux"
 	"github.com/influxdata/flux/execute"
 	"github.com/influxdata/flux/plan"
@@ -20,44 +12,60 @@ import (
 const FromKind = "elastic.from"
 
 type FromOpSpec struct {
-	DataSourceName string `json:"dataSourceName,omitempty"`
-	Query          string `json:"query,omitempty"`
+	URL      string        `json:"url,omitempty"`
+	QueryDSL values.Object `json:"queryDSL,omitempty"`
+	User     string        `json:"user,omitempty"`
+	Password string        `json:"password,omitempty"`
 }
 
 func init() {
 	fromSignature := semantic.FunctionPolySignature{
 		Parameters: map[string]semantic.PolyType{
-			"dataSourceName": semantic.String,
-			"query":          semantic.String,
+			"url":      semantic.String,
+			"queryDSL": semantic.Object,
+			"user":     semantic.String,
+			"password": semantic.String,
 		},
-		Required: semantic.LabelSet{"dataSourceName", "query"},
+		Required: semantic.LabelSet{"queryDSL"},
 		Return:   flux.TableObjectType,
 	}
 	flux.RegisterPackageValue("elastic", "from", flux.FunctionValue(FromKind, createFromOpSpec, fromSignature))
-	flux.RegisterOpSpec(FromKind, newFromOp)
-	plan.RegisterProcedureSpec(FromKind, newFromProcedure, FromKind)
+	flux.RegisterOpSpec(FromKind, newFromOpSpec)
+	plan.RegisterProcedureSpec(FromKind, newFromProcedureSpec, FromKind)
 	execute.RegisterSource(FromKind, createFromSource)
 }
 
-func createFromOpSpec(args flux.Arguments, administration *flux.Administration) (flux.OperationSpec, error) {
+func createFromOpSpec(args flux.Arguments, _ *flux.Administration) (flux.OperationSpec, error) {
 	spec := new(FromOpSpec)
 
-	if dataSourceName, err := args.GetRequiredString("dataSourceName"); err != nil {
+	if url, _, err := args.GetString("url"); err != nil {
 		return nil, err
 	} else {
-		spec.DataSourceName = dataSourceName
+		spec.URL = url
 	}
 
-	if query, err := args.GetRequiredString("query"); err != nil {
+	if query, err := args.GetRequiredObject("queryDSL"); err != nil {
 		return nil, err
 	} else {
-		spec.Query = query
+		spec.QueryDSL = query
+	}
+
+	if user, _, err := args.GetString("user"); err != nil {
+		return nil, err
+	} else {
+		spec.User = user
+	}
+
+	if password, _, err := args.GetString("password"); err != nil {
+		return nil, err
+	} else {
+		spec.Password = password
 	}
 
 	return spec, nil
 }
 
-func newFromOp() flux.OperationSpec {
+func newFromOpSpec() flux.OperationSpec {
 	return new(FromOpSpec)
 }
 
@@ -67,19 +75,23 @@ func (s *FromOpSpec) Kind() flux.OperationKind {
 
 type FromProcedureSpec struct {
 	plan.DefaultCost
-	DataSourceName string
-	Query          string
+	URL      string
+	QueryDSL values.Object
+	User     string
+	Password string
 }
 
-func newFromProcedure(qs flux.OperationSpec, pa plan.Administration) (plan.ProcedureSpec, error) {
+func newFromProcedureSpec(qs flux.OperationSpec, _ plan.Administration) (plan.ProcedureSpec, error) {
 	spec, ok := qs.(*FromOpSpec)
 	if !ok {
 		return nil, fmt.Errorf("invalid spec type %T", qs)
 	}
 
 	return &FromProcedureSpec{
-		DataSourceName: spec.DataSourceName,
-		Query:          spec.Query,
+		URL:      spec.URL,
+		QueryDSL: spec.QueryDSL,
+		User:     spec.User,
+		Password: spec.Password,
 	}, nil
 }
 
@@ -89,8 +101,10 @@ func (s *FromProcedureSpec) Kind() plan.ProcedureKind {
 
 func (s *FromProcedureSpec) Copy() plan.ProcedureSpec {
 	ns := new(FromProcedureSpec)
-	ns.DataSourceName = s.DataSourceName
-	ns.Query = s.Query
+	ns.URL = s.URL
+	ns.QueryDSL = s.QueryDSL
+	ns.User = s.User
+	ns.Password = s.Password
 	return ns
 }
 
@@ -100,29 +114,26 @@ func createFromSource(prSpec plan.ProcedureSpec, dsid execute.DatasetID, a execu
 		return nil, fmt.Errorf("invalid spec type %T", prSpec)
 	}
 
-	ElasticIterator := ElasticIterator{id: dsid, spec: spec, administration: a}
+	ElasticIterator := Iterator{id: dsid, spec: spec, administration: a}
 
 	return execute.CreateSourceFromDecoder(&ElasticIterator, dsid, a)
 }
 
-type ElasticIterator struct {
+type Iterator struct {
 	id             execute.DatasetID
 	administration execute.Administration
 	spec           *FromProcedureSpec
-	client         *elastic.Client
-	searchResult   *elastic.SearchResult
+	client         *Client
+	result         *map[string]interface{}
 }
 
-func (c *ElasticIterator) Connect() error {
-	cfg, err := config.Parse(c.spec.DataSourceName)
+func (c *Iterator) Connect() error {
+	client, err := NewClient(c.spec.URL, c.spec.User, c.spec.Password)
 	if err != nil {
 		return err
 	}
-	client, err := elastic.NewClientFromConfig(cfg)
-	if err != nil {
-		return err
-	}
-	if _, _, err = client.Ping(cfg.URL).Do(context.TODO()); err != nil {
+
+	if err = client.Ping(); err != nil {
 		return err
 	}
 	c.client = client
@@ -130,108 +141,104 @@ func (c *ElasticIterator) Connect() error {
 	return nil
 }
 
-func (c *ElasticIterator) Fetch() (bool, error) {
-	searchResult, err := c.client.Search().
-		Query(elastic.NewSimpleQueryStringQuery(c.spec.Query)).
-		Do(context.TODO())
+func (c *Iterator) Fetch() (bool, error) {
+
+	searchResult, err := c.client.Query(c.spec.QueryDSL)
 
 	if err != nil {
 		return false, err
 	}
-	c.searchResult = searchResult
+	c.result = searchResult
 
 	return false, nil
 }
 
-func (c *ElasticIterator) Decode() (flux.Table, error) {
-	groupKey := execute.NewGroupKey(nil, nil)
-	builder := execute.NewColListTableBuilder(groupKey, c.administration.Allocator())
-
-	firstRow := true
-	for _, hit := range c.searchResult.Hits.Hits {
-		item := make(map[string]interface{})
-		err := json.Unmarshal(*hit.Source, &item)
-		if err != nil {
-			return nil, err
-		}
-
-		if firstRow {
-			for name, value := range item {
-				var dataType flux.ColType
-				switch value.(type) {
-				case bool:
-					dataType = flux.TBool
-				case int64:
-					dataType = flux.TInt
-				case uint64:
-					dataType = flux.TUInt
-				case float64:
-					dataType = flux.TFloat
-				case string:
-					dataType = flux.TString
-				case []uint8:
-					// Hack for MySQL, might need to work with charset? TODO
-					dataType = flux.TString
-				case time.Time:
-					dataType = flux.TTime
-				default:
-					fmt.Println(name, reflect.TypeOf(value))
-					execute.PanicUnknownType(flux.TInvalid)
-				}
-
-				_, err := builder.AddCol(flux.ColMeta{Label: name, Type: dataType})
-				if err != nil {
-					return nil, err
-				}
-			}
-			firstRow = false
-		}
-		var j = 0
-		for _, value := range item {
-			switch value.(type) {
-			case bool:
-				if err := builder.AppendBool(j, value.(bool)); err != nil {
-					return nil, err
-				}
-			case int64:
-				if err := builder.AppendInt(j, value.(int64)); err != nil {
-					return nil, err
-				}
-			case uint64:
-				if err := builder.AppendUInt(j, value.(uint64)); err != nil {
-					return nil, err
-				}
-			case float64:
-				if err := builder.AppendFloat(j, value.(float64)); err != nil {
-					return nil, err
-				}
-			case string:
-				if err := builder.AppendString(j, value.(string)); err != nil {
-					return nil, err
-				}
-			case []uint8:
-				// Hack for MySQL, might need to work with charset? #TODO
-				if err := builder.AppendString(j, string(value.([]uint8))); err != nil {
-					return nil, err
-				}
-			case time.Time:
-				if err := builder.AppendTime(j, values.ConvertTime(value.(time.Time))); err != nil {
-					return nil, err
-				}
-			default:
-				execute.PanicUnknownType(flux.TInvalid)
-			}
-			j++
-		}
-
-	}
-
-	return builder.Table()
+func (c *Iterator) Decode() (flux.Table, error) {
+	//groupKey := execute.NewGroupKey(nil, nil)
+	//builder := execute.NewColListTableBuilder(groupKey, c.administration.Allocator())
+	//
+	//firstRow := true
+	//for _, hit := range c.result.Hits.Hits {
+	//	item := make(map[string]interface{})
+	//	err := json.Unmarshal(*hit.Source, &item)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//
+	//	if firstRow {
+	//		for name, value := range item {
+	//			var dataType flux.ColType
+	//			switch value.(type) {
+	//			case bool:
+	//				dataType = flux.TBool
+	//			case int64:
+	//				dataType = flux.TInt
+	//			case uint64:
+	//				dataType = flux.TUInt
+	//			case float64:
+	//				dataType = flux.TFloat
+	//			case string:
+	//				dataType = flux.TString
+	//			case []uint8:
+	//				// Hack for MySQL, might need to work with charset? TODO
+	//				dataType = flux.TString
+	//			case time.Time:
+	//				dataType = flux.TTime
+	//			default:
+	//				fmt.Println(name, reflect.TypeOf(value))
+	//				execute.PanicUnknownType(flux.TInvalid)
+	//			}
+	//
+	//			_, err := builder.AddCol(flux.ColMeta{Label: name, Type: dataType})
+	//			if err != nil {
+	//				return nil, err
+	//			}
+	//		}
+	//		firstRow = false
+	//	}
+	//	var j = 0
+	//	for _, value := range item {
+	//		switch value.(type) {
+	//		case bool:
+	//			if err := builder.AppendBool(j, value.(bool)); err != nil {
+	//				return nil, err
+	//			}
+	//		case int64:
+	//			if err := builder.AppendInt(j, value.(int64)); err != nil {
+	//				return nil, err
+	//			}
+	//		case uint64:
+	//			if err := builder.AppendUInt(j, value.(uint64)); err != nil {
+	//				return nil, err
+	//			}
+	//		case float64:
+	//			if err := builder.AppendFloat(j, value.(float64)); err != nil {
+	//				return nil, err
+	//			}
+	//		case string:
+	//			if err := builder.AppendString(j, value.(string)); err != nil {
+	//				return nil, err
+	//			}
+	//		case []uint8:
+	//			// Hack for MySQL, might need to work with charset? #TODO
+	//			if err := builder.AppendString(j, string(value.([]uint8))); err != nil {
+	//				return nil, err
+	//			}
+	//		case time.Time:
+	//			if err := builder.AppendTime(j, values.ConvertTime(value.(time.Time))); err != nil {
+	//				return nil, err
+	//			}
+	//		default:
+	//			execute.PanicUnknownType(flux.TInvalid)
+	//		}
+	//		j++
+	//	}
+	//
+	//}
+	//
+	//return builder.Table()
 }
 
-func (c *ElasticIterator) Close() error {
-	var err error
-	_, err = c.client.Flush().Do(context.TODO())
-	c.client = nil
-	return err
+func (c *Iterator) Close() error {
+	return nil
 }
