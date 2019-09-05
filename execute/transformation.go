b42cef5eddbfe26a32a75317bdfc098d97a3cf48
Faith Chikwekwe <fchikwekwe@ip-10-79-40-127.ec2.internal>
2019-09-05 16:38:49 -0700

    docs(execute): updating Transformation interface description

--- execute/transformation.go
+++ execute/transformation.go
@@ -9,8 +9,8 @@ import (
 	"github.com/influxdata/flux/plan"
 )
 
-// Transformation represents functions that control how the execution
-// engine will take in flux Table and perform the necessary data processing
+// Transformation represents the execution engine will taking in data
+// from a Dataset and perform the necessary data processing on it
 type Transformation interface {
 	RetractTable(id DatasetID, key flux.GroupKey) error
 	// Process takes in a DatasetID referring to a list of all transformations
