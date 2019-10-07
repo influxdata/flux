076a0b6030f4c68c192e5f14de57abd88c51e07c
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-10-07 12:48:08 -0500

    ci(release): perform releases with github actions (#1936)
    This creates a release workflow for running goreleaser when a tag is
    created. This is in contrast to doing the build on a developers local
    computer.
    
    The method for release is encapsulated in the github action located in
    the `.github/actions/release` directory.

--- release.sh
+++ release.sh
@@ -5,15 +5,6 @@ cd $DIR
 
 set -e
 
-# Ensure that the GITHUB_TOKEN is exposed in the environment.
-if ! env | grep GITHUB_TOKEN= > /dev/null; then
-  echo "GITHUB_TOKEN must be exported in the environment to perform a release." 2>&1
-  exit 1
-fi
-
-export GO111MODULE=on
-
 version=$(go run github.com/influxdata/changelog nextver)
 git tag -s -m "Release $version" $version
 git push origin "$version"
-go run github.com/goreleaser/goreleaser release --rm-dist --release-notes <(go run github.com/influxdata/changelog generate --version $version --commit-url https://github.com/influxdata/flux/commit)
