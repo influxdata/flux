e70753843a9b8f91b2078466205ba302b19146a8
Yiqun Zhang <github@yzhang.io>
2019-10-21 13:59:04 -0400

    feat(rust): ast check

--- libflux/src/ast/check/mod.rs
+++ libflux/src/ast/check/mod.rs
@@ -1,46 +1,77 @@
-use crate::ast::walk;
+use crate::ast::{walk, PropertyKey};
 use crate::ast::SourceLocation;
 use std::fmt;
 
-// Check counts
-pub fn check(node: walk::Node) -> i32 {
-    let mut count = 0;
+#[cfg(test)]
+mod tests;
+
+// check will inspect a single node and annotate it with any AST errors.
+// Unlike the ast.Check() in our Go parser, the check function in Rust
+// returns a list of errors generated during the check process PLUS
+// any errors existed before ast.check() is performed.
+pub fn check(node: walk::Node) -> Vec<Error> {
+    let mut errors = vec![];
     walk::walk(
         &walk::create_visitor(&mut |n| {
-            if has_error(&n) {
-                count += 1
+            // collect any errors we found prior to ast.check.
+            for err in n.base().errors.iter() {
+                errors.push(Error {
+                    location: n.base().location.clone(),
+                    message: err.clone()
+                });
+            }
+            match n {
+                walk::Node::BadStmt(n) => {
+                    errors.push(Error {
+                        location: n.base.location.clone(),
+                        message: format!("invalid statement {}: {}",
+                                         n.base.location, n.text)
+                    })
+                }
+                walk::Node::ObjectExpr(n) => {
+                    let mut has_implicit = false;
+                    let mut has_explicit = false;
+                    for p in n.properties.iter() {
+                        if p.base.errors.is_empty() {
+                            match p.value {
+                                None => {
+                                    has_implicit = true;
+                                    if let PropertyKey::StringLit(s) = &p.key {
+                                        errors.push(Error {
+                                            location: n.base.location.clone(),
+                                            message: format!("string literal key {} must have a value", s.value)
+                                        })
+                                    }
+                                }
+                                Some(_) => {
+                                    has_explicit = true;
+                                }
+                            }
+                        }
+                    }
+                    if has_implicit && has_explicit {
+                        errors.push(Error {
+                            location: n.base.location.clone(),
+                            message: String::from("cannot mix implicit and explicit properties")
+                        })
+                    }
+                }
+                _ => {}
             }
         }),
-        node,
+        node
     );
-    count
+    errors
 }
 
-pub fn error(node: walk::Node) -> Option<Error> {
-    None
-}
-
-fn has_error(node: &walk::Node) -> bool {
-    node.base().errors.len() > 0
-}
-
-#[derive(Debug)] // derive std::fmt::Debug on AppError
+#[derive(Debug, PartialEq)] // derive std::fmt::Debug on AppError
 pub struct Error {
-    location: SourceLocation,
-    message: String,
+    pub location: SourceLocation,
+    pub message: String,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "error at {}: {}", self.location, self.message)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_has_error() {
-        assert_eq!(true, false);
     }
 }
---
+++ libflux/src/ast/check/tests.rs
@@ -0,0 +1,114 @@
+use super::*;
+use crate::parser::parse_string;
+use crate::ast::{Position, File, BaseNode, VariableAssgn, Identifier, IntegerLit};
+use crate::ast::Statement::Variable;
+use crate::ast::Expression::Integer;
+
+#[test]
+fn test_object_check() {
+    let file = parse_string("object_test", "a = 1\nb = {c: 2, a}");
+    let got = check(walk::Node::File(&file));
+    let want = vec![Error {
+        location: SourceLocation {
+            file: Some(String::from("object_test")),
+            start: Position {
+                line: 2,
+                column: 5
+            },
+            end: Position {
+                line: 2,
+                column: 14
+            },
+            source: Some(String::from("{c: 2, a}"))
+        },
+        message: String::from("cannot mix implicit and explicit properties")
+    }];
+    assert!(want.iter().zip(got).all(|(want, got)| want == &got));
+}
+
+#[test]
+fn test_bad_expr() {
+    let file = parse_string("bad_expr_test", "a = 1\nb = \nc=2");
+    let got = check(walk::Node::File(&file));
+    let want = vec![Error {
+        location: SourceLocation {
+            file: Some(String::from("bad_expr_test")),
+            start: Position {
+                line: 3,
+                column: 2
+            },
+            end: Position {
+                line: 3,
+                column: 3
+            },
+            source: Some(String::from("="))
+        },
+        message: String::from("invalid statement bad_expr_test@3:2-3:3: =")
+    }];
+    assert!(want.iter().zip(got).all(|(want, got)| want == &got))
+}
+
+#[test]
+fn test_check_ok() {
+    let file = parse_string("bad_expr_test", "a = 1\nb=2");
+    let got = check(walk::Node::File(&file));
+    assert_eq!(got.len(), 0);
+}
+
+#[test]
+fn test_check_collect_existing_error() {
+    let file = File {
+        base: BaseNode {
+            location: SourceLocation {
+                file: Some(String::from("test_check_collect_existing_error")),
+                start: Position { line: 1, column: 1 },
+                end: Position { line: 3, column: 6 },
+                source: Some(String::from("a = 1\nb=2\nc=a+b"))
+            },
+            errors: vec![String::from("error 1")]
+        },
+        name: String::from("test_check_collect_existing_error"),
+        package: None,
+        imports: vec![],
+        body: vec![Variable(VariableAssgn {
+            base: BaseNode {
+                location: SourceLocation {
+                    file: Some(String::from("test_check_collect_existing_error")),
+                    start: Position { line: 1, column: 1 },
+                    end: Position { line: 1, column: 6 },
+                    source: Some(String::from("a = 1"))
+                },
+                errors: vec![]
+            },
+            id: Identifier {
+                base: BaseNode {
+                    location: SourceLocation {
+                        file: Some(String::from("test_check_collect_existing_error")),
+                        start: Position { line: 1, column: 1 },
+                        end: Position { line: 1, column: 2 },
+                        source: Some(String::from("a"))
+                    },
+                    errors: vec![]
+                },
+                name: String::from("a")
+            },
+            init: Integer(IntegerLit {
+                base: BaseNode {
+                    location: SourceLocation {
+                        file: Some(String::from("test_check_collect_existing_error")),
+                        start: Position { line: 1, column: 5 },
+                        end: Position {line: 1, column: 6 },
+                        source: Some(String::from("1"))
+                    },
+                    errors: vec![String::from("error 2"), String::from("error 3")]
+                },
+                value: 1
+            })
+        })]
+    };
+    let got = check(walk::Node::File(&file));
+    assert_eq!(3, got.len());
+    for (i, err) in got.iter().enumerate() {
+        assert_eq!(err.message, format!("error {}", i+1));
+    }
+}
