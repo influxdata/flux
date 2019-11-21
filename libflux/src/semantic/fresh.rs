25e3b502913c8d7814f1cfc9a6d5e02683abc8ae
Paul Hummer <paul@eventuallyanyway.com>
2019-11-21 16:13:02 -0700

    Style(libflux): Address new_without_default lints
    `semantic::fresh::Fresher::new` was returning a default in `new`. Clippy
    suggested implementing `Default`, which was easily derived.

--- libflux/src/semantic/fresh.rs
+++ libflux/src/semantic/fresh.rs
@@ -1,6 +1,7 @@
 use crate::semantic::types::Tvar;
 
 // Fresher returns incrementing type variables
+#[derive(Default)]
 pub struct Fresher(u64);
 
 // Create a tvar fresher from a u64
@@ -12,7 +13,7 @@ impl From<u64> for Fresher {
 
 impl Fresher {
     pub fn new() -> Fresher {
-        Fresher(0)
+        Fresher::default()
     }
 
     pub fn fresh(&mut self) -> Tvar {
