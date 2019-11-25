db718c605b3f22a7da3c34e7df9ce71e2ddd36c3
Christopher Wolff <chris.wolff@influxdata.com>
2019-11-25 10:35:02 -0800

    fix(semantic): update semantic graph FlatBuffers schema for identifiers

--- semantic/internal/fbsemantic/NativeVariableAssignment.go
+++ semantic/internal/fbsemantic/NativeVariableAssignment.go
@@ -39,12 +39,12 @@ func (rcv *NativeVariableAssignment) Loc(obj *SourceLocation) *SourceLocation {
 	return nil
 }
 
-func (rcv *NativeVariableAssignment) Identifier(obj *IdentifierExpression) *IdentifierExpression {
+func (rcv *NativeVariableAssignment) Identifier(obj *Identifier) *Identifier {
 	o := flatbuffers.UOffsetT(rcv._tab.Offset(6))
 	if o != 0 {
 		x := rcv._tab.Indirect(o + rcv._tab.Pos)
 		if obj == nil {
-			obj = new(IdentifierExpression)
+			obj = new(Identifier)
 		}
 		obj.Init(rcv._tab.Bytes, x)
 		return obj
