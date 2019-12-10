9709b53218fad58f1af62c6292ff5fee5fb92e5b
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-12-10 12:15:37 -0600

    feat: teach flux-config how to download the sources when using vendor (#2259)

--- internal/cmd/flux-config/build.go
+++ internal/cmd/flux-config/build.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"archive/zip"
+	"bufio"
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -32,6 +37,10 @@ func getGoCache() (string, error) {
 }
 
 func getModule() (*Module, error) {
+	if flags.Vendor {
+		return getModuleFromVendor()
+	}
+
 	cmd := exec.Command("go", "list", "-m", "-json", modulePath)
 	r, err := cmd.StdoutPipe()
 	if err != nil {
@@ -54,9 +63,50 @@ func getModule() (*Module, error) {
 	return &m, nil
 }
 
+func getModuleFromVendor() (*Module, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	re := regexp.MustCompile(`^# ` + modulePath + `\s+([^\s]+).*$`)
+	for {
+		fpath := filepath.Join(cwd, "vendor/modules.txt")
+		if f, err := os.Open(fpath); err == nil {
+			return func() (*Module, error) {
+				defer func() { _ = f.Close() }()
+
+				scanner := bufio.NewScanner(f)
+				for scanner.Scan() {
+					m := re.FindStringSubmatch(scanner.Text())
+					if len(m) > 0 {
+						return &Module{
+							Path:    modulePath,
+							Version: m[1],
+						}, nil
+					}
+				}
+				return nil, fmt.Errorf("module %s not found in vendor modules", modulePath)
+			}()
+		}
+
+		if cwd == "/" {
+			return nil, errors.New("no vendor directory found")
+		}
+		cwd = filepath.Dir(cwd)
+	}
+}
+
 func copySources(srcdir string, mod *Module) error {
+	if mod.Dir != "" {
+		return copySourcesFromDir(srcdir, mod, mod.Dir)
+	}
+	return downloadSources(srcdir, mod)
+}
+
+func copySourcesFromDir(srcdir string, mod *Module, dir string) error {
 	// Retrieve the sources from the module.
-	root := filepath.Join(mod.Dir, "libflux")
+	root := filepath.Join(dir, "libflux")
 	if _, err := os.Stat(root); err != nil {
 		return fmt.Errorf("libflux sources not present: %s", err)
 	}
@@ -85,6 +135,69 @@ func copySources(srcdir string, mod *Module) error {
 		}
 		return nil
 	})
+}
+
+func downloadSources(srcdir string, mod *Module) error {
+	u := fmt.Sprintf("https://%s/archive/%s.zip", modulePath, mod.Version)
+	req, _ := http.NewRequest("GET", u, nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	} else if resp.StatusCode/100 != 2 {
+		return fmt.Errorf("http status error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	var body bytes.Buffer
+	if _, err := io.Copy(&body, resp.Body); err != nil {
+		return err
+	}
+	_ = resp.Body.Close()
+
+	r := bytes.NewReader(body.Bytes())
+	zipf, err := zip.NewReader(r, int64(body.Len()))
+	if err != nil {
+		return err
+	}
+
+	for _, file := range zipf.File {
+		relpath := filepath.Clean(file.Name)
+		if slash := strings.Index(relpath, "/"); slash != -1 {
+			relpath = relpath[slash+1:]
+		}
+
+		if !strings.HasPrefix(relpath, "libflux/") {
+			continue
+		}
+		relpath = relpath[strings.Index(relpath, "/")+1:]
+
+		fpath := filepath.Join(srcdir, relpath)
+		if file.Mode().IsDir() {
+			if err := os.Mkdir(fpath, 0755); err != nil {
+				return err
+			}
+			continue
+		}
+
+		if err := func() error {
+			w, err := os.Create(fpath)
+			if err != nil {
+				return err
+			}
+			defer func() { _ = w.Close() }()
+
+			r, err := file.Open()
+			if err != nil {
+				return err
+			}
+			defer func() { _ = r.Close() }()
+
+			_, err = io.Copy(w, r)
+			return err
+		}(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func runCargo(srcdir string) error {
@@ -121,6 +234,12 @@ func build() (string, error) {
 		version = "dev"
 	}
 	srcdir := filepath.Join(gocache, "libflux", "@"+version)
+	libDir := filepath.Join(srcdir, "lib")
+	target := filepath.Join(libDir, "libflux.a")
+	if _, err := os.Stat(target); version != "dev" && err == nil {
+		return libDir, nil
+	}
+
 	if err := os.MkdirAll(srcdir, 0755); err != nil {
 		return "", err
 	}
@@ -134,11 +253,9 @@ func build() (string, error) {
 	}
 	// Create a directory for the library and static link it there.
 	// This is done to avoid picking up the dynamic library when linking.
-	libDir := filepath.Join(srcdir, "lib")
 	if err := os.MkdirAll(libDir, 0755); err != nil {
 		return "", err
 	}
-	target := filepath.Join(libDir, "libflux.a")
 	if _, err := os.Stat(target); err == nil {
 		_ = os.Remove(target)
 	}
