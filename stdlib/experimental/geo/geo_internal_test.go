93b444d4057c218da0529316bed72339f2056356
Ales Pour <ales.pour@bonitoo.io>
2020-01-22 11:08:04 +0100

    added TODO

--- stdlib/experimental/geo/geo_internal_test.go
+++ stdlib/experimental/geo/geo_internal_test.go
@@ -2,7 +2,7 @@ package geo
 
 import "github.com/influxdata/flux/values"
 
-// TODO: This is exposed so the tests have access
+// TODO(ales.pour@bonitoo.io): This is exposed so the tests have access to the functions.
 var Functions = map[string]values.Function {
 	"getGrid": generateGetGridFunc(),
 }
