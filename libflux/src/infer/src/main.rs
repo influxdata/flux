1426cb7bf0c7b56efce3a05c775bfdadaff4f69a
Nathaniel Cook <nvcook42@gmail.com>
2020-01-23 11:42:21 -0700

    fix(libflux): use libstd API for running type inference
    This change removes the manual invocation of the libstd imports fresher API
    and instead uses the simplified analyze method.
    
    It also adds explicit exit codes for the error conditions.

--- libflux/src/infer/src/main.rs
+++ libflux/src/infer/src/main.rs
@@ -1,34 +1,33 @@
 use flux::ast;
 use flux::parser::parse_string;
-use flux::semantic::convert::convert_with;
-use flux::semantic::convert::Result as ConversionResult;
-use flux::semantic::env::Environment;
-use flux::semantic::fresh::Fresher;
-//use flux::semantic::import::Importer;
 use flux::semantic::nodes;
-use flux::semantic::nodes::{infer_pkg_types, inject_pkg_types};
 use libstd;
 use std::io::{self, Read};
 
-fn main() -> io::Result<()> {
+fn main() {
     let mut src = String::new();
-    io::stdin().read_to_string(&mut src)?;
-    convert_source(&src).unwrap();
-    Ok(())
+    match io::stdin().read_to_string(&mut src) {
+        Ok(_) => {}
+        Err(err) => {
+            print!("{}\n", err);
+            std::process::exit(1);
+        }
+    };
+    match convert_source(&src) {
+        Ok(_) => {}
+        Err(err) => {
+            print!("{}\n", err);
+            std::process::exit(2);
+        }
+    };
 }
 
-pub fn convert_source(source: &str) -> ConversionResult<nodes::Package> {
+pub fn convert_source(source: &str) -> Result<nodes::Package, flux::Error> {
     let file = parse_string("", source);
     let errs = ast::check::check(ast::walk::Node::File(&file));
     if !errs.is_empty() {
-        return Err(format!("Parsing Error: {:?}", errs));
+        return Err(flux::Error::from(format!("Parsing Error: {:?}", errs)));
     }
     let ast_pkg: ast::Package = file.into();
-    let mut f = Fresher::default();
-    let mut sem_pkg = convert_with(ast_pkg, &mut f)?;
-    let env = Environment::new(libstd::prelude().unwrap());
-    let imports = libstd::imports().unwrap();
-
-    let (_, sub) = infer_pkg_types(&mut sem_pkg, env, &mut f, &imports, &None)?;
-    Ok(inject_pkg_types(sem_pkg, &sub))
+    libstd::analyze(ast_pkg)
 }
