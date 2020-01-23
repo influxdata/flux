c7eca719e6e1df203b5811e03bda5dc924c6804f
Dan Gilkerson <dgilkerson@influxdata.com>
2020-01-23 10:01:51 -0800

    feat(libflux/infer): Updated type inference check

--- libflux/src/infer/src/main.rs
+++ libflux/src/infer/src/main.rs
@@ -1,7 +1,10 @@
 use flux::ast;
 use flux::parser::parse_string;
-use flux::semantic::analyze::{analyze_with, Result};
+use flux::semantic::convert::convert_with;
+use flux::semantic::convert::Result as ConversionResult;
 use flux::semantic::env::Environment;
+use flux::semantic::fresh::Fresher;
+//use flux::semantic::import::Importer;
 use flux::semantic::nodes;
 use flux::semantic::nodes::{infer_pkg_types, inject_pkg_types};
 use libstd;
@@ -10,28 +13,22 @@ use std::io::{self, Read};
 fn main() -> io::Result<()> {
     let mut src = String::new();
     io::stdin().read_to_string(&mut src)?;
-    analyze_source(&src).unwrap();
+    convert_source(&src).unwrap();
     Ok(())
 }
 
-fn analyze_source(source: &str) -> Result<nodes::Package> {
+pub fn convert_source(source: &str) -> ConversionResult<nodes::Package> {
     let file = parse_string("", source);
     let errs = ast::check::check(ast::walk::Node::File(&file));
     if !errs.is_empty() {
         return Err(format!("Parsing Error: {:?}", errs));
     }
-
-    let ast_pkg = ast::Package {
-        base: file.base.clone(),
-        path: "".to_string(),
-        package: "main".to_string(),
-        files: vec![file],
-    };
-    let mut fresher = libstd::fresher();
-    let mut sem_pkg = analyze_with(ast_pkg, &mut fresher)?;
+    let ast_pkg: ast::Package = file.into();
+    let mut f = Fresher::default();
+    let mut sem_pkg = convert_with(ast_pkg, &mut f)?;
     let env = Environment::new(libstd::prelude().unwrap());
     let imports = libstd::imports().unwrap();
 
-    let (_, sub) = infer_pkg_types(&mut sem_pkg, env, &mut fresher, &imports, &None)?;
+    let (_, sub) = infer_pkg_types(&mut sem_pkg, env, &mut f, &imports, &None)?;
     Ok(inject_pkg_types(sem_pkg, &sub))
 }
