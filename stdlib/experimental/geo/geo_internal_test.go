3eba1b1748d8dc18ba0722547e03dcb64a64054c
Ales Pour <ales.pour@bonitoo.io>
2020-02-14 13:54:22 +0100

    removed outdated test

--- stdlib/experimental/geo/geo_internal_test.go
+++
@@ -1,8 +0,0 @@
-package geo
-
-import "github.com/influxdata/flux/values"
-
-// TODO(ales.pour@bonitoo.io): This is exposed so the tests have access to the functions.
-var Functions = map[string]values.Function{
-	"getGrid": generateGetGridFunc(),
-}
