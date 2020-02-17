957828ad8a10c73dd32cf3538eea4dcf53a8a397
Ales Pour <ales.pour@bonitoo.io>
2020-02-17 13:50:10 +0100

    added builtin functions tests

---
+++ stdlib/experimental/geo/geo_internal_test.go
@@ -0,0 +1,11 @@
+package geo
+
+import "github.com/influxdata/flux/values"
+
+// TODO(ales.pour@bonitoo.io): This is exposed so the tests have access to the functions.
+var Functions = map[string]values.Function{
+	"containsLatLon": generateContainsLatLonFunc(),
+	"getGrid":        generateGetGridFunc(),
+	"getParent":      generateGetParentFunc(),
+	"getLevel":       generateGetLevelFunc(),
+}
