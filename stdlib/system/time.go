f3f49c902dc01d49f546889c36ae25ac7787b8a0
jlapacik <joshua.lapacik@pacbell.net>
2020-04-22 17:25:07 -0700

    fix(stdlib/system): system.time() should check context for override

--- stdlib/system/time.go
+++ stdlib/system/time.go
@@ -5,21 +5,27 @@ import (
 	"time"
 
 	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/lang/execdeps"
 	"github.com/influxdata/flux/semantic"
 	"github.com/influxdata/flux/values"
 )
 
-var systemTimeFuncName = "time"
+var systemTimeFunc = values.NewFunction(
+	"time",
+	semantic.NewFunctionPolyType(semantic.FunctionPolySignature{
+		Return: semantic.Time,
+	}),
+	func(ctx context.Context, args values.Object) (values.Value, error) {
+		if execdeps.HaveExecutionDependencies(ctx) {
+			if dep := execdeps.GetExecutionDependencies(ctx); dep.Now != nil {
+				return values.NewTime(values.ConvertTime(*dep.Now)), nil
+			}
+		}
+		return values.NewTime(values.ConvertTime(time.Now().UTC())), nil
+	},
+	false,
+)
 
 func init() {
-	flux.RegisterPackageValue("system", systemTimeFuncName, values.NewFunction(
-		systemTimeFuncName,
-		semantic.NewFunctionPolyType(semantic.FunctionPolySignature{
-			Return: semantic.Time,
-		}),
-		func(ctx context.Context, args values.Object) (values.Value, error) {
-			return values.NewTime(values.ConvertTime(time.Now().UTC())), nil
-		},
-		false,
-	))
+	flux.RegisterPackageValue("system", "time", systemTimeFunc)
 }
