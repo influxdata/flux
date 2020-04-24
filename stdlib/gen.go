139b155d278b0b9cbfecd0a22f03ba66a7b89212
Jonathan A. Sternberg <jonathan@influxdata.com>
2020-04-24 15:23:52 -0500

    fix(stdlib): run go generate on libflux when go generate is run on stdlib
    A change to the standard library generally involves recompiling libflux.
    Instead of requiring the developer to remember that, this changes the
    `go generate` step for stdlib to trigger a generate for libflux to force
    a rebuild.
    
    This hack will be needed until we consolidate the standard library
    either into only Go or only Rust.

--- stdlib/gen.go
+++ stdlib/gen.go
@@ -1,3 +1,4 @@
 package stdlib
 
 //go:generate go run github.com/influxdata/flux/internal/cmd/builtin generate --go-pkg github.com/influxdata/flux/stdlib --import-file packages.go
+//go:generate go generate ../libflux/go/libflux
