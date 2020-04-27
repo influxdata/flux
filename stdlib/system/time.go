ab72e1d249fce3071d2c552c24426f36b84941c7
jlapacik <joshua.lapacik@pacbell.net>
2020-04-27 14:26:57 -0700

    Revert "fix(stdlib/system): system.time() should check context for override"
    This reverts commit f3f49c902dc01d49f546889c36ae25ac7787b8a0.

--- stdlib/system/time.go
+++ stdlib/system/time.go
@@ -5,27 +5,21 @@ import (
 	"time"
 
 	"github.com/influxdata/flux"
-	"github.com/influxdata/flux/lang/execdeps"
 	"github.com/influxdata/flux/semantic"
 	"github.com/influxdata/flux/values"
 )
 
-var systemTimeFunc = values.NewFunction(
-	"time",
-	semantic.NewFunctionPolyType(semantic.FunctionPolySignature{
-		Return: semantic.Time,
-	}),
-	func(ctx context.Context, args values.Object) (values.Value, error) {
-		if execdeps.HaveExecutionDependencies(ctx) {
-			if dep := execdeps.GetExecutionDependencies(ctx); dep.Now != nil {
-				return values.NewTime(values.ConvertTime(*dep.Now)), nil
-			}
-		}
-		return values.NewTime(values.ConvertTime(time.Now().UTC())), nil
-	},
-	false,
-)
+var systemTimeFuncName = "time"
 
 func init() {
-	flux.RegisterPackageValue("system", "time", systemTimeFunc)
+	flux.RegisterPackageValue("system", systemTimeFuncName, values.NewFunction(
+		systemTimeFuncName,
+		semantic.NewFunctionPolyType(semantic.FunctionPolySignature{
+			Return: semantic.Time,
+		}),
+		func(ctx context.Context, args values.Object) (values.Value, error) {
+			return values.NewTime(values.ConvertTime(time.Now().UTC())), nil
+		},
+		false,
+	))
 }
