a19929ebecdc35e8771cbceacb7c19fc9c24d4dd
Anaisdg <dganais@gmail.com>
2020-06-01 18:14:39 -0500

    fix

--- stdlib/test_packages.go
+++ stdlib/test_packages.go
@@ -4,6 +4,8 @@ package stdlib
 
 import (
 	ast "github.com/influxdata/flux/ast"
+	anomalydetection "github.com/influxdata/flux/stdlib/contrib/anaisdg/anomalydetection"
+	statsmodels "github.com/influxdata/flux/stdlib/contrib/anaisdg/statsmodels"
 	date "github.com/influxdata/flux/stdlib/date"
 	experimental "github.com/influxdata/flux/stdlib/experimental"
 	aggregate "github.com/influxdata/flux/stdlib/experimental/aggregate"
@@ -29,6 +31,8 @@ import (
 
 var FluxTestPackages = func() []*ast.Package {
 	var pkgs []*ast.Package
+	pkgs = append(pkgs, anomalydetection.FluxTestPackages...)
+	pkgs = append(pkgs, statsmodels.FluxTestPackages...)
 	pkgs = append(pkgs, date.FluxTestPackages...)
 	pkgs = append(pkgs, experimental.FluxTestPackages...)
 	pkgs = append(pkgs, aggregate.FluxTestPackages...)
