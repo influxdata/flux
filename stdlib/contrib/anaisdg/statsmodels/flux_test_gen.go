50aeeae627e9e4a930e4f546623606c39baea5d6
Anaisdg <dganais@gmail.com>
2020-06-08 16:18:36 -0500

    fix: update  gen files

--- stdlib/contrib/anaisdg/statsmodels/flux_test_gen.go
+++ stdlib/contrib/anaisdg/statsmodels/flux_test_gen.go
@@ -21,7 +21,7 @@ var FluxTestPackages = []*ast.Package{&ast.Package{
 					Line:   35,
 				},
 				File:   "linearreg_test.flux",
-				Source: "package statsmodels_test\n\nimport \"testing\"\nimport \"contrib/anaisdg/statsmodels\" \n\n\ninData = \n\"#group,false,false,true,true,false,false,true,true,true,true\n#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,double,string,string,string,string\n#default,_result,,,,,,,,,\n,result,table,_start,_stop,_time,_value,_field,_measurement,shelter,type\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:43:45Z,8,young,cats,B,tabby\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:45:08Z,5,young,cats,B,tabby\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:46:25Z,4,young,cats,B,tabby\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:48:38Z,2,young,cats,B,tabby\n\"\noutData = \n\"#group,false,false,false,true,true,true,true,false,false,true,false,false,false,false,false,true,false,false,false\n#datatype,string,long,double,string,string,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,double,string,double,double,double,double,double,string,double,double,double\n#default,_result,,,,,,,,,,,,,,,,,,\n,result,table,N,_field,_measurement,_start,_stop,_time,errors,shelter,slope,sx,sxx,sxy,sy,type,x,y,y_hat\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:43:45Z,0.16000000000000028,B,-1.9,10,30,38,19,tabby,1,8,7.6\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:45:08Z,0.49000000000000027,B,-1.9,10,30,38,19,tabby,2,5,5.7\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:46:25Z,0.039999999999999716,B,-1.9,10,30,38,19,tabby,3,4,3.8000000000000007\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:48:38Z,0.009999999999999929,B,-1.9,10,30,38,19,tabby,4,2,1.9000000000000004\n\"\n\n\nt_linearRegression = (table=<-) =>\n\ttable\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsModels.linearRegression()\n\ntest _linearRegression = () =>\n({input: testing.loadStorage(csv: inData), want: testing.loadMem(csv: outData), fn: t_linearRegression})",
+				Source: "package statsmodels_test\n\nimport \"testing\"\nimport \"contrib/anaisdg/statsmodels\" \n\n\ninData = \n\"#group,false,false,true,true,false,false,true,true,true,true\n#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,double,string,string,string,string\n#default,_result,,,,,,,,,\n,result,table,_start,_stop,_time,_value,_field,_measurement,shelter,type\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:43:45Z,8,young,cats,B,tabby\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:45:08Z,5,young,cats,B,tabby\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:46:25Z,4,young,cats,B,tabby\n,,0,2020-04-28T23:03:24.565187Z,2020-05-28T23:03:24.565187Z,2020-05-21T21:48:38Z,2,young,cats,B,tabby\n\"\noutData = \n\"#group,false,false,false,true,true,true,true,false,false,true,false,false,false,false,false,true,false,false,false\n#datatype,string,long,double,string,string,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,double,string,double,double,double,double,double,string,double,double,double\n#default,_result,,,,,,,,,,,,,,,,,,\n,result,table,N,_field,_measurement,_start,_stop,_time,errors,shelter,slope,sx,sxx,sxy,sy,type,x,y,y_hat\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:43:45Z,0.16000000000000028,B,-1.9,10,30,38,19,tabby,1,8,7.6\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:45:08Z,0.49000000000000027,B,-1.9,10,30,38,19,tabby,2,5,5.7\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:46:25Z,0.039999999999999716,B,-1.9,10,30,38,19,tabby,3,4,3.8000000000000007\n,,0,4,young,cats,2020-04-28T22:36:37.605243Z,2020-05-28T22:36:37.605243Z,2020-05-21T21:48:38Z,0.009999999999999929,B,-1.9,10,30,38,19,tabby,4,2,1.9000000000000004\n\"\n\n\nt_linearRegression = (table=<-) =>\n\ttable\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsmodels.linearRegression()\n\ntest _linearRegression = () =>\n({input: testing.loadStorage(csv: inData), want: testing.loadMem(csv: outData), fn: t_linearRegression})",
 				Start: ast.Position{
 					Column: 1,
 					Line:   1,
@@ -141,7 +141,7 @@ var FluxTestPackages = []*ast.Package{&ast.Package{
 						Line:   32,
 					},
 					File:   "linearreg_test.flux",
-					Source: "t_linearRegression = (table=<-) =>\n\ttable\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsModels.linearRegression()",
+					Source: "t_linearRegression = (table=<-) =>\n\ttable\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsmodels.linearRegression()",
 					Start: ast.Position{
 						Column: 1,
 						Line:   29,
@@ -175,7 +175,7 @@ var FluxTestPackages = []*ast.Package{&ast.Package{
 							Line:   32,
 						},
 						File:   "linearreg_test.flux",
-						Source: "(table=<-) =>\n\ttable\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsModels.linearRegression()",
+						Source: "(table=<-) =>\n\ttable\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsmodels.linearRegression()",
 						Start: ast.Position{
 							Column: 22,
 							Line:   29,
@@ -384,7 +384,7 @@ var FluxTestPackages = []*ast.Package{&ast.Package{
 								Line:   32,
 							},
 							File:   "linearreg_test.flux",
-							Source: "table\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsModels.linearRegression()",
+							Source: "table\n\t\t|> range(start: 2020-04-28T22:36:00Z, stop: 2020-05-28T22:37:00Z)\n\t\t|> statsmodels.linearRegression()",
 							Start: ast.Position{
 								Column: 2,
 								Line:   30,
@@ -401,7 +401,7 @@ var FluxTestPackages = []*ast.Package{&ast.Package{
 									Line:   32,
 								},
 								File:   "linearreg_test.flux",
-								Source: "statsModels.linearRegression()",
+								Source: "statsmodels.linearRegression()",
 								Start: ast.Position{
 									Column: 6,
 									Line:   32,
@@ -417,7 +417,7 @@ var FluxTestPackages = []*ast.Package{&ast.Package{
 										Line:   32,
 									},
 									File:   "linearreg_test.flux",
-									Source: "statsModels.linearRegression",
+									Source: "statsmodels.linearRegression",
 									Start: ast.Position{
 										Column: 6,
 										Line:   32,
@@ -433,14 +433,14 @@ var FluxTestPackages = []*ast.Package{&ast.Package{
 											Line:   32,
 										},
 										File:   "linearreg_test.flux",
-										Source: "statsModels",
+										Source: "statsmodels",
 										Start: ast.Position{
 											Column: 6,
 											Line:   32,
 										},
 									},
 								},
-								Name: "statsModels",
+								Name: "statsmodels",
 							},
 							Property: &ast.Identifier{
 								BaseNode: ast.BaseNode{
