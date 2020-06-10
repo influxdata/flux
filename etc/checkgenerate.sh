b968ed688e166f34b841c52daf94ae3317678389
Jonathan A. Sternberg <jonathan@influxdata.com>
2020-06-10 13:57:14 -0500

    fix(libflux): include a hash of the sources for libflux (#2897)
    This changes the libflux generate process to generate a hash of the
    sources so that libflux is forced to rebuild.
    
    Backport of #2894.

--- etc/checkgenerate.sh
+++ etc/checkgenerate.sh
@@ -10,5 +10,6 @@ if [ -n "$status" ]; then
   >&2 echo "generated code is not accurate, please run make generate"
   >&2 echo "ragel version is: $(ragel --version)"
   >&2 echo -e "Files changed:\n$status"
+  >&2 git diff
   exit 1
 fi
