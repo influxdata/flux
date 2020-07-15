4b1e15ca375a29adb2211c74696b229bd2732259
Jonathan A. Sternberg <jonathan@influxdata.com>
2020-07-15 16:35:08 -0500

    feat(execute/table): expose static table package and table diff functions
    This exposes the static table package to more easily produce test
    tables. This is exposed publicly so other projects, such as influxdb,
    can use it to test the tables they produce.
    
    It adds a new function to the static table to extend the table by either
    adding or overwriting columns. This way it is easier to create a base
    table, such as one with common group keys, and then override it for each
    table in a table iterator.
    
    This also adds a diff iterator function that will accept a table
    iterator, sort it, then pass it to `cmp.Diff` as a way of encapsulating
    the diff workflow.

--- internal/execute/table/sort.go
+++ internal/execute/table/sort.go
@@ -2,7 +2,7 @@ package table
 
 import (
 	"github.com/influxdata/flux"
-	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/execute/table"
 )
 
 // Sort will read a TableIterator and produce another TableIterator
@@ -12,21 +12,5 @@ import (
 // all of the tables are read to avoid any deadlocks. Be careful
 // using this method in performance sensitive areas.
 func Sort(tables flux.TableIterator) (flux.TableIterator, error) {
-	groups := execute.NewGroupLookup()
-	if err := tables.Do(func(table flux.Table) error {
-		buffered, err := execute.CopyTable(table)
-		if err != nil {
-			return err
-		}
-		groups.Set(buffered.Key(), buffered)
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	var buffered []flux.Table
-	groups.Range(func(_ flux.GroupKey, value interface{}) {
-		buffered = append(buffered, value.(flux.Table))
-	})
-	return Iterator(buffered), nil
+	return table.Sort(tables)
 }
