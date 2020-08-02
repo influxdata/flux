010172243c720cc2dac584267c75796c2ec0ac5e
Jonathan A. Sternberg <jonathan@influxdata.com>
2020-08-02 13:09:08 -0500

    fix(contrib): write tests and fix issues with rows.map
    The `rows.map()` call in contrib was changed to remove `table.Stream`
    for speed. The changed implementation wasn't tested as thoroughly so it
    didn't correctly construct the table or report the schema for the new
    table.
    
    Tests have been written for the map function and these issues have been
    corrected.

--- stdlib/test_packages.go
+++ stdlib/test_packages.go
@@ -4,6 +4,7 @@ package stdlib
 
 import (
 	ast "github.com/influxdata/flux/ast"
+	rows "github.com/influxdata/flux/stdlib/contrib/jsternberg/rows"
 	date "github.com/influxdata/flux/stdlib/date"
 	experimental "github.com/influxdata/flux/stdlib/experimental"
 	aggregate "github.com/influxdata/flux/stdlib/experimental/aggregate"
@@ -29,6 +30,7 @@ import (
 
 var FluxTestPackages = func() []*ast.Package {
 	var pkgs []*ast.Package
+	pkgs = append(pkgs, rows.FluxTestPackages...)
 	pkgs = append(pkgs, date.FluxTestPackages...)
 	pkgs = append(pkgs, experimental.FluxTestPackages...)
 	pkgs = append(pkgs, aggregate.FluxTestPackages...)
