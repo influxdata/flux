1a357c64a9c88a20df28d0d65567a7502de80b49
ChitlangeSahas <chitlangesahas@gmail.com>
2020-08-12 11:09:04 -0700

    feat: ...

--- libflux/src/core/semantic/builtins.rs
+++ libflux/src/core/semantic/builtins.rs
@@ -66,7 +66,7 @@ pub fn builtins() -> Builtins<'static> {
                  "truncate" => "(t: T, unit: duration) => time where T : Timeable",
             },
             "experimental/array" => semantic_map! {
-                "from" => "forall [t0] where t0: Row (rows: [t0]) -> [t0]",
+                "from" => "(rows: [A]) => [A] where A: Row ",
             },
             "experimental/bigtable" => semantic_map! {
                      "from" => "(token: string, project: string, instance: string, table: string) => [T] where T: Row",
@@ -123,7 +123,7 @@ pub fn builtins() -> Builtins<'static> {
                  // must specify exactly one of org, orgID
                  // if host is specified, token must be too.
                  // https://github.com/influxdata/flux/issues/1660
-                 "to" => "(<-tables: [t0], ?bucket: string, ?bucketID: string, ?org: string, ?orgID: string, ?host: string, ?token: string) => [A] where A: Row",
+                 "to" => "(<-tables: [A], ?bucket: string, ?bucketID: string, ?org: string, ?orgID: string, ?host: string, ?token: string) => [A] where A: Row",
                  "join" => "(left: [A], right: [B], fn: (left: A, right: B) => C) => [C] where A: Row, B: Row, C: Row ",
                  "table" => "(rows: [A]) => [A] where A: Row ",
             },
