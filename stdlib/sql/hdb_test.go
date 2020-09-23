6009e1e2411c95a44e994ec28dccbd75d647a966
Ales Pour <ales.pour@bonitoo.io>
2020-09-23 15:29:27 +0200

    test: update to match template changes

--- stdlib/sql/hdb_test.go
+++ stdlib/sql/hdb_test.go
@@ -6,12 +6,14 @@ import (
 )
 
 func TestHdb_IfNoExist(t *testing.T) {
+	// test table with fqn
 	q := hdbAddIfNotExist("stores.orders_copy", "CREATE TABLE stores.orders_copy (ORDER_ID BIGINT)")
-	if !strings.HasPrefix(q, "DO") || !strings.Contains(q, "SCHEMA_NAME") || !strings.Contains(q, "TABLE_NAME") {
+	if !strings.HasPrefix(q, "DO") || !strings.Contains(q, "(:SCHEMA_NAME)") || !strings.Contains(q, "(:TABLE_NAME)") {
 		t.Fail()
 	}
+	// test table in default schema
 	q = hdbAddIfNotExist("orders_copy", "CREATE TABLE orders_copy (ORDER_ID BIGINT)")
-	if !strings.HasPrefix(q, "DO") || strings.Contains(q, "SCHEMA_NAME") || !strings.Contains(q, "TABLE_NAME") {
+	if !strings.HasPrefix(q, "DO") || strings.Contains(q, "(:SCHEMA_NAME)") || !strings.Contains(q, "(:TABLE_NAME)") {
 		t.Fail()
 	}
 }
