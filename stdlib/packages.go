a093a23cd8dfe5178d262dc64218afb6a49e39b8
Anaisdg <30506042+Anaisdg@users.noreply.github.com>
2020-10-09 16:51:11 -0500

    fix: commit (#3246)
    * feat(stdlib/contrib): add linear regression and MAD
    
    * fix(.circleci): allow staticcheck more memory

--- stdlib/packages.go
+++ stdlib/packages.go
@@ -4,6 +4,8 @@ package stdlib
 
 import (
 	_ "github.com/influxdata/flux/stdlib/contrib/RohanSreerama5/naiveBayesClassifier"
+	_ "github.com/influxdata/flux/stdlib/contrib/anaisdg/anomalydetection"
+	_ "github.com/influxdata/flux/stdlib/contrib/anaisdg/statsmodels"
 	_ "github.com/influxdata/flux/stdlib/contrib/chobbs/discord"
 	_ "github.com/influxdata/flux/stdlib/contrib/jsternberg/aggregate"
 	_ "github.com/influxdata/flux/stdlib/contrib/jsternberg/influxdb"
