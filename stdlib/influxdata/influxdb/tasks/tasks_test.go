bd6804aa32602c64afc75eb9e8d8df4ce16217ba
jl <22945345+jlapacik@users.noreply.github.com>
2020-10-20 13:33:45 -0700

    test: orTime argument of lastSuccess is timeable

--- stdlib/influxdata/influxdb/tasks/tasks_test.go
+++ stdlib/influxdata/influxdb/tasks/tasks_test.go
@@ -3,8 +3,11 @@ package tasks_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/lang/execdeps"
 	"github.com/influxdata/flux/stdlib/influxdata/influxdb/tasks"
 	"github.com/influxdata/flux/values"
 )
@@ -13,6 +16,7 @@ func TestLastSuccess(t *testing.T) {
 	for _, tt := range []struct {
 		name string
 		args map[string]values.Value
+		now  time.Time
 		want values.Value
 	}{
 		{
@@ -31,10 +35,33 @@ func TestLastSuccess(t *testing.T) {
 			},
 			want: values.NewTime(5),
 		},
+		{
+			name: "implied orTime",
+			args: map[string]values.Value{
+				"orTime":          values.NewDuration(flux.ConvertDuration(-5 * time.Minute)),
+				"lastSuccessTime": values.NewTime(5),
+			},
+			want: values.NewTime(5),
+		},
+		{
+			name: "implied orTime with null",
+			args: map[string]values.Value{
+				"orTime":          values.NewDuration(flux.ConvertDuration(-5 * time.Nanosecond)),
+				"lastSuccessTime": values.Null,
+			},
+			now:  time.Unix(0, 10),
+			want: values.NewTime(5),
+		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			args := values.NewObjectWithValues(tt.args)
-			got, err := tasks.LastSuccess(context.Background(), args)
+			deps := execdeps.DefaultExecutionDependencies()
+
+			if !tt.now.IsZero() {
+				deps = execdeps.NewExecutionDependencies(nil, &tt.now, nil)
+			}
+
+			got, err := tasks.LastSuccess(deps.Inject(context.Background()), args)
 			if err != nil {
 				t.Fatal(err)
 			} else if !cmp.Equal(tt.want, got) {
