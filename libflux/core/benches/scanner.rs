488977fe30a1f50fbd993c2c5b99a55a85f7ddea
Sean Brickley <sean@wabr.io>
2021-01-14 14:21:44 -0500

    chore(bench): squash errors from refactor

--- libflux/core/benches/scanner.rs
+++ libflux/core/benches/scanner.rs
@@ -6,7 +6,7 @@ use std::ffi::CString;
 
 use criterion::{black_box, Criterion};
 
-use core::scanner::{rust::Scan, rust::Scanner as RustScanner, Scanner, TOK_EOF};
+use core::scanner;
 
 const FLUX: &'static str = r#"from(bucket: "benchtest")
     # Here's a random comment
@@ -19,10 +19,10 @@ fn scanner_scan(c: &mut Criterion) {
     let cdata = CString::new(FLUX).expect("CString::new failed");
     c.bench_function("scanner.scan", |b| {
         b.iter(black_box(|| {
-            let mut s = Scanner::new(cdata.clone());
+            let mut s = scanner::Scanner::new(cdata.clone());
             loop {
                 let token = s.scan();
-                if token.tok == TOK_EOF {
+                if token.tok == scanner::TokenType::EOF {
                     break;
                 }
             }
@@ -30,22 +30,5 @@ fn scanner_scan(c: &mut Criterion) {
     });
 }
 
-/// Create a Scanner with pre-determined text, and scan every token
-/// until EOF. NOTE: This benchmark can be removed when the rust scanner
-/// replaces the current scanner.
-fn rust_scanner_scan(c: &mut Criterion) {
-    let cdata = CString::new(FLUX).expect("CString::new failed");
-    c.bench_function("rustscanner.scan", |b| {
-        b.iter(black_box(|| {
-            let mut s = RustScanner::new(cdata.clone());
-            loop {
-                let token = s.scan();
-                if token.tok == TOK_EOF {
-                    break;
-                }
-            }
-        }));
-    });
-}
-criterion_group!(scanner, scanner_scan, rust_scanner_scan);
+criterion_group!(scanner, scanner_scan);
 criterion_main!(scanner);
