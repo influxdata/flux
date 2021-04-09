f4946a682f9708ec4dc31cef9743b2bf1536885e
Jonathan A. Sternberg <jonathan@influxdata.com>
2021-04-09 18:37:30 -0500

    feat: integrate filter

--- stdlib/universe/filter.go
+++ stdlib/universe/filter.go
@@ -15,94 +15,63 @@ import (
 	"github.com/influxdata/flux/execute"
 	"github.com/influxdata/flux/internal/arrowutil"
 	"github.com/influxdata/flux/internal/errors"
+	"github.com/influxdata/flux/internal/execute/execkit"
 	"github.com/influxdata/flux/internal/execute/table"
 	"github.com/influxdata/flux/interpreter"
 	"github.com/influxdata/flux/memory"
 	"github.com/influxdata/flux/plan"
-	"github.com/influxdata/flux/runtime"
 	"github.com/influxdata/flux/semantic"
 	"github.com/influxdata/flux/values"
 )
 
 const FilterKind = "filter"
 
-type FilterOpSpec struct {
-	Fn      interpreter.ResolvedFunction `json:"fn"`
-	OnEmpty string                       `json:"onEmpty,omitempty"`
-}
-
 func init() {
-	filterSignature := runtime.MustLookupBuiltinType("universe", "filter")
-
-	runtime.RegisterPackageValue("universe", FilterKind, flux.MustValue(flux.FunctionValue(FilterKind, createFilterOpSpec, filterSignature)))
-	flux.RegisterOpSpec(FilterKind, newFilterOp)
-	plan.RegisterProcedureSpec(FilterKind, newFilterProcedure, FilterKind)
-	execute.RegisterTransformation(FilterKind, createFilterTransformation)
+	execkit.RegisterTransformation(&FilterProcedureSpec{})
 	plan.RegisterPhysicalRules(
 		RemoveTrivialFilterRule{},
 	)
 }
 
-func createFilterOpSpec(args flux.Arguments, a *flux.Administration) (flux.OperationSpec, error) {
+type FilterProcedureSpec struct {
+	plan.DefaultCost
+	Fn              interpreter.ResolvedFunction `flux:"fn,required"`
+	KeepEmptyTables bool                         `flux:"onEmpty"`
+}
+
+func (s *FilterProcedureSpec) CreateTransformation(id execute.DatasetID, a execute.Administration) (execute.Transformation, execute.Dataset, error) {
+	return NewFilterTransformation(a.Context(), s, id, a.Allocator())
+}
+
+func (s *FilterProcedureSpec) ReadArgs(args flux.Arguments, a *flux.Administration) error {
 	if err := a.AddParentFromArgs(args); err != nil {
-		return nil, err
+		return err
 	}
+
 	f, err := args.GetRequiredFunction("fn")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	onEmpty, ok, err := args.GetString("onEmpty")
 	if err != nil {
-		return nil, err
+		return err
 	} else if ok {
 		// Check that the string is ok.
 		switch onEmpty {
 		case "keep", "drop":
 		default:
-			return nil, errors.Newf(codes.Invalid, "onEmpty must be keep or drop, was %q", onEmpty)
+			return errors.Newf(codes.Invalid, "onEmpty must be keep or drop, was %q", onEmpty)
 		}
 	}
+	s.KeepEmptyTables = onEmpty == "keep"
 
 	fn, err := interpreter.ResolveFunction(f)
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	return &FilterOpSpec{
-		Fn:      fn,
-		OnEmpty: onEmpty,
-	}, nil
-}
-func newFilterOp() flux.OperationSpec {
-	return new(FilterOpSpec)
-}
-
-func (s *FilterOpSpec) Kind() flux.OperationKind {
-	return FilterKind
-}
-
-type FilterProcedureSpec struct {
-	plan.DefaultCost
-	Fn              interpreter.ResolvedFunction
-	KeepEmptyTables bool
-}
-
-func newFilterProcedure(qs flux.OperationSpec, pa plan.Administration) (plan.ProcedureSpec, error) {
-	spec, ok := qs.(*FilterOpSpec)
-	if !ok {
-		return nil, errors.Newf(codes.Internal, "invalid spec type %T", qs)
-	}
-
-	onEmpty := spec.OnEmpty
-	if onEmpty == "" {
-		onEmpty = "drop"
-	}
-
-	return &FilterProcedureSpec{
-		Fn:              spec.Fn,
-		KeepEmptyTables: onEmpty == "keep",
-	}, nil
+	s.Fn = fn
+	return nil
 }
 
 func (s *FilterProcedureSpec) Kind() plan.ProcedureKind {
@@ -115,23 +84,6 @@ func (s *FilterProcedureSpec) Copy() plan.ProcedureSpec {
 	return ns
 }
 
-// TriggerSpec implements plan.TriggerAwareProcedureSpec
-func (s *FilterProcedureSpec) TriggerSpec() plan.TriggerSpec {
-	return plan.NarrowTransformationTriggerSpec{}
-}
-
-func createFilterTransformation(id execute.DatasetID, mode execute.AccumulationMode, spec plan.ProcedureSpec, a execute.Administration) (execute.Transformation, execute.Dataset, error) {
-	s, ok := spec.(*FilterProcedureSpec)
-	if !ok {
-		return nil, nil, errors.Newf(codes.Internal, "invalid spec type %T", spec)
-	}
-	t, d, err := NewFilterTransformation(a.Context(), s, id, a.Allocator())
-	if err != nil {
-		return nil, nil, err
-	}
-	return t, d, nil
-}
-
 func (s *FilterProcedureSpec) PlanDetails() string {
 	if expr, ok := s.Fn.Fn.GetFunctionBodyExpression(); ok {
 		return fmt.Sprintf("%v", semantic.Formatted(expr))
@@ -140,8 +92,6 @@ func (s *FilterProcedureSpec) PlanDetails() string {
 }
 
 type filterTransformation struct {
-	execute.ExecutionNode
-	d               *execute.PassthroughDataset
 	ctx             context.Context
 	fn              *execute.RowPredicateFn
 	keepEmptyTables bool
@@ -150,23 +100,16 @@ type filterTransformation struct {
 
 func NewFilterTransformation(ctx context.Context, spec *FilterProcedureSpec, id execute.DatasetID, alloc *memory.Allocator) (execute.Transformation, execute.Dataset, error) {
 	fn := execute.NewRowPredicateFn(spec.Fn.Fn, compiler.ToScope(spec.Fn.Scope))
-	t := &filterTransformation{
-		d:               execute.NewPassthroughDataset(id),
+	return execkit.NewNarrowTransformation(id, &filterTransformation{
 		fn:              fn,
 		ctx:             ctx,
 		keepEmptyTables: spec.KeepEmptyTables,
-		alloc:           alloc,
-	}
-	return t, t.d, nil
+	}, alloc)
 }
 
-func (t *filterTransformation) RetractTable(id execute.DatasetID, key flux.GroupKey) error {
-	return t.d.RetractTable(key)
-}
-
-func (t *filterTransformation) Process(id execute.DatasetID, tbl flux.Table) error {
+func (t *filterTransformation) Process(view table.View, d *execkit.Dataset, mem arrowmem.Allocator) error {
 	// Prepare the function for the column types.
-	cols := tbl.Cols()
+	cols := view.Cols()
 	fn, err := t.fn.Prepare(cols)
 	if err != nil {
 		// TODO(nathanielc): Should we not fail the query for failed compilation?
@@ -176,35 +119,28 @@ func (t *filterTransformation) Process(id execute.DatasetID, tbl flux.Table) err
 	// Retrieve the inferred input type for the function.
 	// If all of the inferred inputs are part of the group
 	// key, we can evaluate a record with only the group key.
-	if t.canFilterByKey(fn, tbl) {
-		return t.filterByKey(tbl)
+	if t.canFilterByKey(fn, view) {
+		return t.filterByKey(view, d)
 	}
 
 	// Prefill the columns that can be inferred from the group key.
 	// Retrieve the input type from the function and record the indices
 	// that need to be obtained from the columns.
 	record := values.NewObject(fn.InputType())
-	indices := make([]int, 0, len(tbl.Cols())-len(tbl.Key().Cols()))
-	for j, c := range tbl.Cols() {
-		if idx := execute.ColIdx(c.Label, tbl.Key().Cols()); idx >= 0 {
-			record.Set(c.Label, tbl.Key().Value(idx))
+	indices := make([]int, 0, view.NCols()-len(view.Key().Cols()))
+	for j, c := range view.Cols() {
+		if idx := execute.ColIdx(c.Label, view.Key().Cols()); idx >= 0 {
+			record.Set(c.Label, view.Key().Value(idx))
 			continue
 		}
 		indices = append(indices, j)
 	}
 
 	// Filter the table and pass in the indices we have to read.
-	table, err := t.filterTable(fn, tbl, record, indices)
-	if err != nil {
-		return err
-	} else if table.Empty() && !t.keepEmptyTables {
-		// Drop the table.
-		return nil
-	}
-	return t.d.Process(table)
+	return t.filterTable(d, fn, view, record, indices)
 }
 
-func (t *filterTransformation) canFilterByKey(fn *execute.RowPredicatePreparedFn, tbl flux.Table) bool {
+func (t *filterTransformation) canFilterByKey(fn *execute.RowPredicatePreparedFn, view table.View) bool {
 	inType := fn.InferredInputType()
 	nargs, err := inType.NumProperties()
 	if err != nil {
@@ -221,12 +157,12 @@ func (t *filterTransformation) canFilterByKey(fn *execute.RowPredicatePreparedFn
 		// in the table at all, we don't care if it is missing
 		// since it will always be missing.
 		label := prop.Name()
-		if execute.ColIdx(label, tbl.Cols()) < 0 {
+		if !view.HasCol(label) {
 			continue
 		}
 
 		// Look for a column with this name in the group key.
-		if execute.ColIdx(label, tbl.Key().Cols()) < 0 {
+		if execute.ColIdx(label, view.Key().Cols()) < 0 {
 			// If we cannot find this referenced column in the group
 			// key, then it is provided by the table and we need to
 			// evaluate each row individually.
@@ -238,8 +174,8 @@ func (t *filterTransformation) canFilterByKey(fn *execute.RowPredicatePreparedFn
 	return true
 }
 
-func (t *filterTransformation) filterByKey(tbl flux.Table) error {
-	key := tbl.Key()
+func (t *filterTransformation) filterByKey(view table.View, d *execkit.Dataset) error {
+	key := view.Key()
 	cols := key.Cols()
 	fn, err := t.fn.Prepare(cols)
 	if err != nil {
@@ -262,45 +198,51 @@ func (t *filterTransformation) filterByKey(tbl flux.Table) error {
 	}
 
 	if !v {
-		tbl.Done()
 		if !t.keepEmptyTables {
 			return nil
 		}
 		// If we are supposed to keep empty tables, produce
-		// an empty table with this group key and send it
-		// to the next transformation to process it.
-		tbl = execute.NewEmptyTable(tbl.Key(), tbl.Cols())
+		// an empty buffer with this group key so later transformations
+		// see the group key.
+		buffer := arrow.EmptyBuffer(view.Key(), view.Cols())
+		return d.Process(table.ViewFromBuffer(buffer))
 	}
-	return t.d.Process(tbl)
+	view.Retain()
+	return d.Process(view)
 }
 
-func (t *filterTransformation) filterTable(fn *execute.RowPredicatePreparedFn, in flux.Table, record values.Object, indices []int) (flux.Table, error) {
-	return table.StreamWithContext(t.ctx, in.Key(), in.Cols(), func(ctx context.Context, w *table.StreamWriter) error {
-		return in.Do(func(cr flux.ColReader) error {
-			bitset, err := t.filter(fn, cr, record, indices)
-			if err != nil {
-				return err
-			}
-			defer bitset.Release()
+func (t *filterTransformation) filterTable(d *execkit.Dataset, fn *execute.RowPredicatePreparedFn, in table.View, record values.Object, indices []int) error {
+	buffer := in.Buffer()
+	bitset, err := t.filter(fn, &buffer, record, indices)
+	if err != nil {
+		return err
+	}
+	defer bitset.Release()
 
-			n := bitutil.CountSetBits(bitset.Buf(), 0, bitset.Len())
-			if n == 0 {
-				return nil
-			}
+	n := bitutil.CountSetBits(bitset.Buf(), 0, bitset.Len())
+	if n == 0 {
+		if t.keepEmptyTables {
+			buffer := arrow.EmptyBuffer(in.Key(), in.Cols())
+			return d.Process(table.ViewFromBuffer(buffer))
+		}
+		return nil
+	}
 
-			// Produce arrays for each column.
-			vs := make([]array.Interface, len(w.Cols()))
-			for j, col := range w.Cols() {
-				arr := table.Values(cr, j)
-				if in.Key().HasCol(col.Label) {
-					vs[j] = arrow.Slice(arr, 0, int64(n))
-					continue
-				}
-				vs[j] = arrowutil.Filter(arr, bitset.Bytes(), t.alloc)
-			}
-			return w.Write(vs)
-		})
-	})
+	// Produce arrays for each column.
+	buffer = arrow.TableBuffer{
+		GroupKey: in.Key(),
+		Columns:  in.Cols(),
+		Values:   make([]array.Interface, in.NCols()),
+	}
+	for j, col := range in.Cols() {
+		arr := in.Borrow(j)
+		if in.Key().HasCol(col.Label) {
+			buffer.Values[j] = arrow.Slice(arr, 0, int64(n))
+			continue
+		}
+		buffer.Values[j] = arrowutil.Filter(arr, bitset.Bytes(), t.alloc)
+	}
+	return d.Process(table.ViewFromBuffer(buffer))
 }
 
 func (t *filterTransformation) filter(fn *execute.RowPredicatePreparedFn, cr flux.ColReader, record values.Object, indices []int) (*arrowmem.Buffer, error) {
@@ -320,16 +262,6 @@ func (t *filterTransformation) filter(fn *execute.RowPredicatePreparedFn, cr flu
 		bitutil.SetBitTo(bitset.Buf(), i, val)
 	}
 	return bitset, nil
-}
-
-func (t *filterTransformation) UpdateWatermark(id execute.DatasetID, mark execute.Time) error {
-	return t.d.UpdateWatermark(mark)
-}
-func (t *filterTransformation) UpdateProcessingTime(id execute.DatasetID, pt execute.Time) error {
-	return t.d.UpdateProcessingTime(pt)
-}
-func (t *filterTransformation) Finish(id execute.DatasetID, err error) {
-	t.d.Finish(err)
 }
 
 // RemoveTrivialFilterRule removes Filter nodes whose predicate always evaluates to true.
@@ -388,7 +320,7 @@ func (MergeFiltersRule) Rewrite(ctx context.Context, filterNode plan.Node) (plan
 		// Not an expression.
 		return filterNode, false, nil
 	}
-	//checks if the fields of KeepEmptyTables are different and only allows merge if 1) they are the same 2) keep is the Predecessors field
+	//checks if the fields of OnEmptyMode are different and only allows merge if 1) they are the same 2) keep is the Predecessors field
 	if filterSpec1.KeepEmptyTables != filterSpec2.KeepEmptyTables && !filterSpec2.KeepEmptyTables {
 		return filterNode, false, nil
 	}
