56ba9c7b0f19b527758679d6e48199c213709fd3
barweiner <bweiner@influxdata.com>
2021-05-20 13:23:27 -0700

    fix(formatter/tests.rs): assert_eq! output

--- libflux/flux-core/src/formatter/tests.rs
+++ libflux/flux-core/src/formatter/tests.rs
@@ -7,7 +7,11 @@ use pretty_assertions::assert_eq;
 
 fn assert_unchanged(script: &str) {
     let output = format(script).unwrap();
-    assert_eq!(script, output);
+    assert_eq!(
+        script, output,
+        "\n EXPECTED: \n {} \n OUTPUT: \n {} \n",
+        script, output
+    );
 }
 
 fn assert_format(script: &str, expected: &str) {
@@ -1079,5 +1083,28 @@ else if z then
     42
 else
     z",
+    );
+}
+#[test]
+fn line_deleter() {
+    assert_unchanged(
+        "tables
+    |> map(
+        fn: (r) => ({r with
+
+
+            level_value: if r._level == levelCrit then
+                4
+            else if r._level == levelWarn then
+                3
+            else if r._level == levelInfo then
+                2
+            else if r._level == levelOK then
+                1
+            else
+                0,
+            foo: bar,
+        }),
+    )",
     );
 }
