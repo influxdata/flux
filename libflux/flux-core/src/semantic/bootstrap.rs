a77eba6c1d889f5273ae4eaa80870d5847dd2f28
barweiner <bweiner@influxdata.com>
2021-06-10 13:28:58 -0700

    fix: changed file and function structure

--- libflux/flux-core/src/semantic/bootstrap.rs
+++ libflux/flux-core/src/semantic/bootstrap.rs
@@ -38,11 +38,10 @@ pub struct Error {
 // DocPackage represents the documentation for a package and its sub packages
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DocPackage {
-    pub path: Vec<String>,
+    pub path: String,
     pub name: String,
     pub doc: String,
     pub values: Vec<DocValue>,
-    pub packages: Vec<DocPackage>,
 }
 
 // DocValue represents the documentation for a single value within a package.
@@ -50,24 +49,10 @@ pub struct DocPackage {
 // package.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DocValue {
-    pub pkgpath: Vec<String>,
+    pub pkgpath: String,
     pub name: String,
     pub doc: String,
     pub typ: String,
-}
-
-struct UntypedDocPackage {
-    pub path: Vec<String>,
-    pub name: String,
-    pub doc: String,
-    pub values: Vec<UntypedDocValue>,
-    pub packages: Vec<UntypedDocPackage>,
-}
-
-struct UntypedDocValue {
-    pub pkgpath: Vec<String>,
-    pub name: String,
-    pub doc: String,
 }
 
 impl From<io::Error> for Error {
@@ -132,7 +117,7 @@ pub fn infer_stdlib() -> Result<(PolyTypeMap, PolyTypeMap, Fresher, Vec<String>,
 }
 
 // new stdlib docs function
-pub fn stdlib_docs(lib:PolyTypeMap, files:Vec<String>) -> Result<Vec<DocPackage>, Error> {
+pub fn stdlib_docs(lib: &PolyTypeMap, files: &AstFileMap) -> Result<Vec<DocPackage>, Error> {
     // initialize DocPackage vector
     // run through stdlib and parse all value comments
     //     create and populate a DocValue for each individual
@@ -144,85 +129,87 @@ pub fn stdlib_docs(lib:PolyTypeMap, files:Vec<String>) -> Result<Vec<DocPackage>
     // return the results from the parsing
 
     //let pkg = docs::walk_pkg(&args.pkg, &args.pkg)?;
-    let path = Path::new("../../../../stdlib");
-    let pkg = walk_pkg(&path, &path)?;
-    let typ = lib.Fmt([monotype]);
+    let mut docs = Vec::new();
+    for (path,file) in files{
+        let pkg = generate_docs(path.clone(),lib, file)?;
+        docs.push(pkg);
+    }
+   Ok(docs)
 }
-
 
 // Generates the docs by parsing the sources and checking type inference.
 fn generate_docs(
-    pkgpath: &Path,
-    srcs: Vec<PathBuf>,
-    mut packages: Vec<DocPackage>,
+    path: String,
+    types: &PolyTypeMap,
+    file: &ast::File,
 ) -> Result<DocPackage, Box<dyn std::error::Error>> {
-    // determine path vector
-    let mut path: Vec<String> = Vec::new();
-    let mut curr = pkgpath;
-    loop {
-        if curr == Path::new("") {
-            break;
-        }
-        path.push(curr.file_name().unwrap().to_str().unwrap().to_string());
-        if let Some(parent) = curr.parent() {
-            curr = parent
-        }
-    }
-    path.reverse();
-
-    // parse each src in the package
-    let mut pkg: Option<ast::Package> = None;
-    for src in srcs {
-        let source = fs::read_to_string(&src)?;
-        let file_name = src.file_name().unwrap();
-        let mut p = Parser::new(source.as_str());
-        let mut fpkg: ast::Package = p.parse_file(file_name.to_str().unwrap().to_owned()).into();
-        // skip test packages
-        if !fpkg.package.ends_with("_test") {
-            match pkg {
-                None => pkg = Some(fpkg),
-                Some(ref mut pkg) => {
-                    if let Some(err) = merge_packages(&mut fpkg, pkg) {
-                        return Err(Box::new(err));
-                    }
-                }
-            }
-        }
-    }
-    packages.sort_by_key(|p| p.name.clone());
 
     // construct the package documentation
-    if let Some(pkg) = pkg {
-        // use type inference to determine types of all values
-        //let sem_pkg = analyze(pkg.clone())?;
-        //let types = pkg_types(&sem_pkg);
-        let mut values: Vec<DocValue> = Vec::new();
-        values.sort_by_key(|v| v.name.clone());
-        let mut doc = String::new();
-        for f in &pkg.files {
-            let vs = generate_values(&f, &types, &path)?;
-            values.extend(vs);
-            if let Some(comment) = &f.package {
-                doc = comments_to_string(&comment.base.comments);
-            }
-        }
-        Ok(DocPackage {
-            path,
-            name: pkg.package,
-            doc,
-            values,
-            packages,
-        })
-    } else {
-        let name = path.last().unwrap().clone();
-        Ok(DocPackage {
-            path,
-            name,
-            doc: "".to_string(),
-            values: vec![],
-            packages,
-        })
+    // use type inference to determine types of all values
+    //let sem_pkg = analyze(pkg.clone())?;
+    //let types = pkg_types(&sem_pkg);
+
+    let mut doc = String::new();
+    let values = generate_values(&file, &types, &path)?;
+    if let Some(comment) = &file.package {
+        doc = comments_to_string(&comment.base.comments);
     }
+    ///TODO check if package name exists and if it doesn't throw an error message
+    Ok(DocPackage {
+        path,
+        name: file.package.unwrap().name.name.clone(),
+        doc,
+        values,
+    })
+
+}
+fn generate_values(
+    f: &ast::File,
+    types: &PolyTypeMap,
+    pkgpath: &[String],
+) -> Result<Vec<DocValue>, Box<dyn std::error::Error>> {
+    let mut values: Vec<DocValue> = Vec::new();
+    for stmt in &f.body {
+        match stmt {
+            ast::Statement::Variable(s) => {
+                let doc = comments_to_string(&s.id.base.comments);
+                let name = s.id.name.clone();
+                let typ = format!("{}", types[&name].normal());
+                values.push(DocValue {
+                    pkgpath: pkgpath.to_vec(),
+                    name,
+                    doc,
+                    typ,
+                });
+            }
+            ast::Statement::Builtin(s) => {
+                let doc = comments_to_string(&s.base.comments);
+                let name = s.id.name.clone();
+                let typ = format!("{}", types[&name].normal());
+                values.push(DocValue {
+                    pkgpath: pkgpath.to_vec(),
+                    name,
+                    doc,
+                    typ,
+                });
+            }
+            ast::Statement::Option(s) => {
+                if let ast::Assignment::Variable(v) = &s.assignment {
+                    let doc = comments_to_string(&s.base.comments);
+                    let name = v.id.name.clone();
+                    let typ = format!("{}", types[&name].normal());
+                    values.push(DocValue {
+                        pkgpath: pkgpath.to_vec(),
+                        name,
+                        doc,
+                        typ,
+                    });
+                }
+            }
+            _ => {}
+        }
+    }
+    Ok(values)
 }
 fn pkg_types(pkg: &semantic::nodes::Package) -> HashMap<String, PolyType> {
     let mut types: HashMap<String, PolyType> = HashMap::new();
