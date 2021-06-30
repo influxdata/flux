748e9a006af67350a2b78d63b68a045b890a0032
Noé Garcia <71153687+JustSomeCarbon@users.noreply.github.com>
2021-06-30 10:00:02 -0600

    docs: document math pkg (#3822)
    * docs: math docs added
    
    * chore: fmt and generate
    
    * docs: cleaned up docs
    
    * docs: pkg doc comment
    
    * docs: pkg docs

--- libflux/go/libflux/buildinfo.gen.go
+++ libflux/go/libflux/buildinfo.gen.go
@@ -263,7 +263,7 @@ var sourceHashes = map[string]string{
 	"stdlib/interpolate/interpolate_test.flux":                                      "3e5963a5176e17f7dc2eb7f69c9485bb635793e8e93469d6d9ee569aaa30d492",
 	"stdlib/json/json.flux":                                                         "1dc1dd84fb873ad3978b6ad311b99163b6959731422554c228037a55774bd3c9",
 	"stdlib/kafka/kafka.flux":                                                       "214fb699bcde2db9ecb9a6673bf9f29c481ec51306bcf6316cec23324431ea3d",
-	"stdlib/math/math.flux":                                                         "66e94140d5dedfd69446b55d275be8d391be982fb926f06cd0faa5174c1b5ff1",
+	"stdlib/math/math.flux":                                                         "2eaf5f7e986ba142fc9aea6df2909da2baceea3bf9c746fa08777f0980d21a82",
 	"stdlib/pagerduty/pagerduty.flux":                                               "2243db0ced078978ccbeeecc4a3cdb947c77c961e2c9089208d36c384ea4cd11",
 	"stdlib/pagerduty/pagerduty_test.flux":                                          "d8f9bd6ffe2de7af4270aa51a2adbaeb77405bc52310fb3fc4f77090362b77c4",
 	"stdlib/planner/aggregate_window_max_eval_test.flux":                            "7993881ef948aad5039e3534bd05964e008cefe14c6259152fbc36dca8b3d902",
