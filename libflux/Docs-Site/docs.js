024817ea9050eee72b2bd4e3a725ba0bac9a00d4
barweiner <bweiner@influxdata.com>
2021-07-30 14:33:20 -0700

    feat: progress on solution

---
+++ libflux/Docs-Site/docs.js
@@ -0,0 +1,9 @@
+import * as wasm from '/Users/barweiner/GolandProjects/flux/libflux/flux/pkg';
+
+
+export function get_json_documentation() {
+    let identifier = document.getElementById("text").value
+    let documentation = wasm.get_json_documentation(identifier);
+    document.getElementById("body").innerHTML= "Documentation: " + documentation;
+
+}
