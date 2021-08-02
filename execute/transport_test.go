107666f2ad5fff1b3290b5a4ae231c3d5959f5a3
Jonathan A. Sternberg <jonathan@influxdata.com>
2021-08-02 11:00:05 -0500

    feat(execute): add two additional message types
    This adds two additional message types: `ProcessTableChunk` and `FlushKey`.
    
    The `ProcessTableChunk` message represents that a chunk of a table is
    ready to be processed by the transformation. It contains a reference to
    the table chunk which is an arrow table buffer.
    
    The `FlushKey` message represents that a group key has been finished and
    is ready to flush any data stored in an associated dataset.

--- execute/transport_test.go
+++ execute/transport_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/apache/arrow/go/arrow/memory"
 	"github.com/influxdata/flux"
 	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/execute/table"
 	"github.com/influxdata/flux/execute/table/static"
 )
 
@@ -78,6 +79,84 @@ func TestProcessMsg(t *testing.T) {
 		}
 		dup2.Ack()
 		return nil
+	}); err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	mem.AssertSize(t, 0)
+}
+
+func TestProcessChunkMsg(t *testing.T) {
+	fromTableChunk := func(chunk table.Chunk) flux.Table {
+		buffer := chunk.Buffer()
+		buffer.Retain()
+		return table.FromBuffer(&buffer)
+	}
+	mem := memory.NewCheckedAllocator(memory.DefaultAllocator)
+	defer mem.AssertSize(t, 0)
+
+	// Create a set of tables, wrap them in a process message,
+	// and then ack it.
+	ti := static.Table{
+		static.StringKey("_measurement", "m0"),
+		static.Times("_time", 0, 10, 20),
+		static.Floats("_value", 0, 1, 2),
+	}
+	if err := ti.Do(func(tbl flux.Table) error {
+		return tbl.Do(func(cr flux.ColReader) error {
+			chunk := table.ChunkFromReader(cr)
+			chunk.Retain()
+			m := execute.NewProcessChunkMsg(chunk)
+
+			cr.Retain()
+			want := table.Iterator{table.FromBuffer(cr)}
+			got := table.Iterator{fromTableChunk(m.TableChunk())}
+			if diff := table.Diff(want, got); diff != "" {
+				t.Errorf("unexpected table data -want/+got:\n%s", diff)
+			}
+			m.Ack()
+			return nil
+		})
+	}); err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	mem.AssertSize(t, 0)
+
+	// Create the same set of tables, but this time we are
+	// going to use dup and acknowledge the duplications.
+	// This should similarly work.
+	if err := ti.Do(func(tbl flux.Table) error {
+		return tbl.Do(func(cr flux.ColReader) error {
+			chunk := table.ChunkFromReader(cr)
+			chunk.Retain()
+			m := execute.NewProcessChunkMsg(chunk)
+			dup1, dup2 := m.Dup(), m.Dup()
+			m.Ack()
+
+			// Dup contents should be identical.
+			if diff := table.Diff(
+				table.Iterator{fromTableChunk(dup1.(execute.ProcessChunkMsg).TableChunk())},
+				table.Iterator{fromTableChunk(dup2.(execute.ProcessChunkMsg).TableChunk())},
+			); diff != "" {
+				t.Errorf("unexpected table data:\n%s", diff)
+			}
+
+			// Acknowledge the original message multiple times.
+			// This should not affect the dup'd messages.
+			for i := 0; i < 10; i++ {
+				m.Ack()
+			}
+			mem.AssertSize(t, 0)
+
+			if diff := table.Diff(
+				table.Iterator{fromTableChunk(dup1.(execute.ProcessChunkMsg).TableChunk())},
+				table.Iterator{fromTableChunk(dup2.(execute.ProcessChunkMsg).TableChunk())},
+			); diff != "" {
+				t.Errorf("unexpected table data:\n%s", diff)
+			}
+			dup1.Ack()
+			dup2.Ack()
+			return nil
+		})
 	}); err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
