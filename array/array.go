ee1928a66600c63a5ae9d91355e45e4bf1cc2a2d
Jonathan A. Sternberg <jonathan@influxdata.com>
2021-08-02 11:30:08 -0500

    feat(array): modify the string array builder to support constant data

--- array/array.go
+++ array/array.go
@@ -223,56 +223,154 @@ func (a *String) ValueLen(i int) int {
 }
 
 type StringBuilder struct {
-	b *array.BinaryBuilder
+	builder      *array.BinaryBuilder
+	mem          memory.Allocator
+	value        string
+	length       int
+	capacity     int
+	dataCapacity int
+	refCount     int
 }
 
 func NewStringBuilder(mem memory.Allocator) *StringBuilder {
 	return &StringBuilder{
-		b: array.NewBinaryBuilder(mem, arrow.BinaryTypes.String),
+		mem:      mem,
+		refCount: 1,
 	}
 }
+func (b *StringBuilder) init() {
+	if b.builder == nil {
+		if b.refCount <= 0 {
+			return
+		}
+
+		builder := array.NewBinaryBuilder(b.mem, StringType)
+		if capacity := b.Cap(); capacity > 0 {
+			builder.Resize(capacity)
+			dataCapacity := len(b.value) * capacity
+			if dataCapacity < b.dataCapacity {
+				dataCapacity = b.dataCapacity
+			}
+			builder.ReserveData(dataCapacity)
+		}
+		if b.length > 0 {
+			for i := 0; i < b.length; i++ {
+				builder.AppendString(b.value)
+			}
+		}
+
+		for i := 1; i < b.refCount; i++ {
+			builder.Retain()
+		}
+		b.builder = builder
+	}
+}
+func (b *StringBuilder) reset() {
+	b.builder = nil
+	b.length = 0
+	b.capacity = 0
+	b.dataCapacity = 0
+	b.value = ""
+}
 func (b *StringBuilder) Retain() {
-	b.b.Retain()
+	if b.builder != nil {
+		b.builder.Retain()
+		return
+	}
+	b.refCount++
 }
 func (b *StringBuilder) Release() {
-	b.b.Release()
+	if b.builder != nil {
+		b.builder.Release()
+		return
+	}
+	b.refCount--
 }
 func (b *StringBuilder) Len() int {
-	return b.b.Len()
+	if b.builder != nil {
+		return b.builder.Len()
+	}
+	return b.length
 }
 func (b *StringBuilder) Cap() int {
-	return b.b.Cap()
+	if b.builder != nil {
+		return b.builder.Cap()
+	}
+
+	capacity := b.capacity
+	if capacity < b.length {
+		capacity = b.length
+	}
+	return capacity
 }
 func (b *StringBuilder) NullN() int {
-	return b.b.NullN()
+	if b.builder != nil {
+		return b.builder.NullN()
+	}
+	return 0
 }
 func (b *StringBuilder) Append(v string) {
-	b.b.AppendString(v)
+	if b.builder == nil && (b.length == 0 || v == b.value) {
+		b.value = v
+		b.length++
+		return
+	}
+	b.init()
+	b.builder.AppendString(v)
 }
 func (b *StringBuilder) AppendValues(v []string, valid []bool) {
-	b.b.AppendStringValues(v, valid)
+	for i, val := range v {
+		if len(valid) != 0 && valid[i] {
+			b.AppendNull()
+			continue
+		}
+		b.Append(val)
+	}
 }
 func (b *StringBuilder) AppendNull() {
-	b.b.AppendNull()
+	b.init()
+	b.builder.AppendNull()
 }
 func (b *StringBuilder) UnsafeAppendBoolToBitmap(isValid bool) {
-	b.b.UnsafeAppendBoolToBitmap(isValid)
+	b.init()
+	b.builder.UnsafeAppendBoolToBitmap(isValid)
 }
 func (b *StringBuilder) Reserve(n int) {
-	b.b.Reserve(n)
+	if b.builder != nil {
+		b.builder.Reserve(n)
+		return
+	}
+	b.capacity = n
 }
 func (b *StringBuilder) ReserveData(n int) {
-	b.b.ReserveData(n)
+	if b.builder != nil {
+		b.builder.ReserveData(n)
+		return
+	}
+	b.dataCapacity = n
 }
 func (b *StringBuilder) Resize(n int) {
-	b.b.Resize(n)
+	if b.builder != nil {
+		b.builder.Resize(n)
+		return
+	}
+	// In arrow, resize and reserve both affect
+	// the capacity. Neither of them change the
+	// length of the built array.
+	b.capacity = n
 }
 func (b *StringBuilder) NewArray() Interface {
 	return b.NewStringArray()
 }
 func (b *StringBuilder) NewStringArray() *String {
-	data := b.b.NewBinaryArray()
-	return &String{data: data}
+	arr := &String{}
+	if b.builder == nil {
+		arr.value, arr.length = b.value, b.length
+	} else {
+		arr.data = b.builder.NewBinaryArray()
+	}
+	b.reset()
+	return arr
 }
 
 type sliceable interface {
