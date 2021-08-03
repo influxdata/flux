6952c642fc37ef09fd2a0103a07b7e74cfd62a2e
Jonathan A. Sternberg <jonathan@influxdata.com>
2021-08-03 16:51:11 -0500

    feat(array): add optimized repeat function for arrow arrays (#3922)
    This adds a `Repeat()` method to the `array` package that mimics the
    pattern in from the `flux/arrow` package.
    
    This `Repeat()` differs slightly in that it takes an interface as its
    input instead of a `values.Value` and it differs in that it composes an
    optimized version of string without repeated iterations over the string
    builder.
    
    The `arrow.Repeat()` function has been modified to unwrap the value and
    make use of the `array.Repeat()` function instead of its own
    implementation.

--- array/array.go
+++ array/array.go
@@ -3,13 +3,13 @@ package array
 import (
 	"github.com/apache/arrow/go/arrow"
 	"github.com/apache/arrow/go/arrow/array"
-	"github.com/apache/arrow/go/arrow/memory"
 	"github.com/influxdata/flux/codes"
 	"github.com/influxdata/flux/internal/errors"
 )
 
 //go:generate -command tmpl ../gotool.sh github.com/benbjohnson/tmpl
-//go:generate tmpl -data=@numeric.tmpldata -o numeric.gen.go numeric.gen.go.tmpl
+//go:generate tmpl -data=@types.tmpldata -o builder.gen.go builder.gen.go.tmpl
+//go:generate tmpl -data=@types.tmpldata -o repeat.gen.go repeat.gen.go.tmpl
 
 type DataType = arrow.DataType
 
@@ -93,60 +93,6 @@ type Builder interface {
 	NewArray() Interface
 }
 
-type Boolean = array.Boolean
-
-type BooleanBuilder struct {
-	b *array.BooleanBuilder
-}
-
-func NewBooleanBuilder(mem memory.Allocator) *BooleanBuilder {
-	return &BooleanBuilder{
-		b: array.NewBooleanBuilder(mem),
-	}
-}
-func (b *BooleanBuilder) Retain() {
-	b.b.Retain()
-}
-func (b *BooleanBuilder) Release() {
-	b.b.Release()
-}
-func (b *BooleanBuilder) Len() int {
-	return b.b.Len()
-}
-func (b *BooleanBuilder) Cap() int {
-	return b.b.Cap()
-}
-func (b *BooleanBuilder) NullN() int {
-	return b.b.NullN()
-}
-func (b *BooleanBuilder) Append(v bool) {
-	b.b.Append(v)
-}
-func (b *BooleanBuilder) AppendValues(v []bool, valid []bool) {
-	b.b.AppendValues(v, valid)
-}
-func (b *BooleanBuilder) UnsafeAppend(v bool) {
-	b.b.UnsafeAppend(v)
-}
-func (b *BooleanBuilder) AppendNull() {
-	b.b.AppendNull()
-}
-func (b *BooleanBuilder) UnsafeAppendBoolToBitmap(isValid bool) {
-	b.b.UnsafeAppendBoolToBitmap(isValid)
-}
-func (b *BooleanBuilder) Reserve(n int) {
-	b.b.Reserve(n)
-}
-func (b *BooleanBuilder) Resize(n int) {
-	b.b.Resize(n)
-}
-func (b *BooleanBuilder) NewArray() Interface {
-	return b.NewBooleanArray()
-}
-func (b *BooleanBuilder) NewBooleanArray() *Boolean {
-	return b.b.NewBooleanArray()
-}
-
 type String struct {
 	value  string
 	length int
@@ -220,157 +166,6 @@ func (a *String) ValueLen(i int) int {
 		return a.data.ValueLen(i)
 	}
 	return len(a.value)
-}
-
-type StringBuilder struct {
-	builder      *array.BinaryBuilder
-	mem          memory.Allocator
-	value        string
-	length       int
-	capacity     int
-	dataCapacity int
-	refCount     int
-}
-
-func NewStringBuilder(mem memory.Allocator) *StringBuilder {
-	return &StringBuilder{
-		mem:      mem,
-		refCount: 1,
-	}
-}
-func (b *StringBuilder) init() {
-	if b.builder == nil {
-		if b.refCount <= 0 {
-			return
-		}
-
-		builder := array.NewBinaryBuilder(b.mem, StringType)
-		if capacity := b.Cap(); capacity > 0 {
-			builder.Resize(capacity)
-			dataCapacity := len(b.value) * capacity
-			if dataCapacity < b.dataCapacity {
-				dataCapacity = b.dataCapacity
-			}
-			builder.ReserveData(dataCapacity)
-		}
-		if b.length > 0 {
-			for i := 0; i < b.length; i++ {
-				builder.AppendString(b.value)
-			}
-		}
-
-		for i := 1; i < b.refCount; i++ {
-			builder.Retain()
-		}
-		b.builder = builder
-	}
-}
-func (b *StringBuilder) reset() {
-	b.builder = nil
-	b.length = 0
-	b.capacity = 0
-	b.dataCapacity = 0
-	b.value = ""
-}
-func (b *StringBuilder) Retain() {
-	if b.builder != nil {
-		b.builder.Retain()
-		return
-	}
-	b.refCount++
-}
-func (b *StringBuilder) Release() {
-	if b.builder != nil {
-		b.builder.Release()
-		return
-	}
-	b.refCount--
-}
-func (b *StringBuilder) Len() int {
-	if b.builder != nil {
-		return b.builder.Len()
-	}
-	return b.length
-}
-func (b *StringBuilder) Cap() int {
-	if b.builder != nil {
-		return b.builder.Cap()
-	}
-
-	capacity := b.capacity
-	if capacity < b.length {
-		capacity = b.length
-	}
-	return capacity
-}
-func (b *StringBuilder) NullN() int {
-	if b.builder != nil {
-		return b.builder.NullN()
-	}
-	return 0
-}
-func (b *StringBuilder) Append(v string) {
-	if b.builder == nil && (b.length == 0 || v == b.value) {
-		b.value = v
-		b.length++
-		return
-	}
-	b.init()
-	b.builder.AppendString(v)
-}
-func (b *StringBuilder) AppendValues(v []string, valid []bool) {
-	for i, val := range v {
-		if len(valid) != 0 && valid[i] {
-			b.AppendNull()
-			continue
-		}
-		b.Append(val)
-	}
-}
-func (b *StringBuilder) AppendNull() {
-	b.init()
-	b.builder.AppendNull()
-}
-func (b *StringBuilder) UnsafeAppendBoolToBitmap(isValid bool) {
-	b.init()
-	b.builder.UnsafeAppendBoolToBitmap(isValid)
-}
-func (b *StringBuilder) Reserve(n int) {
-	if b.builder != nil {
-		b.builder.Reserve(n)
-		return
-	}
-	b.capacity = n
-}
-func (b *StringBuilder) ReserveData(n int) {
-	if b.builder != nil {
-		b.builder.ReserveData(n)
-		return
-	}
-	b.dataCapacity = n
-}
-func (b *StringBuilder) Resize(n int) {
-	if b.builder != nil {
-		b.builder.Resize(n)
-		return
-	}
-	// In arrow, resize and reserve both affect
-	// the capacity. Neither of them change the
-	// length of the built array.
-	b.capacity = n
-}
-func (b *StringBuilder) NewArray() Interface {
-	return b.NewStringArray()
-}
-func (b *StringBuilder) NewStringArray() *String {
-	arr := &String{}
-	if b.builder == nil {
-		arr.value, arr.length = b.value, b.length
-	} else {
-		arr.data = b.builder.NewBinaryArray()
-	}
-	b.reset()
-	return arr
 }
 
 type sliceable interface {
