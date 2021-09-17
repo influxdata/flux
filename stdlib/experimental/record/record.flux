633a72b4c9fe9f8f5c10762818f1f9252b45ac0b
Ales Pour <ales.pour@bonitoo.io>
2021-09-17 16:57:01 +0200

    feat: add get method

--- stdlib/experimental/record/record.flux
+++ stdlib/experimental/record/record.flux
@@ -3,3 +3,6 @@ package record
 
 // any is a record that contains no properties but according to its type may contain any additional properties.
 builtin any : A where A: Record
+
+// get returns record field identified by key or default if no such field exist in the record.
+builtin get : (r: A, key: string, default: B) => B where A: Record
--- stdlib/experimental/record/record.go
+++ stdlib/experimental/record/record.go
@@ -1,10 +1,43 @@
 package record
 
 import (
+	"context"
+
+	"github.com/influxdata/flux/interpreter"
 	"github.com/influxdata/flux/runtime"
 	"github.com/influxdata/flux/values"
 )
 
+const packagePath = "experimental/record"
+
 func init() {
-	runtime.RegisterPackageValue("experimental/record", "any", values.NewObjectWithValues(nil))
+	runtime.RegisterPackageValue(packagePath, "any", values.NewObjectWithValues(nil))
+	runtime.RegisterPackageValue(packagePath, "get", values.NewFunction(
+		"get",
+		runtime.MustLookupBuiltinType(packagePath, "get"),
+		func(ctx context.Context, args values.Object) (values.Value, error) {
+			return interpreter.DoFunctionCallContext(func(ctx context.Context, args interpreter.Arguments) (values.Value, error) {
+				r, err := args.GetRequiredObject("r")
+				if err != nil {
+					return nil, err
+				}
+
+				key, err := args.GetRequiredString("key")
+				if err != nil {
+					return nil, err
+				}
+
+				def, err := args.GetRequired("default")
+				if err != nil {
+					return nil, err
+				}
+
+				if v, ok := r.Get(key); ok {
+					return v, nil
+				}
+
+				return def, nil
+			}, ctx, args)
+		}, false,
+	))
 }
