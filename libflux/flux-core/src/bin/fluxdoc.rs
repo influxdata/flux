1186e4b7e1fb0ec373be94da38d9e7c6dbecfea7
Scott Anderson <scott@influxdata.com>
2021-11-01 16:39:47 -0600

    feat(docs): add inline documentation to the dict package

--- libflux/flux-core/src/bin/fluxdoc.rs
+++ libflux/flux-core/src/bin/fluxdoc.rs
@@ -237,7 +237,7 @@ impl<'a> example::Executor for CLIExecutor<'a> {
 // high standard going forward.
 //
 // See https://github.com/influxdata/flux/issues/4141 for tacking removing of this list.
-const EXCEPTIONS: [&str; 98] = [
+const EXCEPTIONS: [&str; 97] = [
     "array",
     "contrib",
     "contrib/RohanSreerama5",
@@ -272,7 +272,6 @@ const EXCEPTIONS: [&str; 98] = [
     "contrib/tomhollingworth/events",
     "csv",
     "date",
-    "dict",
     "experimental",
     "experimental/aggregate",
     "experimental/array",
