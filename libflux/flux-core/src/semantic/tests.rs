0e7b49322cf20087a0a71b825099120519bf8863
Markus Westerlind <mwesterlind@influxdata.com>
2021-11-16 12:19:18 +0100

    feat: Add locations to conversion errors
    The location is readily available so there is little reason not to include it.

--- libflux/flux-core/src/semantic/tests.rs
+++ libflux/flux-core/src/semantic/tests.rs
@@ -295,6 +295,29 @@ macro_rules! test_infer_err {
 /// ```
 ///
 macro_rules! test_error_msg {
+    ( $(imp: $imp:expr,)? $(env: $env:expr,)? src: $src:expr $(,)?, expect: $expect:expr $(,)? ) => {{
+        #[allow(unused_mut, unused_assignments)]
+        let mut imp = HashMap::default();
+        $(
+            imp = $imp;
+        )?
+        #[allow(unused_mut, unused_assignments)]
+        let mut env = HashMap::default();
+        $(
+            env = $env;
+        )?
+        match infer_types(
+            $src,
+            env,
+            imp,
+            None,
+            AnalyzerConfig::default(),
+        ) {
+            Err(e) => $expect.assert_eq(&e.to_string()),
+            Ok(_) => panic!("expected error, instead program passed type checking"),
+        }
+    }};
+
     ( $(imp: $imp:expr,)? $(env: $env:expr,)? src: $src:expr $(,)?, err: $err:expr $(,)? ) => {{
         #[allow(unused_mut, unused_assignments)]
         let mut imp = HashMap::default();
@@ -3667,5 +3690,25 @@ fn primitive_kind_errors() {
         err: "error @2:13-2:42: {} is not Basic (argument v)
 
 error @3:13-3:41: [A] is not Basic (argument v)",
+    }
+}
+
+#[test]
+fn invalid_mono_type() {
+    test_error_msg! {
+        src: r#"
+            builtin x : abc
+        "#,
+        expect: expect_test::expect![[r#"error @2:25-2:28: invalid named type abc"#]]
+    }
+}
+
+#[test]
+fn missing_return() {
+    test_error_msg! {
+        src: r#"
+            () => { }
+        "#,
+        expect: expect_test::expect![[r#"error @2:19-2:22: missing return statement in block"#]]
     }
 }
