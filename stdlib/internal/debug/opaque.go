07e315c4f9804513c1d917e094d15ccb57b69bb5
Christopher M. Wolff <chris.wolff@influxdata.com>
2021-12-17 17:01:38 -0800

    feat: add an internal/debug.opaque function (#4363)

---
+++ stdlib/internal/debug/opaque.go
@@ -0,0 +1,35 @@
+package debug
+
+import (
+	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/runtime"
+)
+
+const OpaqueKind = "internal/debug.opaque"
+
+type OpaqueOpSpec struct{}
+
+func init() {
+	opaqueSig := runtime.MustLookupBuiltinType("internal/debug", "opaque")
+
+	runtime.RegisterPackageValue("internal/debug", "opaque", flux.MustValue(flux.FunctionValue(OpaqueKind, createOpaqueOpSpec, opaqueSig)))
+	flux.RegisterOpSpec(OpaqueKind, newOpaqueOp)
+	// opaque uses the same procedure spec and transformation as pass, so we only need to
+	// create and register the op spec here.
+}
+
+func createOpaqueOpSpec(args flux.Arguments, a *flux.Administration) (flux.OperationSpec, error) {
+	if err := a.AddParentFromArgs(args); err != nil {
+		return nil, err
+	}
+
+	return new(OpaqueOpSpec), nil
+}
+
+func newOpaqueOp() flux.OperationSpec {
+	return new(OpaqueOpSpec)
+}
+
+func (s *OpaqueOpSpec) Kind() flux.OperationKind {
+	return OpaqueKind
+}
