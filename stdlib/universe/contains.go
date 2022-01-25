f5a0e8b52add70140c9f3b5b3f523f84ce17037a
Owen Nelson <onelson@gmail.com>
2022-01-25 11:46:22 -0800

    fix(universe): return false if contains gets invalid value (#4424)
    * test: test that contains gives `false` when value is Invalid (fails)
    
    * fix(universe): return `false` if `contains` gets invalid value
    
    Flux treats the access of a missing field in the `value` parameter for
    `contains` as a hard error, whereas similar access in `filter` is
    treated as "falsy."
    
    As noted in <https://github.com/influxdata/influxdb/issues/23025>, it's
    possible the error behavior was introduced as a breaking change accidentally
    in <https://github.com/influxdata/flux/pull/2696> since a user raised the
    issue as something that worked previously but  no longer does.
    
    This patch checks to see if `value` is invalid and immediately returns
    `false` if it is, sidestepping the type check between the `value` and
    `set` parameters that produces the error.
    
    Refs:
    - <https://github.com/influxdata/flux/issues/4381>
    - <https://github.com/influxdata/influxdb/issues/23025>
    - <https://github.com/influxdata/flux/pull/2696>
    
    * chore: make generate
    
    * refactor: skip invalid values in `set` array
    
    If we want to be permissive when `contains` receives `Invalid` values at
    runtime, it makes sense to skip any `Invalid` items in `set` in addition
    to the early (`false`) return.
    
    This comes at the cost of being able to fail fast with a strict
    comparison to decide the type of the `set` array. Now we check the type
    and nature of each item as we iterate.
    
    * test: ordering of items in set is significant
    
    * chore: make generate

--- stdlib/universe/contains.go
+++ stdlib/universe/contains.go
@@ -7,6 +7,7 @@ import (
 	"github.com/influxdata/flux/internal/errors"
 	"github.com/influxdata/flux/interpreter"
 	"github.com/influxdata/flux/runtime"
+	"github.com/influxdata/flux/semantic"
 	"github.com/influxdata/flux/values"
 )
 
@@ -24,6 +25,14 @@ func MakeContainsFunc() values.Function {
 				return nil, err
 			}
 
+			// In situations where value `v` is "invalid" (like when it's a field that is missing from an incoming
+			// unknown record type, we give a surprising and confusing error message indicating
+			// `keyword argument "set" should be of an array of type invalid`.
+			// This error is normally produced by the `GetRequiredArrayAllowEmpty` call immediately below.
+			// To avoid the confusion and handle this case more intuitively, short-circuit with a "false" outcome.
+			if v.Type().Nature() == semantic.Invalid {
+				return values.NewBool(false), nil
+			}
 			setarg, err := a.GetRequiredArrayAllowEmpty("set", v.Type().Nature())
 			if err != nil {
 				return nil, err
@@ -35,15 +44,19 @@ func MakeContainsFunc() values.Function {
 			if _, ok := set.(values.TableObject); ok {
 				return nil, errors.New(codes.Invalid, "set cannot be a table stream; expected an array")
 			}
+
 			if set.Len() > 0 {
-				if set.Get(0).Type() != v.Type() {
-					err = errors.Newf(codes.Invalid, "value type %T does not match set type %T", v.Type(), set.Get(0).Type())
-				} else {
-					for i := 0; i < set.Len(); i++ {
-						if set.Get(i).Equal(v) {
-							found = true
-							break
-						}
+				for i := 0; i < set.Len(); i++ {
+					// Skip any members of the `set` array that are invalid.
+					if set.Get(i).Type().Nature() == semantic.Invalid {
+						continue
+					} else if set.Get(i).Type() != v.Type() {
+						err = errors.Newf(codes.Invalid, "value type %T does not match set type %T", v.Type(), set.Get(0).Type())
+						break
+					}
+					if set.Get(i).Equal(v) {
+						found = true
+						break
 					}
 				}
 			}
