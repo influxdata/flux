86607ff291b7f319748213044723d35e05869684
Jonathan A. Sternberg <jonathan@influxdata.com>
2022-02-14 11:49:37 -0600

    refactor: remove detailed query traces
    The detailed query traces weren't really practical as they commonly
    caused the tracing program, jaeger, to fall over with traces that were
    too large.
    
    This removes the code from the engine because we can't practically turn
    it on.

--- internal/cmd/flux/main.go
+++ internal/cmd/flux/main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io/ioutil"
 	"os"
 
-	"github.com/influxdata/flux"
-	"github.com/influxdata/flux/codes"
 	"github.com/influxdata/flux/dependencies"
 	"github.com/influxdata/flux/dependency"
 	"github.com/influxdata/flux/fluxinit"
-	"github.com/influxdata/flux/internal/errors"
-	"github.com/opentracing/opentracing-go"
 	"github.com/spf13/cobra"
-	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
 
 var flags struct {
 	ExecScript bool
-	Trace      string
 	Format     string
 }
 
@@ -37,56 +30,17 @@ func runE(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	ctx, close, err := configureTracing(context.Background())
-	if err != nil {
-		return err
-	}
-	defer close()
-
 	// Defer initialization until other common errors
 	// have already passed to avoid a long load time
 	// for a simple unrelated error.
 	fluxinit.FluxInit()
-	ctx, span := injectDependencies(ctx)
+	ctx, span := injectDependencies(context.Background())
 	defer span.Finish()
 
 	if len(args) == 0 {
 		return replE(ctx)
 	}
 	return executeE(ctx, script, flags.Format)
-}
-
-func configureTracing(ctx context.Context) (context.Context, func(), error) {
-	if flags.Trace == "" {
-		return ctx, func() {}, nil
-	} else if flags.Trace != "jaeger" {
-		return nil, nil, errors.Newf(codes.Invalid, "unknown tracer name: %s", flags.Trace)
-	}
-
-	cfg, err := jaegercfg.FromEnv()
-	if err != nil {
-		return nil, nil, err
-	}
-	if cfg.ServiceName == "" {
-		cfg.ServiceName = "flux"
-	}
-	if cfg.Sampler.Type == "" {
-		cfg.Sampler.Type = "const"
-		cfg.Sampler.Param = 1.0
-	}
-
-	tracer, closer, err := cfg.NewTracer()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	opentracing.SetGlobalTracer(tracer)
-	ctx = flux.WithQueryTracingEnabled(ctx)
-	return ctx, func() {
-		if err := closer.Close(); err != nil {
-			fmt.Printf("error closing tracer: %s.\n", err)
-		}
-	}, nil
 }
 
 const DefaultInfluxDBHost = "http://localhost:9999"
@@ -103,9 +57,7 @@ func main() {
 		RunE: runE,
 	}
 	cmd.Flags().BoolVarP(&flags.ExecScript, "exec", "e", false, "Interpret file argument as a raw flux script")
-	cmd.Flags().StringVar(&flags.Trace, "trace", "", "Trace query execution")
 	cmd.Flags().StringVarP(&flags.Format, "format", "", "cli", "Output format one of: cli,csv. Defaults to cli")
-	cmd.Flag("trace").NoOptDefVal = "jaeger"
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
