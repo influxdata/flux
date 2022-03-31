7c37c5bf0bb1d510807a2f3c21d5e51a37548fc8
Christopher M. Wolff <chris.wolff@influxdata.com>
2022-03-31 10:14:09 -0700

    fix: add the error to the span created for a transformation (#4618)
    * fix: add the error to the span created for a transformation
    
    * fix: update test to make it deterministic

--- execute/transport.go
+++ execute/transport.go
@@ -223,8 +223,8 @@ func (t *consecutiveTransport) contextWithSpan(ctx context.Context) context.Cont
 	return opentracing.ContextWithSpan(ctx, t.span)
 }
 
-func (t *consecutiveTransport) finishSpan() {
-	t.span.LogFields(log.Int("messages_processed", int(atomic.LoadInt32(&t.totalMsgs))))
+func (t *consecutiveTransport) finishSpan(err error) {
+	t.span.LogFields(log.Int("messages_processed", int(atomic.LoadInt32(&t.totalMsgs))), log.Error(err))
 	t.span.Finish()
 }
 
@@ -251,7 +251,7 @@ PROCESS:
 				}
 				// We are finished
 				close(t.finished)
-				t.finishSpan()
+				t.finishSpan(err)
 				return
 			}
 		}
