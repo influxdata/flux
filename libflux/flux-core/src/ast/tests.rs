10b7aab6cb1925177cf3bf93f07792ea5b8a46a8
Markus Westerlind <mwesterlind@influxdata.com>
2022-04-11 14:37:24 +0200

    refactor: Update json tests

--- libflux/flux-core/src/ast/tests.rs
+++ libflux/flux-core/src/ast/tests.rs
@@ -598,10 +598,10 @@ fn test_json_record() {
         }],
     });
     let serialized = serde_json::to_string(&n).unwrap();
-    assert_eq!(
-        serialized,
-        r#"{"type":"RecordType","tvar":{"name":"A"},"properties":[{"name":{"name":"A"},"monotype":{"type":"NamedType","name":{"name":"int"}}}]}"#
-    );
+    expect_test::expect![[
+        r#"{"type":"RecordType","tvar":{"name":"A"},"properties":[{"name":{"type":"Identifier","name":"A"},"monotype":{"type":"NamedType","name":{"name":"int"}}}]}"#
+    ]]
+            .assert_eq(&serialized);
     let deserialized: MonoType = serde_json::from_str(serialized.as_str()).unwrap();
     assert_eq!(deserialized, n)
 }
@@ -639,10 +639,9 @@ fn test_json_record_no_tvar() {
         }],
     });
     let serialized = serde_json::to_string(&n).unwrap();
-    assert_eq!(
-        serialized,
-        r#"{"type":"RecordType","properties":[{"name":{"name":"A"},"monotype":{"type":"NamedType","name":{"name":"int"}}}]}"#
-    );
+    expect_test::expect![[
+        r#"{"type":"RecordType","properties":[{"name":{"type":"Identifier","name":"A"},"monotype":{"type":"NamedType","name":{"name":"int"}}}]}"#
+    ]].assert_eq(&serialized);
     let deserialized: MonoType = serde_json::from_str(serialized.as_str()).unwrap();
     assert_eq!(deserialized, n)
 }
