6f674b43a3dad99fbc1a4ed0046314527c0580b8
Jonathan A. Sternberg <jonathan@influxdata.com>
2022-04-14 15:58:35 -0500

    feat(cmd): add trace option to the flux cli

--- cmd/flux/main.go
+++ cmd/flux/main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
 
 	"github.com/influxdata/flux/cmd/flux/cmd"
+	"github.com/influxdata/flux/codes"
 	"github.com/influxdata/flux/dependencies"
 	"github.com/influxdata/flux/dependency"
 	"github.com/influxdata/flux/fluxinit"
+	"github.com/influxdata/flux/internal/errors"
+	"github.com/opentracing/opentracing-go"
 	"github.com/spf13/cobra"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
 
 var flags struct {
 	ExecScript bool
+	Trace      string
 	Format     string
 }
 
@@ -31,17 +37,55 @@ func runE(cmd *cobra.Command, args []string) error {
 		}
 	}
 
+	ctx, close, err := configureTracing(context.Background())
+	if err != nil {
+		return err
+	}
+	defer close()
+
 	// Defer initialization until other common errors
 	// have already passed to avoid a long load time
 	// for a simple unrelated error.
 	fluxinit.FluxInit()
-	ctx, span := injectDependencies(context.Background())
+	ctx, span := injectDependencies(ctx)
 	defer span.Finish()
 
 	if len(args) == 0 {
 		return replE(ctx)
 	}
 	return executeE(ctx, script, flags.Format)
+}
+
+func configureTracing(ctx context.Context) (context.Context, func(), error) {
+	if flags.Trace == "" {
+		return ctx, func() {}, nil
+	} else if flags.Trace != "jaeger" {
+		return nil, nil, errors.Newf(codes.Invalid, "unknown tracer name: %s", flags.Trace)
+	}
+
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		return nil, nil, err
+	}
+	if cfg.ServiceName == "" {
+		cfg.ServiceName = "flux"
+	}
+	if cfg.Sampler.Type == "" {
+		cfg.Sampler.Type = "const"
+		cfg.Sampler.Param = 1.0
+	}
+
+	tracer, closer, err := cfg.NewTracer()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	opentracing.SetGlobalTracer(tracer)
+	return ctx, func() {
+		if err := closer.Close(); err != nil {
+			fmt.Printf("error closing tracer: %s.\n", err)
+		}
+	}, nil
 }
 
 const DefaultInfluxDBHost = "http://localhost:9999"
@@ -58,7 +102,9 @@ func main() {
 		RunE: runE,
 	}
 	fluxCmd.Flags().BoolVarP(&flags.ExecScript, "exec", "e", false, "Interpret file argument as a raw flux script")
+	fluxCmd.Flags().StringVar(&flags.Trace, "trace", "", "Trace query execution")
 	fluxCmd.Flags().StringVarP(&flags.Format, "format", "", "cli", "Output format one of: cli,csv. Defaults to cli")
+	fluxCmd.Flag("trace").NoOptDefVal = "jaeger"
 
 	fmtCmd := &cobra.Command{
 		Use:           "fmt",
