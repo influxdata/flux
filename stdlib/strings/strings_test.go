397dc38839ffcdbbe8669f22ee76eec45d5a2829
Sunil Kartikey <s.kartikey@gmail.com>
2022-04-18 19:47:42 +0100

    fix(stdlib/strings): strings.joinStr panic when receives nil values
    fixes: #4554 https://github.com/influxdata/EAR/issues/3066

--- stdlib/strings/strings_test.go
+++ stdlib/strings/strings_test.go
@@ -8,9 +8,16 @@ package strings_test
 import (
 	"context"
 
+	"testing"
+
+	"github.com/influxdata/flux/dependency"
+	fluxstdlibstrings "github.com/influxdata/flux/stdlib/strings"
+
+	"github.com/influxdata/flux/dependencies/dependenciestest"
 	_ "github.com/influxdata/flux/fluxinit/static"
 	"github.com/influxdata/flux/runtime"
-	"testing"
+	"github.com/influxdata/flux/semantic"
+	"github.com/influxdata/flux/values"
 )
 
 func TestJoinStr_ReceiveTableObjectIsError(t *testing.T) {
@@ -25,5 +32,25 @@ func TestJoinStr_ReceiveTableObjectIsError(t *testing.T) {
 
 	if want, got := "error @4:59-4:83: expected [string] (array) but found stream[string] (argument arr)", err.Error(); want != got {
 		t.Errorf("wanted error %q, got %q", want, got)
+	}
+}
+
+func TestJoinStr_NullInArrParam(t *testing.T) {
+	fluxFunc := fluxstdlibstrings.SpecialFns["joinStr"]
+	arr := values.NewArrayWithBacking(semantic.NewArrayType(semantic.BasicString), []values.Value{
+		values.NewString("a"), values.NewString("b"), values.NewNull(semantic.BasicString)})
+	fluxArg := values.NewObjectWithValues(map[string]values.Value{"arr": arr, "v": values.NewString(", ")})
+	wantErr := "expected elements of argument \"arr\" to be of type string, got type string value <nil>"
+	ctx, deps := dependency.Inject(context.Background(), dependenciestest.Default())
+	defer deps.Finish()
+	gotErr, err := fluxFunc.Call(ctx, fluxArg)
+	if err != nil {
+		if gotErr, wantErr := err.Error(), wantErr; gotErr != wantErr {
+			t.Errorf("unexpected error - wantErr: %s, gotErr: %s", wantErr, gotErr)
+		}
+		return
+	}
+	if wantErr != gotErr.Str() {
+		t.Errorf("input %f: expected %v, gotErr %f", arr, wantErr, gotErr)
 	}
 }
