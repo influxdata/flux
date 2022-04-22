ab673a42b4fabe7f064f5a460d51c2f238ff0c0d
Scott Anderson <scott@influxdata.com>
2022-04-22 13:19:40 -0600

    chore(docs): corrected version for types package

--- libflux/go/libflux/buildinfo.gen.go
+++ libflux/go/libflux/buildinfo.gen.go
@@ -405,7 +405,7 @@ var sourceHashes = map[string]string{
 	"stdlib/testing/usage/writes_test.flux":                                                       "88ddcba45de796c2794e93483f1dbc425873df979f9881c92dc6339a5dad07ac",
 	"stdlib/timezone/timezone.flux":                                                               "c71dad5331723b0ca1ebf4c46febf0d3360b753f3f223a26ff97d2e4e2d2d26c",
 	"stdlib/types/is_type_test.flux":                                                              "66daa27dea82fff1325bd6823274a2946ce75e4e27c9b6068c2f2c82677f7257",
-	"stdlib/types/types.flux":                                                                     "746b0438da8d959979a69c19ae4fa972268bd3a7db965e214747402c3528ecd5",
+	"stdlib/types/types.flux":                                                                     "5be1c610a4cabf7ee783d7a4f9eb9e6d9218908092b1e6da73e28cfaa499ecd4",
 	"stdlib/universe/aggregate_empty_window_count_test.flux":                                      "39ac3741edab42fc471bcee92c70db37ad62c23ed3cb5574c238235d9bd9f9a0",
 	"stdlib/universe/aggregate_empty_window_first_test.flux":                                      "9aead8a23604ed2337dfffbd49e2429c80a5f6ba92ae71f96ad7fe6d91f1a23f",
 	"stdlib/universe/aggregate_empty_window_last_test.flux":                                       "f2f3b6c72ce466fa91f4c6ebf6c2862bf2c20f0e24fde113882fb51fc46c7be8",
