f8b977d12513a627a7baf1f78a218b015883223a
Markus Westerlind <mwesterlind@influxdata.com>
2022-05-19 14:16:01 +0200

    feat: Add a planner rule that expands logical join nodes (#4765)
    * feat: Add a planner rule that expands logical join nodes
    
    Just implements the pattern matching and tests that the rule is used. There isn't an actual
    transformation that makes use of this.
    
    Closes #4627
    
    * fix: Actually inject sort in sortmergejoin
    
    * fix: Create sorts using the columns determined by equijoin
    
    * chore: address review comments

---
+++ stdlib/join/sort_merge_join.go
@@ -0,0 +1,108 @@
+package join
+
+import (
+	"context"
+
+	"github.com/influxdata/flux/codes"
+	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/internal/errors"
+	"github.com/influxdata/flux/memory"
+	"github.com/influxdata/flux/plan"
+	"github.com/influxdata/flux/stdlib/universe"
+)
+
+const SortMergeJoinKind = "sortmergejoin"
+
+func init() {
+	plan.RegisterPhysicalRules(SortMergeJoinPredicateRule{})
+	execute.RegisterTransformation(SortMergeJoinKind, createSortMergeJoinTransformation)
+}
+
+func createSortMergeJoinTransformation(id execute.DatasetID, mode execute.AccumulationMode, spec plan.ProcedureSpec, a execute.Administration) (execute.Transformation, execute.Dataset, error) {
+	s, ok := spec.(*SortMergeJoinProcedureSpec)
+	if !ok {
+		return nil, nil, errors.Newf(codes.Internal, "invalid spec type %T", spec)
+	}
+
+	return newSortMergeJoinTransformation(id, s, a.Allocator())
+}
+
+func newSortMergeJoinTransformation(id execute.DatasetID, s *SortMergeJoinProcedureSpec, mem memory.Allocator) (execute.Transformation, execute.Dataset, error) {
+	return nil, nil, errors.Newf(codes.Internal, "sort merge join transformation is not implemented")
+}
+
+type SortMergeJoinProcedureSpec EquiJoinProcedureSpec
+
+func (p *SortMergeJoinProcedureSpec) Kind() plan.ProcedureKind {
+	return plan.ProcedureKind(SortMergeJoinKind)
+}
+
+func (p *SortMergeJoinProcedureSpec) Copy() plan.ProcedureSpec {
+	return &SortMergeJoinProcedureSpec{
+		On:     p.On,
+		As:     p.As,
+		Left:   p.Left,
+		Right:  p.Right,
+		Method: p.Method,
+	}
+}
+
+func (p *SortMergeJoinProcedureSpec) Cost(inStats []plan.Statistics) (cost plan.Cost, outStats plan.Statistics) {
+	return plan.Cost{}, plan.Statistics{}
+}
+
+type SortMergeJoinPredicateRule struct{}
+
+func (SortMergeJoinPredicateRule) Name() string {
+	return "sortMergeJoinPredicate"
+}
+
+func (SortMergeJoinPredicateRule) Pattern() plan.Pattern {
+	return plan.Pat(EquiJoinKind, plan.Any(), plan.Any())
+}
+
+func (SortMergeJoinPredicateRule) Rewrite(ctx context.Context, n plan.Node) (plan.Node, bool, error) {
+	s := n.ProcedureSpec()
+	spec, ok := s.(*EquiJoinProcedureSpec)
+	if !ok {
+		return nil, false, errors.New(codes.Internal, "invalid spec type on join node")
+	}
+
+	predecessors := n.Predecessors()
+	n.ClearPredecessors()
+
+	makeSortNode := func(parentNode plan.Node, columns []string) *plan.PhysicalPlanNode {
+		sortProc := universe.SortProcedureSpec{
+			Columns: columns,
+		}
+		sortNode := plan.CreateUniquePhysicalNode(ctx, "sortMergeJoin", &sortProc)
+
+		sortNode.AddPredecessors(parentNode)
+		sortNode.AddSuccessors(n)
+		n.AddPredecessors(sortNode)
+
+		return sortNode
+	}
+
+	successors := predecessors[0].Successors()
+
+	columns := make([]string, len(spec.On))
+	for _, pair := range spec.On {
+		columns = append(columns, pair.Left)
+	}
+	successors[0] = makeSortNode(predecessors[0], columns)
+
+	successors = predecessors[1].Successors()
+
+	columns = make([]string, len(spec.On))
+	for _, pair := range spec.On {
+		columns = append(columns, pair.Right)
+	}
+	successors[0] = makeSortNode(predecessors[1], columns)
+
+	// Replace the spec so we don't end up trying to apply this rewrite forever
+	x := SortMergeJoinProcedureSpec(*spec)
+	n.ReplaceSpec(&x)
+
+	return n, true, nil
+}
---
+++ stdlib/join/sortmergejoin_test.go
@@ -0,0 +1,105 @@
+package join_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	_ "github.com/influxdata/flux/fluxinit/static"
+	"github.com/influxdata/flux/plan"
+	"github.com/influxdata/flux/plan/plantest"
+	"github.com/influxdata/flux/stdlib/influxdata/influxdb"
+	"github.com/influxdata/flux/stdlib/join"
+	"github.com/influxdata/flux/stdlib/universe"
+)
+
+func TestSortMergeJoinPredicateRule(t *testing.T) {
+	now := time.Now().UTC()
+	testCases := []struct {
+		name     string
+		flux     string
+		wantErr  error
+		wantPlan *plantest.PlanSpec
+	}{
+		{
+			name: "single comparison",
+			flux: `import "join"
+			left = from(bucket: "b1", host: "http://localhost:8086")
+				|> filter(fn: (r) => r._measurement == "a")
+			right = from(bucket: "b2", host: "http://localhost:8086")
+				|> filter(fn: (r) => r._measurement == "b")
+			join.join(
+				left: left,
+				right: right,
+				on: (l, r) => l.a == r.b,
+				as: (l, r) => ({l with c: r._value}),
+				method: "inner",
+			)`,
+			wantPlan: &plantest.PlanSpec{
+				Nodes: []plan.Node{
+					plan.CreateLogicalNode("from0", &influxdb.FromProcedureSpec{}),
+					plan.CreateLogicalNode("filter1", &universe.FilterProcedureSpec{}),
+					plan.CreateLogicalNode("sort2", &universe.SortProcedureSpec{}),
+					plan.CreateLogicalNode("from3", &influxdb.FromProcedureSpec{}),
+					plan.CreateLogicalNode("filter4", &universe.FilterProcedureSpec{}),
+					plan.CreateLogicalNode("sort5", &universe.SortProcedureSpec{}),
+					plan.CreateLogicalNode("join.join6", &join.SortMergeJoinProcedureSpec{}),
+				},
+				Edges: [][2]int{
+					{0, 1},
+					{1, 2},
+					{2, 6},
+					{3, 4},
+					{4, 5},
+					{5, 6},
+				},
+				Now: now,
+			},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			fluxSpec, err := compile(tc.flux, now)
+			if err != nil {
+				t.Fatalf("could not compile flux query: %v", err)
+			}
+
+			logicalPlanner := plan.NewLogicalPlanner()
+			initPlan, err := logicalPlanner.CreateInitialPlan(fluxSpec)
+			if err != nil {
+				t.Fatal(err)
+			}
+			logicalPlan, err := logicalPlanner.Plan(context.Background(), initPlan)
+			if err != nil {
+				t.Fatal(err)
+			}
+			physicalPlanner := plan.NewPhysicalPlanner(plan.OnlyPhysicalRules(
+				&join.EquiJoinPredicateRule{},
+				&join.SortMergeJoinPredicateRule{},
+			))
+			physicalPlan, err := physicalPlanner.Plan(context.Background(), logicalPlan)
+			if err != nil {
+				if tc.wantErr != nil {
+					if tc.wantErr.Error() != err.Error() {
+						t.Fatalf("expected error: %s - got %s", tc.wantErr, err)
+					}
+					return
+				} else {
+					t.Fatalf("got unexpected error: %s", err)
+				}
+			} else {
+				if tc.wantErr != nil {
+					t.Fatalf("expected error `%s` - got none", tc.wantErr)
+				}
+			}
+
+			wantPlan := plantest.CreatePlanSpec(tc.wantPlan)
+			if err := plantest.ComparePlansShallow(wantPlan, physicalPlan); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
