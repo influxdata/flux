Westonside/flux_headlessrepl

chore: Remove the VectorizeLogicalOperators flag
To be merged once we are confident enough to stabilize this feature.

Closes #4600

libflux/go/libflux/buildinfo.gen.go
@@ -47,12 +47,12 @@ var sourceHashes = map[string]string{
 	"libflux/flux-core/src/semantic/fs.rs":                                                        "f7f609bc8149769d99b737150e184a2d54029c0b768365dbcf08ff193b0e1f6f",
 	"libflux/flux-core/src/semantic/import.rs":                                                    "184e955211db1ceb1be782b4daf75584b86907b1428e50015497909cfc2dd89a",
 	"libflux/flux-core/src/semantic/infer.rs":                                                     "fc8e7ab4b1339c7f384e3b4223364bcc7b81e04eb01bbd4e02c5426392f5abab",
-	"libflux/flux-core/src/semantic/mod.rs":                                                       "c09e559d5495929a2f8aee9e513ec7a2a8f521456c13e50d5c1131bc101620ca",
+	"libflux/flux-core/src/semantic/mod.rs":                                                       "f4f6870e15d158d5c99fc0e1fd8a35e37514ef8acd8a650fa138f4bfdf7add65",
 	"libflux/flux-core/src/semantic/nodes.rs":                                                     "c167257c25ea41e1f8ccfa6bb942cbb5c87bf392b5d2346f32e75ede13d38974",
 	"libflux/flux-core/src/semantic/sub.rs":                                                       "043d92de0ca86ffb93fc4fcb7256bcbba8026016f45792dcaf4597812e8b3b4c",
 	"libflux/flux-core/src/semantic/symbols.rs":                                                   "ddbceca632ca384c6bb461a660e02781c43295025f2dd10f1ea997131dd5eb30",
 	"libflux/flux-core/src/semantic/types.rs":                                                     "2f2c791c585866f06912e417eb7df3f9efa2cbb129b87c61391df7b619a34051",
-	"libflux/flux-core/src/semantic/vectorize.rs":                                                 "7024304c2f2accc57ef44cb4e739e184bee3d217bf2bf38a957edfdee935d6b4",
+	"libflux/flux-core/src/semantic/vectorize.rs":                                                 "d070ed16e51d5c7557ed5281b2ec19b26a2311b6ac44c5ad17a3435545af8682",
 	"libflux/flux-core/src/semantic/walk/_walk.rs":                                                "c3d04e72cfbe595d4919b84f4df4bc6c55297516cf8e12e6cb691f48be648291",
 	"libflux/flux-core/src/semantic/walk/mod.rs":                                                  "f71aac086dd1d7b730e24bac690a3e47a9bfe575cd6cba499af67db9b920c726",
 	"libflux/flux-core/src/semantic/walk/test_utils.rs":                                           "a4a594ed40ff8a8297752f1ba618abd8b94f30a649afa59bc7c26129dd192fd8",

influxdata/flux

libflux/go/libflux/buildinfo.gen.go
@@ -47,12 +47,12 @@ var sourceHashes = map[string]string{
 	"libflux/flux-core/src/semantic/fs.rs":                                                        "f7f609bc8149769d99b737150e184a2d54029c0b768365dbcf08ff193b0e1f6f",
 	"libflux/flux-core/src/semantic/import.rs":                                                    "184e955211db1ceb1be782b4daf75584b86907b1428e50015497909cfc2dd89a",
 	"libflux/flux-core/src/semantic/infer.rs":                                                     "fc8e7ab4b1339c7f384e3b4223364bcc7b81e04eb01bbd4e02c5426392f5abab",
-	"libflux/flux-core/src/semantic/mod.rs":                                                       "7dbc3bf025770b10b0bd97226055dda8eae8af8aeb8489243b869a5015bbccbb",
+	"libflux/flux-core/src/semantic/mod.rs":                                                       "acd901ec56a8e7b37b6ef7f977a1194dffee4db07cfa08e96f5d779fad731429",
 	"libflux/flux-core/src/semantic/nodes.rs":                                                     "c4876ff84fdc0196e3b41730f9699d2b366eb385a2ff22810e15992ee56031ef",
 	"libflux/flux-core/src/semantic/sub.rs":                                                       "043d92de0ca86ffb93fc4fcb7256bcbba8026016f45792dcaf4597812e8b3b4c",
 	"libflux/flux-core/src/semantic/symbols.rs":                                                   "ddbceca632ca384c6bb461a660e02781c43295025f2dd10f1ea997131dd5eb30",
 	"libflux/flux-core/src/semantic/types.rs":                                                     "190ba7823fa0f42262747ca46eb7b43f2b24c8247c5e88ea67e3b735343ad9cf",
-	"libflux/flux-core/src/semantic/vectorize.rs":                                                 "7024304c2f2accc57ef44cb4e739e184bee3d217bf2bf38a957edfdee935d6b4",
+	"libflux/flux-core/src/semantic/vectorize.rs":                                                 "d070ed16e51d5c7557ed5281b2ec19b26a2311b6ac44c5ad17a3435545af8682",
 	"libflux/flux-core/src/semantic/walk/_walk.rs":                                                "c3d04e72cfbe595d4919b84f4df4bc6c55297516cf8e12e6cb691f48be648291",
 	"libflux/flux-core/src/semantic/walk/mod.rs":                                                  "ce740f3c33b3bb08ce4c19d0bd249fc38a50b4c3523ae2e282f1069cb360f63e",
 	"libflux/flux-core/src/semantic/walk/test_utils.rs":                                           "043d5137ed626f1990c89f639e867e5b33a906f13c427313b497062f98215846",