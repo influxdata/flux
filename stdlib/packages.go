df33603f9bffde066f798026fbf78e3a649de01c
Nguyen Trinh <trinh.nguyen2@monstar-lab.com>
2022-07-06 11:53:23 +0700

    Revert "clone tomhollingworth/events -> tomhollingworth/status"
    This reverts commit 7e6219462de4a9ac6695ef676a34ee11bd9da4b2.

--- stdlib/packages.go
+++ stdlib/packages.go
@@ -25,7 +25,6 @@ import (
 	_ "github.com/influxdata/flux/stdlib/contrib/sranka/telegram"
 	_ "github.com/influxdata/flux/stdlib/contrib/sranka/webexteams"
 	_ "github.com/influxdata/flux/stdlib/contrib/tomhollingworth/events"
-	_ "github.com/influxdata/flux/stdlib/contrib/tomhollingworth/status"
 	_ "github.com/influxdata/flux/stdlib/csv"
 	_ "github.com/influxdata/flux/stdlib/date"
 	_ "github.com/influxdata/flux/stdlib/dict"
