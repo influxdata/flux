ca075143682940cc47fdd34d216d6f80abff1c15
Markus Westerlind <mwesterlind@influxdata.com>
2022-08-11 12:12:29 +0200

    fix: Don't include null columns when unpivoting
    The pivoted data we get from IOx may contain fields with nulls which we must exclude from the unpivoted chunk so they do not show up as extra rows with `null` as the `_value`.

--- stdlib/experimental/unpivot.go
+++ stdlib/experimental/unpivot.go
@@ -88,7 +88,7 @@ func (t *unpivotTransformation) Process(chunk table.Chunk, d *execute.TransportD
 
 	timeColumn := -1
 	for j, c := range chunk.Cols() {
-		if c.Label == "_time" {
+		if c.Label == execute.DefaultTimeColLabel {
 			timeColumn = j
 			break
 		}
@@ -99,16 +99,21 @@ func (t *unpivotTransformation) Process(chunk table.Chunk, d *execute.TransportD
 	}
 
 	for i, c := range chunk.Cols() {
-		if chunk.Key().HasCol(c.Label) || c.Label == "_time" {
+		if chunk.Key().HasCol(c.Label) || c.Label == execute.DefaultTimeColLabel {
 			continue
 		}
+
+		chunkValues := chunk.Values(i)
+		chunkValues.Retain()
+
+		newChunkLen := chunk.Len() - chunkValues.NullN()
 
 		groupKey := chunk.Key()
 		columns := groupKey.Cols()
 		columns = append(columns,
 			flux.ColMeta{Label: "_field", Type: flux.TString},
-			flux.ColMeta{Label: "_time", Type: flux.TTime},
-			flux.ColMeta{Label: "_value", Type: c.Type},
+			flux.ColMeta{Label: execute.DefaultTimeColLabel, Type: flux.TTime},
+			flux.ColMeta{Label: execute.DefaultValueColLabel, Type: c.Type},
 		)
 
 		groupCols := []flux.ColMeta{{Label: "_field", Type: flux.TString}}
@@ -131,20 +136,38 @@ func (t *unpivotTransformation) Process(chunk table.Chunk, d *execute.TransportD
 					break
 				}
 			}
-			values := chunk.Values(fromColumn)
-			values.Retain()
+
+			oldValues := chunk.Values(fromColumn)
+			var values array.Array
+			if newChunkLen == chunk.Len() {
+				values = oldValues
+				values.Retain()
+			} else {
+				values = array.Slice(oldValues, 0, newChunkLen)
+			}
 			buffer.Values[toColumn] = values
 		}
 
-		buffer.Values[len(buffer.Values)-3] = array.StringRepeat(c.Label, chunk.Len(), mem)
+		buffer.Values[len(buffer.Values)-3] = array.StringRepeat(c.Label, newChunkLen, mem)
 
-		times := chunk.Values(timeColumn)
-		times.Retain()
+		var times array.Array
+		oldTimes := chunk.Ints(timeColumn)
+		if newChunkLen == chunk.Len() {
+			times = oldTimes
+			times.Retain()
+		} else {
+			builder := array.NewIntBuilder(mem)
+			builder.Reserve(newChunkLen)
+			for i := 0; i < chunkValues.Len(); i++ {
+				if chunkValues.IsValid(i) {
+					builder.Append(oldTimes.Value(i))
+				}
+			}
+			times = builder.NewArray()
+		}
 		buffer.Values[len(buffer.Values)-2] = times
 
-		values := chunk.Values(i)
-		values.Retain()
-		buffer.Values[len(buffer.Values)-1] = values
+		buffer.Values[len(buffer.Values)-1] = chunkValues
 
 		out := table.ChunkFromBuffer(buffer)
 		if err := d.Process(out); err != nil {
