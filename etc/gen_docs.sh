bf75c1bf675c3c5705205a985eed9231d9788ec9
Nathaniel Cook <nvcook42@gmail.com>
2022-11-01 08:29:09 -0600

    fix(fluxdoc): use db when generating docs (#5322)
    Previously the fluxdoc gen would run a compilation step using `fluxc` to
    speed up compilation. Compilation is very fast now and we have the salsa
    db that does proper caching of intermediate steps. This change removes
    the `fluxc` binary and the left over unused code. Also we update the
    `fluxdoc` binary to use the salsa db only.
    
    Since this is potentially a perf change I tested how fast the docs
    generate, its dominated almost exclusively in running the examples.
    Compiling the stdlib is instantaneous in my system.

--- etc/gen_docs.sh
+++ etc/gen_docs.sh
@@ -3,22 +3,13 @@
 set -e
 
 stdlib=${FLUX_STDLIB_SRC-./stdlib}
-fluxc=${FLUXC-fluxc}
 fluxdoc=${FLUXDOC-fluxdoc}
 gendir=${DOCS_GEN_DIR-./generated_docs}
 full_docs="${gendir}/flux-docs-full.json"
 short_docs="${gendir}/flux-docs-short.json"
 
-dir=$(mktemp -d)
-stdlib_compiled="${dir}/stdlib"
-
-# Compile stdlib to a temporary dir
-mkdir -p $stdlib_compiled
-$fluxc stdlib --srcdir "${stdlib}" --outdir "${stdlib_compiled}"
-
 # Generate docs JSON files
 mkdir -p "${gendir}"
-$fluxdoc dump --dir ${stdlib} --stdlib-dir "${stdlib_compiled}" --output ${full_docs} --nested
-$fluxdoc dump --dir ${stdlib} --stdlib-dir "${stdlib_compiled}" --output ${short_docs} --short
+$fluxdoc dump --dir ${stdlib} --output ${full_docs} --nested
+$fluxdoc dump --dir ${stdlib} --output ${short_docs} --short
 
-rm -rf "${dir}"
