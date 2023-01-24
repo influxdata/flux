1eba4ff2ba6002756333eed3f4c95e6f6c79e9cc
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-01-24 01:03:19 +0100

    feat(contrib/qxip/hash): add sha1, md5, base64, hmac (#5371)
    * feat(contrib/qxip/hash): add sha1, md5, b64
    
    * feat(contrib/qxip/hash): extend coverage
    
    * feat(contrib/qxip/hash): extend coverage
    
    * feat(contrib/qxip/hash): test checkpoint
    
    * feat(contrib/qxip/hash): test checkpoint
    
    * feat(contrib/qxip/hash): fix readme
    
    * feat(contrib/qxip/hash): add hmac
    
    * feat(contrib/qxip/hash): test hmac
    
    * feat(contrib/qxip/hash): merge suggestion
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat(contrib/qxip/hash): merge suggestion
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat(contrib/qxip/hash): merge suggestion
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat(contrib/qxip/hash): merge suggestion
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat(contrib/qxip/hash): merge suggestion
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat(contrib/qxip/hash): merge suggestion
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat(contrib/qxip/hash): merge suggestion
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat(contrib/qxip/hash): regenerate
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>

--- stdlib/contrib/qxip/hash/README.md
+++ stdlib/contrib/qxip/hash/README.md
@@ -10,9 +10,20 @@ Example:
 ```
     import "contrib/qxip/hash"
 
-    a = hash.sha256("Hello, world!")
-    // a is "315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3"
+    a = hash.sha256(v: "Hello, world!")
 ```
+
+## hash.sha1
+The `hash.sha256()` function converts a single string to a hash using sha256.
+
+Example:
+
+```
+    import "contrib/qxip/hash"
+
+    a = hash.sha1(v: "Hello, world!")
+```
+
 ## hash.xxhash64
 The `hash.xxhash64()` function converts a single string to a hash using xxhash64.
 
@@ -21,9 +32,9 @@ Example:
 ```
     import "contrib/qxip/hash"
 
-    a = hash.xxhash64("Hello, world!")
-    // a is "17691043854468224118"
+    a = hash.xxhash64(v: "Hello, world!")
 ```
+
 ## hash.cityhash64
 The `hash.cityhash64()` function converts a single string to hash using cityhash64.
 
@@ -32,8 +43,40 @@ Example:
 ```
     import "contrib/qxip/hash"
 
-    a = hash.cityhash64("Hello, world!")
-    // a is "2359500134450972198"
+    a = hash.cityhash64(v: "Hello, world!")
+```
+
+## hash.md5
+The `hash.md5()` function converts a single string to hash using MD5.
+
+Example:
+
+```
+    import "contrib/qxip/hash"
+
+    a = hash.md5(v: "Hello, world!")
+```
+
+## hash.b64
+The `hash.b64()` function converts a single string to a Base64 string.
+
+Example:
+
+```
+    import "contrib/qxip/hash"
+
+    a = hash.b64(v: "Hello, world!")
+```
+
+## hash.hmac
+The `hash.hmac()` function converts a string and key pair to sha1 signed Base64 string.
+
+Example:
+
+```
+    import "contrib/qxip/hash"
+
+    a = hash.hmac(v: "helloworld", k: "123456")
 ```
 
 ## Contact
--- stdlib/contrib/qxip/hash/hash_test.go
+++ stdlib/contrib/qxip/hash/hash_test.go
@@ -51,6 +51,49 @@ func Test_Sha256(t *testing.T) {
 	}
 }
 
+func Test_Sha1(t *testing.T) {
+	testCases := []struct {
+		name      string
+		v         interface{}
+		want      string
+		wantNull  bool
+		expectErr error
+	}{
+		{
+			name: "sha1(v:string)",
+			v:    "Hello, world!",
+			want: "943a702d06f34599aee1f8da8ef9f7296031d699",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			myMap := map[string]values.Value{
+				"v": values.New(tc.v),
+			}
+			args := interpreter.NewArguments(values.NewObjectWithValues(myMap))
+			got, err := sha1(args)
+			if err != nil {
+				if tc.expectErr == nil {
+					t.Errorf("unexpected error - want: <nil>, got: %s", err.Error())
+				} else if want, got := tc.expectErr.Error(), err.Error(); got != want {
+					t.Errorf("unexpected error - want: %s, got: %s", want, got)
+				}
+				return
+			}
+			if !tc.wantNull {
+				want := values.NewString(tc.want)
+				if !got.Equal(want) {
+					t.Errorf("Wanted: %s, got: %v", want, got)
+				}
+			} else {
+				if !got.IsNull() {
+					t.Errorf("Wanted: %v, got: %v", values.Null, got)
+				}
+			}
+		})
+	}
+}
+
 func Test_Xxhash64(t *testing.T) {
 	testCases := []struct {
 		name      string
@@ -115,6 +158,138 @@ func Test_Cityhash64(t *testing.T) {
 			}
 			args := interpreter.NewArguments(values.NewObjectWithValues(myMap))
 			got, err := cityhash64(args)
+			if err != nil {
+				if tc.expectErr == nil {
+					t.Errorf("unexpected error - want: <nil>, got: %s", err.Error())
+				} else if want, got := tc.expectErr.Error(), err.Error(); got != want {
+					t.Errorf("unexpected error - want: %s, got: %s", want, got)
+				}
+				return
+			}
+			if !tc.wantNull {
+				want := values.NewString(tc.want)
+				if !got.Equal(want) {
+					t.Errorf("Wanted: %s, got: %v", want, got)
+				}
+			} else {
+				if !got.IsNull() {
+					t.Errorf("Wanted: %v, got: %v", values.Null, got)
+				}
+			}
+		})
+	}
+}
+
+func Test_Base64(t *testing.T) {
+	testCases := []struct {
+		name      string
+		v         interface{}
+		want      string
+		wantNull  bool
+		expectErr error
+	}{
+		{
+			name: "b64(v:string)",
+			v:    "Hello, world!",
+			want: "SGVsbG8sIHdvcmxkIQ==",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			myMap := map[string]values.Value{
+				"v": values.New(tc.v),
+			}
+			args := interpreter.NewArguments(values.NewObjectWithValues(myMap))
+			got, err := b64(args)
+			if err != nil {
+				if tc.expectErr == nil {
+					t.Errorf("unexpected error - want: <nil>, got: %s", err.Error())
+				} else if want, got := tc.expectErr.Error(), err.Error(); got != want {
+					t.Errorf("unexpected error - want: %s, got: %s", want, got)
+				}
+				return
+			}
+			if !tc.wantNull {
+				want := values.NewString(tc.want)
+				if !got.Equal(want) {
+					t.Errorf("Wanted: %s, got: %v", want, got)
+				}
+			} else {
+				if !got.IsNull() {
+					t.Errorf("Wanted: %v, got: %v", values.Null, got)
+				}
+			}
+		})
+	}
+}
+
+func Test_MD5(t *testing.T) {
+	testCases := []struct {
+		name      string
+		v         interface{}
+		want      string
+		wantNull  bool
+		expectErr error
+	}{
+		{
+			name: "md5(v:string)",
+			v:    "Hello, world!",
+			want: "6cd3556deb0da54bca060b4c39479839",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			myMap := map[string]values.Value{
+				"v": values.New(tc.v),
+			}
+			args := interpreter.NewArguments(values.NewObjectWithValues(myMap))
+			got, err := md5(args)
+			if err != nil {
+				if tc.expectErr == nil {
+					t.Errorf("unexpected error - want: <nil>, got: %s", err.Error())
+				} else if want, got := tc.expectErr.Error(), err.Error(); got != want {
+					t.Errorf("unexpected error - want: %s, got: %s", want, got)
+				}
+				return
+			}
+			if !tc.wantNull {
+				want := values.NewString(tc.want)
+				if !got.Equal(want) {
+					t.Errorf("Wanted: %s, got: %v", want, got)
+				}
+			} else {
+				if !got.IsNull() {
+					t.Errorf("Wanted: %v, got: %v", values.Null, got)
+				}
+			}
+		})
+	}
+}
+
+func Test_HMac(t *testing.T) {
+	testCases := []struct {
+		name      string
+		v         interface{}
+		k         interface{}
+		want      string
+		wantNull  bool
+		expectErr error
+	}{
+		{
+			name: "hmac(v:string, k: key)",
+			v:    "helloworld",
+			k:    "123456",
+			want: "75B5ueLnnGepYvh+KoevTzXCrjc=",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			myMap := map[string]values.Value{
+				"v": values.New(tc.v),
+				"k": values.New(tc.k),
+			}
+			args := interpreter.NewArguments(values.NewObjectWithValues(myMap))
+			got, err := hmac(args)
 			if err != nil {
 				if tc.expectErr == nil {
 					t.Errorf("unexpected error - want: <nil>, got: %s", err.Error())
