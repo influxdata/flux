7bc947c887a8c4bac7fded86d5c32e006e3187cc
Martin Hilton <mhilton@influxdata.com>
2025-11-27 08:41:34 +0000

    build(deps): Replace github.com/lib/pq
    Replace the deprecated github.com/lib/pq module with
    github.com/jackc/pgx/v5 for postgres support.

--- stdlib/sql/open.go
+++ stdlib/sql/open.go
@@ -38,6 +38,8 @@ func getOpenFunc(driverName, dataSourceName string) openFunc {
 	switch driverName {
 	case "mssql", "sqlserver":
 		return mssqlOpenFunction(driverName, dataSourceName)
+	case "postgres":
+		return defaultOpenFunction("pgx", dataSourceName)
 	default:
 		return defaultOpenFunction(driverName, dataSourceName)
 	}
